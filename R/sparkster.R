af5778557d4ee56c22355fab3d1f0115ff7d85fc
Javier Luraschi <javierluraschi@hotmail.com>
2016-04-07 13:39:50 -0700

    add launch_ec2 command

--- R/sparkster.R
+++ R/sparkster.R
@@ -1,6 +1,6 @@
 #' Start an EC2 Spark Cluster
 #'
-#' This function will start an EC2 cluster.
+#' This function will launch a new an EC2 cluster.
 #'
 #' @param access_key_id EC2 access key id. Create a new access key from https://console.aws.amazon.com/iam/home?#security_credential
 #' @param secret_access_key EC2 secret access key.
@@ -9,7 +9,7 @@
 #' @param pem_path Identity file for ssh connections.
 #' @param cluster_name Name used to identify cluster.
 #' @export
-start_ec2 <- function(
+launch_ec2 <- function(
   access_key_id,
   secret_access_key,
   pem_path,
@@ -29,6 +29,27 @@ start_ec2 <- function(
 
   run_ec2_command(command = paste("launch", cluster_name),
                   command_params = command_params,
+                  spark_dir = sparkInfo$sparkVersionDir,
+                  access_key_id = access_key_id,
+                  secret_access_key = secret_access_key,
+                  version = version,
+                  pem_path = pem_path)
+}
+
+start_ec2 <- function(
+  access_key_id,
+  secret_access_key,
+  pem_path,
+  instance_count = 1,
+  version = "1.6.1",
+  cluster_name = "sparkster") {
+
+  sparkInfo <- download_spark(version)
+
+  validate_pem(pem_path);
+
+  run_ec2_command(command = paste("launch", cluster_name),
+                  command_params = "",
                   spark_dir = sparkInfo$sparkVersionDir,
                   access_key_id = access_key_id,
                   secret_access_key = secret_access_key,
