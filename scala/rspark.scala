e8962b328d2509bc98974514489a3f11989864d3
Javier Luraschi <javierluraschi@hotmail.com>
2016-06-03 09:06:42 -0700

    sketch schemaRddToVectorRdd scala implementation

--- scala/rspark.scala
+++ scala/rspark.scala
@@ -7,4 +7,17 @@ object utils {
   def plus2(base:Int) : Int = {
     return base + 2
   }
+
+  // See: http://spark.apache.org/docs/latest/mllib-clustering.html
+  /*
+  def schemaRddToVectorRdd(schemaRdd:RDD[Row]) : RDD[Vector] = {
+    val featureVectors = schemaRdd.map(row => {
+      Vectors.dense(row.toSeq.toArray.map({
+        case s: String => s.toDouble
+        case l: Long => l.toDouble
+        case _ => 0.0
+      }))
+    })
+  }
+  */
 }
