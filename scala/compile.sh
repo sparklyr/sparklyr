8d268c0079a3587ca293e803c5757dc8f30e46f3
Javier Luraschi <javierluraschi@hotmail.com>
2016-06-03 09:04:08 -0700

    add support to compile jars against mllib and spark libraries

--- scala/compile.sh
+++ scala/compile.sh
@@ -1,5 +1,12 @@
+#!/bin/sh
 # To compile the inst/java classes, run this script from the script directory
 
+THE_CLASSPATH=.
+for i in `ls ~/Library/Caches/spark/spark-2.0.0-preview-bin-hadoop2.6/jars/*.jar`
+do
+  THE_CLASSPATH=${THE_CLASSPATH}:${i}
+done
+
 mkdir ../inst/java
-scalac rspark.scala
+scalac -classpath "$THE_CLASSPATH" rspark.scala
 jar cf ../inst/java/rspark_utils.jar utils.class utils$.class
--- scala/rspark.scala
+++ scala/rspark.scala
@@ -1,3 +1,8 @@
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
+import org.apache.spark.rdd.RDD
+
 object utils {
   def plus2(base:Int) : Int = {
     return base + 2
