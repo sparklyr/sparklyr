86cd49ea5f7931df211bb39dfc44aa8224723adc
Kevin Ushey <kevinushey@gmail.com>
2016-06-06 12:33:23 -0700

    use 'ml.KMeans' for ml_kmeans impl

--- R/ml_kmeans.R
+++ R/ml_kmeans.R
@@ -5,23 +5,31 @@
 #' @param iter.max Maximum number of iterations used to compute kmeans.
 ml_kmeans <- function(x, centers, iter.max = 10) {
   scon <- spark_scon(x)
-  sparkRdd <- as_spark_rdd(x)
+  df <- as_spark_dataframe(x)
 
-  kmeansModel <- spark_invoke_static(
+  # use VectorAssembler to join columns into
+  # single 'features' column
+  assembler <- spark_invoke_static_ctor(
     scon,
-
-    "org.apache.spark.mllib.clustering.KMeans",
-    "train",
-
-    sparkRdd,
-    as.integer(centers),
-    as.integer(iter.max)
+    "org.apache.spark.ml.feature.VectorAssembler"
   )
 
-  list(
-    model = kmeansModel,
-    centers = spark_invoke(kmeansModel, "clusterCenters") %>%
-      spark_jobj_list_to_array_df(colnames(x))
+  columns <- as.list(spark_invoke(df, "columns"))
+  tdf <- assembler %>%
+    spark_invoke("setInputCols", columns) %>%
+    spark_invoke("setOutputCol", "features") %>%
+    spark_invoke("transform", df)
+
+  # invoke KMeans
+  kmeans <- spark_invoke_static_ctor(
+    sc,
+    "org.apache.spark.ml.clustering.KMeans"
   )
+
+  kmm <- kmeans %>%
+    spark_invoke("setK", 3L) %>%
+    spark_invoke("setFeaturesCol", "features") %>%
+    spark_invoke("fit", tdf)
+
+  kmm
 }
-
