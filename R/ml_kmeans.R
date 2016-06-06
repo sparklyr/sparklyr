74aec86dba7ffd4d02b822682b97f6971ea28c21
Kevin Ushey <kevinushey@gmail.com>
2016-06-06 12:57:02 -0700

    add (internal) 'spark_ml_kmeans'

--- R/ml_kmeans.R
+++ R/ml_kmeans.R
@@ -1,9 +1,4 @@
-#' Computes kmeans from a dplyr source
-#' @export
-#' @param x A dplyr source.
-#' @param centers Number of centers to compute.
-#' @param iter.max Maximum number of iterations used to compute kmeans.
-ml_kmeans <- function(x, centers, iter.max = 10) {
+spark_ml_kmeans <- function(x, centers, iter.max = 10) {
   scon <- spark_scon(x)
   df <- as_spark_dataframe(x)
 
@@ -32,5 +27,15 @@ ml_kmeans <- function(x, centers, iter.max = 10) {
     spark_invoke("setFeaturesCol", "features") %>%
     spark_invoke("fit", tdf)
 
+  kmm
+}
+
+#' Computes kmeans from a dplyr source
+#' @export
+#' @param x A dplyr source.
+#' @param centers Number of centers to compute.
+#' @param iter.max Maximum number of iterations used to compute kmeans.
+ml_kmeans <- function(x, centers, iter.max = 10) {
+  kmm <- spark_ml_kmeans(x, centers, iter.max)
   kmm
 }
