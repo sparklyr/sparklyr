aea6e7d3a2b1d2cec9cfabc4562603b1dc1307f8
Kevin Ushey <kevinushey@gmail.com>
2016-06-07 17:37:43 -0700

    remove redundant methods (handled by '.default')

--- R/ml_interface.R
+++ R/ml_interface.R
@@ -1,18 +1,3 @@
-#' @export
-coef.spark_ml <- function(object, ...) {
-  object$coefficients
-}
-
-#' @export
-coefficients.spark_ml <- function(object, ...) {
-  object$coefficients
-}
-
-#' @export
-residuals.spark_ml <- function(object, ...) {
-  object$residuals
-}
-
 ml_model <- function(class, model, ...) {
   object <- list(..., .model = model)
   class(object) <- c(
