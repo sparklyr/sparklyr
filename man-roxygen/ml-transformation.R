3ad7a0cf9ec79766eb77dbc84b34108b2a533411
Kevin Ushey <kevinushey@gmail.com>
2016-06-14 16:52:28 -0700

    documentation for the 'ml_apply' functions

---
+++ man-roxygen/ml-transformation.R
@@ -0,0 +1,11 @@
+#' @param df A Spark Dataset.
+#' @param input.col The name of the input column(s).
+#' @param output.col The name of the output column.
+#'
+#' @seealso See \url{http://spark.apache.org/docs/latest/ml-features.html} for
+#'   more information on the set of transformations available for DataFrame
+#'   columns in Spark.
+#'
+#' @family feature transformation routines
+#'
+#' @export
