faf32c654682af65af96a586aab0325789e4827b
JJ Allaire <jj@rstudio.org>
2016-06-14 14:15:59 -0400

    initial staticdocs index

---
+++ inst/staticdocs/index.R
@@ -0,0 +1,11 @@
+sd_section("Connecting to Spark",
+    "Functions for installing Spark components and connecting to Spark clusters.",
+    c("install_spark",
+      "spark_connect")
+)
+
+sd_section("Reading and Writing Data",
+    "Functions for reading and writing Spark data frames",
+    c("load_csv",
+      "load_json")
+)
