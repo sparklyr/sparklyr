6c7a20607caf2d3455d2596fe5badef705136c2c
Kevin Ushey <kevinushey@gmail.com>
2016-06-15 15:42:38 -0700

    remove unused methods

--- R/spark_rdd.R
+++
@@ -1,6 +0,0 @@
-as_spark_rdd <- function(jobj) {
-  scon <- spark_scon(jobj)
-  jobj <- as_spark_dataframe(jobj)
-  rdd <- spark_invoke(jobj, "rdd")
-  spark_invoke_static(scon, "utils", "schemaRddToVectorRdd", rdd)
-}
--- inst/scala/rspark.scala
+++ inst/scala/rspark.scala
@@ -8,23 +8,6 @@ import org.apache.spark.sql._
 
 object utils {
 
-  def plus2(base:Int) : Int = {
-    return base + 2
-  }
-
-  // See: http://spark.apache.org/docs/latest/mllib-clustering.html
-  def schemaRddToVectorRdd(schemaRdd:RDD[Row]) : RDD[Vector] = {
-    schemaRdd.map(row => {
-      Vectors.dense(row.toSeq.toArray.map({
-        case s: String => s.toDouble
-        case l: Long => l.toDouble
-        case i: Integer => i.toDouble
-        case d: Double => d
-        case _ => 0.0
-      }))
-    })
-  }
-
   def castColumnDouble(df: DataFrame, inputColName: String, outputColName: String): DataFrame = {
     df.withColumn(outputColName, df.col(inputColName).cast(DataTypes.DoubleType))
   }
