e802c8b2bd4c96bb9eb09b22d01490804a044eda
Javier Luraschi <javierluraschi@hotmail.com>
2016-06-15 10:21:53 -0700

    add stub for multilayer perceptron

---
+++ R/ml_multilayer_perceptron.R
@@ -0,0 +1,46 @@
+spark_ml_multilayer_perceptron <- function(x)
+{
+  scon <- spark_scon(x)
+  df <- as_spark_dataframe(x)
+
+  envir <- new.env(parent = emptyenv())
+  tdf <- ml_prepare_dataframe(df, features, response, envir = envir)
+
+  lr <- spark_invoke_static_ctor(
+    scon,
+    "org.apache.spark.ml.classification.MultilayerPerceptronClassifier"
+  )
+
+  layers <- as.integer(c(4, 5, 4, 3))
+
+  fit <- lr %>%
+    spark_invoke("setLayers", envir$features) %>%
+    spark_invoke("setBlockSize", envir$features) %>%
+    spark_invoke("setSeed", envir$response) %>%
+    spark_invoke("setMaxIter", as.logical(intercept)) %>%
+
+    spark_invoke("fit", tdf)
+}
+
+#' Spark ML -- Multilayer Perceptron
+#'
+#' Creates and trains multilayer perceptron on a \code{spark_tbl}.
+#'
+#' @param x A \code{spark_tbl}.
+#'
+#' @family Spark ML routines
+#'
+#' @export
+ml_multilayer_perceptron <- function(x) {
+  spark_ml_multilayer_perceptron(x)
+}
+
+#' @export
+print.ml_multilayer_perceptron <- function(x, ...) {
+}
+
+#' @export
+residuals.ml_multilayer_perceptron <- function(object, ...) {
+  stop("residuals not yet available for Spark multilayer perceptron")
+}
+
