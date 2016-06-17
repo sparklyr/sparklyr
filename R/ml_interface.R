0a3164fc5da6f9e8dd8b34c69af3c5322d668c12
Kevin Ushey <kevinushey@gmail.com>
2016-06-16 17:30:11 -0700

    computation of 5-number summary for residuals

--- R/ml_interface.R
+++ R/ml_interface.R
@@ -6,3 +6,47 @@ ml_model <- function(class, model, ...) {
   )
   object
 }
+
+#' @export
+spark_scon.ml_model <- function(x, ...) {
+  spark_scon(x$.model)
+}
+
+ml_model_print_residuals_summary <- function(model) {
+  scon <- spark_scon(model)
+
+  residuals <- model$.model %>%
+    spark_invoke("summary") %>%
+    spark_invoke("residuals")
+
+  name <- random_string()
+  spark_invoke(residuals, "registerTempTable", name)
+
+  ctx <- spark_invoke(residuals, "sqlContext")
+
+  sql <- paste(
+    "SELECT",
+    paste(
+      "MIN(residuals) as Min",
+      "PERCENTILE_APPROX(residuals, 0.25) AS Q1",
+      "PERCENTILE_APPROX(residuals, 0.50) AS Median",
+      "PERCENTILE_APPROX(residuals, 0.75) AS Q3",
+      "MAX(residuals) as Max",
+      sep = ", "
+    ),
+    "FROM", name
+  )
+
+  summary <-
+    spark_invoke(ctx, "sql", sql) %>%
+    spark_invoke("collect") %>%
+    unlist(recursive = FALSE) %>%
+    as.numeric()
+
+  names(summary) <- c("Min", "1Q", "Median", "3Q", "Max")
+
+  cat("Residuals:", sep = "\n")
+  print(summary)
+
+  invisible(summary)
+}
