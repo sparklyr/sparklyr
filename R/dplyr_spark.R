6755418a39c8f78dc13cbed028ea656d88efe462
JJ Allaire <jj@rstudio.org>
2016-06-20 06:07:38 -0400

    fix incompatible S3 method parameter

--- R/dplyr_spark.R
+++ R/dplyr_spark.R
@@ -217,8 +217,8 @@ sdf_partition <- function(x, ..., seed = sample(.Machine$integer.max, 1)) {
 }
 
 #' @export
-db_save_query.DBISparkConnection <- function (conn, sql, name, temporary = TRUE, ...) 
+db_save_query.DBISparkConnection <- function (con, sql, name, temporary = TRUE, ...) 
 {
-  df <- as_spark_dataframe(conn@scon, sql)
+  df <- as_spark_dataframe(con@scon, sql)
   spark_invoke(df, "registerTempTable", name)
 }
