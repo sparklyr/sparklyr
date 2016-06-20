53d5c11ae5aea38d812471409db43e2aae538496
JJ Allaire <jj@rstudio.org>
2016-06-20 11:55:08 -0400

    make the DBI interface internal
    We don't encourage or document using the DBI interface. This commit removes the exports and documentation for the DBI interface (we can still use it in exactly the same way internally).

--- R/dbi_spark_table.R
+++ R/dbi_spark_table.R
@@ -1,36 +1,4 @@
-#' DBI interface for Spark
-#'
-#' @param conn a \code{\linkS4class{DBISparkConnection}} object, produced by
-#'   \code{\link[DBI]{dbConnect}}
-#' @param name a character string specifying a table name.
-#' @param value A data.frame to write to the database.
-#' @inheritParams DBI::sqlCreateTable
-#' @examples
-#' \dontrun{
-#' sc <- spark_connect()
-#' con <- dbConnect(spark::DBISpark(sc))
-#'
-#' dbListTables(con)
-#' dbWriteTable(con, "mtcars", mtcars, temporary = TRUE)
-#' dbReadTable(con, "mtcars")
-#'
-#' dbListTables(con)
-#' dbExistsTable(con, "mtcars")
-#'
-#' # A zero row data frame just creates a table definition.
-#' dbWriteTable(con, "mtcars2", mtcars[0, ], temporary = TRUE)
-#' dbReadTable(con, "mtcars2")
-#'
-#' dbDisconnect(con)
-#' }
-#' @name DBI-interface
-#' @keywords internal
-NULL
 
-#' @export
-#' @rdname DBI-interface
-#' @param repartition Total of partitions used to distribute table or 0 (default) to avoid partitioning
-#' @param local_file When TRUE, uses a local file to copy the data frame, this is only available in local installs.
 setMethod("dbWriteTable", "DBISparkConnection",
   function(conn, name, value, temporary = TRUE, repartition = 0, local_file = NULL) {
     if (!temporary) {
@@ -54,8 +22,6 @@ setMethod("dbWriteTable", "DBISparkConnection",
   }
 )
 
-#' @export
-#' @rdname DBI-interface
 setMethod("dbReadTable", c("DBISparkConnection", "character"),
   function(conn, name) {
     name <- dbQuoteIdentifier(conn, name)
@@ -63,21 +29,18 @@ setMethod("dbReadTable", c("DBISparkConnection", "character"),
   }
 )
 
-#' @export
-#' @rdname DBI-interface
+
 setMethod("dbListTables", "DBISparkConnection", function(conn) {
   df <- spark_api_sql_tables(conn@api)
   df$tableName
 })
 
-#' @export
-#' @rdname DBI-interface
+
 setMethod("dbExistsTable", c("DBISparkConnection", "character"), function(conn, name) {
   name %in% dbListTables(conn)
 })
 
-#' @export
-#' @rdname DBI-interface
+
 setMethod("dbRemoveTable", c("DBISparkConnection", "character"),
   function(conn, name) {
     spark_drop_temp_table(conn@api, name)
@@ -86,11 +49,8 @@ setMethod("dbRemoveTable", c("DBISparkConnection", "character"),
   }
 )
 
+
 #' @export
-#' @rdname DBI-interface
-#' @param .data Data and operations references
-#' @param ... Additional parameters
-#' @param .dots Original parameters
 mutate_.tbl_spark <- function(.data, ..., .dots) {
   dots <- lazyeval::all_dots(.dots, ..., all_named = TRUE)
 
