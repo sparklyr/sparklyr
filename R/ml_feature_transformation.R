f3865016455d8486b1c5e1729cb6c537fd3244ca
Kevin Ushey <kevinushey@gmail.com>
2016-06-21 12:09:52 -0700

    extend interface for 'sdf_partition'; docs

--- R/ml_feature_transformation.R
+++ R/ml_feature_transformation.R
@@ -13,6 +13,61 @@ sdf_register <- function(x, name = random_string()) {
   tbl(spark_connection(x), name)
 }
 
+#' Partition a Spark Dataframe
+#'
+#' Partition a Spark DataFrame into multiple groups. This routine is useful
+#' for splitting a DataFrame into, for example, training and test datasets.
+#' 
+#' The sampling weights define the probability that a particular observation
+#' will be assigned to a particular partition, not the resulting size of the
+#' partition. This implies that partitioning a DataFrame with, for example,
+#' 
+#' \code{sdf_partition(x, training = 0.5, test = 0.5)}
+#' 
+#' is not guaranteed to produce \code{training} and \code{test} partitions
+#' of equal size.
+#'
+#' @param x A \code{tbl_spark}.
+#' @param ... Named parameters, mapping table names to weights. The weights
+#'   will be normalized such that they sum to 1.
+#' @param weights An alternate mechanism for supplying weights -- when
+#'   specified, this takes precedence over the \code{...} arguments.
+#' @param seed Random seed to use for randomly partitioning the dataset. Set
+#'   this if you want your partitioning to be reproducible on repeated runs.
+#'
+#' @return An \R \code{list} of \code{tbl_spark}s.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # randomly partition data into a 'training' and 'test'
+#' # dataset, with 60% of the observations assigned to the
+#' # 'training' dataset, and 40% assigned to the 'test' dataset
+#' data(diamonds, package = "ggplot2")
+#' diamonds_tbl <- copy_to(sc, diamonds, "diamonds")
+#' partitions <- diamonds_tbl %>%
+#'   sdf_partition(training = 0.6, test = 0.4)
+#' print(partitions)
+#' 
+#' # alternate way of specifying weights
+#' weights <- c(training = 0.6, test = 0.4)
+#' diamonds_tbl %>% sdf_partition(weights = weights)
+#' }
+sdf_partition <- function(x,
+                          ...,
+                          weights = NULL,
+                          seed = sample(.Machine$integer.max, 1))
+{
+  weights <- weights %||% list(...)
+  nm <- names(weights)
+  if (is.null(nm) || any(!nzchar(nm)))
+    stop("all weights must be named")
+  partitions <- spark_dataframe_split(x, as.numeric(weights), seed = seed)
+  names(partitions) <- nm
+  partitions
+}
+
 #' Mutate a Spark DataFrame
 #'
 #' Use Spark's \href{http://spark.apache.org/docs/latest/ml-features.html}{feature transformers}
