81112c8e4c47b94067c2f5c8c8e88ecdd2ea9b0a
Javier Luraschi <javierluraschi@hotmail.com>
2016-06-21 09:39:01 -0700

    subclass spark_write_csv

---
+++ R/api_spark_data.R
@@ -0,0 +1,37 @@
+#' Reads a CSV file and provides a data source compatible with dplyr
+#'
+#' @param sc The Spark connection
+#' @param name Name to reference the data source once it's loaded
+#' @param path The path to the file. Needs to be accessible from the cluster. Supports: "hdfs://" or "s3n://"
+#' @param memory Loads data into memory
+#' @param repartition Total of partitions used to distribute table or 0 (default) to avoid partitioning
+#' @param overwrite Overwrite the table with the given name when it exists
+#'
+#' @export
+spark_read_csv <- function(x, name, path, repartition = 0, memory = TRUE, overwrite = TRUE) {
+  UseMethod("spark_read_csv")
+}
+
+spark_read_csv.spark_connection <- function(x, name, path, repartition = 0, memory = TRUE, overwrite = TRUE) {
+  sc <- x
+  if (overwrite) spark_remove_table_if_exists(sc, name)
+  
+  api <- spark_api(sc)
+  df <- spark_api_read_csv(api, path.expand(path))
+  spark_partition_register_df(sc, df, api, name, repartition, memory)
+}
+
+#' Writes a dplyr operation result as a CSV file
+#'
+#' @inheritParams spark_read_csv
+#' @param x A dplyr operation, for instance, `tbls(db, "flights")`
+#'
+#' @export
+spark_write_csv <- function(x, path) {
+  UseMethod("spark_write_csv")
+}
+
+spark_write_csv.spark_connection <- function(x, path) {
+  sqlResult <- spark_sqlresult_from_dplyr(x)
+  spark_api_write_csv(sqlResult, path.expand(path))
+}
--- R/dplyr_spark_data.R
+++ R/dplyr_spark_data.R
@@ -25,27 +25,6 @@ spark_remove_table_if_exists <- function(sc, name) {
   }
 }
 
-#' Reads a CSV file and provides a data source compatible with dplyr
-#'
-#' Reads a CSV file and provides a data source compatible with dplyr
-#'
-#'
-#' @param sc The Spark connection
-#' @param name Name to reference the data source once it's loaded
-#' @param path The path to the file. Needs to be accessible from the cluster. Supports: "hdfs://" or "s3n://"
-#' @param memory Loads data into memory
-#' @param repartition Total of partitions used to distribute table or 0 (default) to avoid partitioning
-#' @param overwrite Overwrite the table with the given name when it exists
-#'
-#' @export
-spark_read_csv <- function(sc, name, path, repartition = 0, memory = TRUE, overwrite = TRUE) {
-  if (overwrite) spark_remove_table_if_exists(sc, name)
-
-  api <- spark_api(sc)
-  df <- spark_api_read_csv(api, path.expand(path))
-  spark_partition_register_df(sc, df, api, name, repartition, memory)
-}
-
 spark_source_from_ops <- function(x) {
   classList <- lapply(x, function(e) { attr(e, "class") } )
 
@@ -62,19 +41,6 @@ spark_sqlresult_from_dplyr <- function(x) {
   api <- spark_api(sparkSource)
   sql <- dplyr::sql_render(x)
   sqlResult <- spark_api_sql(api, as.character(sql))
-}
-
-#' Writes a dplyr operation result as a CSV file
-#'
-#' Writes a dplyr operation result as a CSV file
-#'
-#' @inheritParams spark_read_csv
-#' @param x A dplyr operation, for instance, `tbls(db, "flights")`
-#'
-#' @export
-spark_write_csv <- function(x, path) {
-  sqlResult <- spark_sqlresult_from_dplyr(x)
-  spark_api_write_csv(sqlResult, path.expand(path))
 }
 
 #' Reads a parquet file and provides a data source compatible with dplyr
