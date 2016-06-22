b2542e157fea0ff3650c5f1086754fbf96052621
Javier Luraschi <javierluraschi@hotmail.com>
2016-06-21 21:20:08 -0700

    implement ml_naive_bayes

--- R/ml_naive_bayes.R
+++ R/ml_naive_bayes.R
@@ -1,1 +1,44 @@
-#TODO: http://spark.apache.org/docs/2.0.0-preview/ml-classification-regression.html#naive-bayes
+#' Spark ML -- Naive-Bayes
+#'
+#' Perform regression or classification using naive bayes.
+#'
+#' @param x An object convertable to a Spark DataFrame (typically, a \code{tbl_spark}).
+#' @param response The name of the response vector.
+#' @param features The names of features (terms) included in the model.
+#'
+#' @family Spark ML routines
+#'
+#' @export
+ml_naive_bayes <- function(x,
+                           response,
+                           features)
+{
+  df <- sparkapi_dataframe(x)
+  sc <- sparkapi_connection(df)
+  
+  envir <- new.env(parent = emptyenv())
+  tdf <- ml_prepare_dataframe(df, features, response, envir = envir)
+  
+  schema <- spark_dataframe_schema(df)
+  model <- "org.apache.spark.ml.classification.NaiveBayes"
+  
+  rf <- sparkapi_invoke_new(sc, model)
+  
+  fit <- rf %>%
+    sparkapi_invoke("setFeaturesCol", envir$features) %>%
+    sparkapi_invoke("setLabelCol", envir$response) %>%
+    sparkapi_invoke("fit", tdf)
+  
+  ml_model("ml_naive_bayes", fit,
+           features = features,
+           response = response,
+           model.parameters = as.list(envir)
+  )
+}
+
+#' @export
+print.ml_naive_bayes <- function(x, ...) {
+  formula <- paste(x$response, "~", paste(x$features, collapse = " + "))
+  cat("Call: ", formula, "\n\n", sep = "")
+  cat(sparkapi_invoke(x$.model, "toString"), sep = "\n")
+}
