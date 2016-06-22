7816b34c2e2b1f74fbecebe64f66e94b56fcf91e
Kevin Ushey <kevinushey@gmail.com>
2016-06-22 10:45:22 -0700

    expose 'lambda' for naive bayes; tweaks

--- R/ml_naive_bayes.R
+++ R/ml_naive_bayes.R
@@ -5,13 +5,15 @@
 #' @param x An object convertable to a Spark DataFrame (typically, a \code{tbl_spark}).
 #' @param response The name of the response vector.
 #' @param features The names of features (terms) included in the model.
+#' @param lambda The (Laplace) smoothing parameter. Defaults to zero.
 #'
 #' @family Spark ML routines
 #'
 #' @export
 ml_naive_bayes <- function(x,
                            response,
-                           features)
+                           features,
+                           lambda = 0)
 {
   df <- sparkapi_dataframe(x)
   sc <- sparkapi_connection(df)
@@ -19,7 +21,6 @@ ml_naive_bayes <- function(x,
   envir <- new.env(parent = emptyenv())
   tdf <- ml_prepare_dataframe(df, features, response, envir = envir)
   
-  schema <- spark_dataframe_schema(df)
   model <- "org.apache.spark.ml.classification.NaiveBayes"
   
   rf <- sparkapi_invoke_new(sc, model)
@@ -27,9 +28,25 @@ ml_naive_bayes <- function(x,
   fit <- rf %>%
     sparkapi_invoke("setFeaturesCol", envir$features) %>%
     sparkapi_invoke("setLabelCol", envir$response) %>%
+    sparkapi_invoke("setSmoothing", lambda) %>%
     sparkapi_invoke("fit", tdf)
   
-  ml_model("ml_naive_bayes", fit,
+  pi <- fit %>%
+    sparkapi_invoke("pi") %>%
+    sparkapi_invoke("toArray")
+  names(pi) <- envir$labels
+  
+  thetaMatrix <- fit %>% sparkapi_invoke("theta")
+  thetaValues <- thetaMatrix %>% sparkapi_invoke("toArray")
+  theta <- matrix(thetaValues,
+                  nrow = sparkapi_invoke(thetaMatrix, "numRows"),
+                  ncol = sparkapi_invoke(thetaMatrix, "numCols"))
+  rownames(theta) <- envir$labels
+  colnames(theta) <- features
+  
+  ml_model("naive_bayes", fit,
+           pi = sparkapi_invoke(fit, "pi"),
+           theta = sparkapi_invoke(fit, "theta"),
            features = features,
            response = response,
            model.parameters = as.list(envir)
@@ -37,7 +54,7 @@ ml_naive_bayes <- function(x,
 }
 
 #' @export
-print.ml_naive_bayes <- function(x, ...) {
+print.ml_model_naive_bayes <- function(x, ...) {
   formula <- paste(x$response, "~", paste(x$features, collapse = " + "))
   cat("Call: ", formula, "\n\n", sep = "")
   cat(sparkapi_invoke(x$.model, "toString"), sep = "\n")
