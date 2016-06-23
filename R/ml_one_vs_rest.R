1231953d3cd98ba8ba7a5d2bc44aacd8c6ab3393
Javier Luraschi <javierluraschi@hotmail.com>
2016-06-23 11:08:37 -0700

    implement ml_one_vs_rest

--- R/ml_one_vs_rest.R
+++ R/ml_one_vs_rest.R
@@ -1,1 +1,44 @@
-# TODO: http://spark.apache.org/docs/2.0.0-preview/ml-classification-regression.html#one-vs-rest-classifier-aka-one-vs-all
+#' Spark ML -- One vs Rest
+#'
+#' Perform regression or classification using one vs rest.
+#'
+#' @param x An object convertable to a Spark DataFrame (typically, a \code{tbl_spark}).
+#' @param classifier The classifier model. Can be obtained using the \code{only_model} parameter.
+#' @param response The name of the response vector.
+#' @param features The names of features (terms) to use as linear predictors
+#'   for the response.
+#'
+#' @family Spark ML routines
+#'
+#' @export
+ml_one_vs_rest <- function(x,
+                           classifier,
+                           response,
+                           features)
+{
+  df <- sparkapi_dataframe(x)
+  sc <- sparkapi_connection(df)
+  
+  envir <- new.env(parent = emptyenv())
+  tdf <- ml_prepare_dataframe(df, features, response, envir = envir)
+  
+  ovrc <- sparkapi_invoke_new(
+    sc,
+    "org.apache.spark.ml.classification.OneVsRest"
+  )
+  
+  fit <- ovrc %>%
+    sparkapi_invoke("setClassifier", classifier)
+    sparkapi_invoke("fit", tdf)
+  
+  ml_model("one_vs_rest", fit,
+           features = features,
+           response = response,
+           model.parameters = as.list(envir)
+  )
+}
+
+#' @export
+print.ml_one_vs_rest <- function(x, ...) {
+}
+
