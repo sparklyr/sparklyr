45e6951434bb8a1f2527fd52ad4442d563c5003f
Javier Luraschi <javierluraschi@hotmail.com>
2016-06-24 15:44:20 -0700

    add instruction to copy winutils.exe

--- R/install_spark_windows.R
+++ R/install_spark_windows.R
@@ -1,13 +1,19 @@
 spark_install_windows_local <- function() {
-  hivePath <- "\\tmp\\hive"
-  if (!dir.exists(hivePath)) {
-    dir.create(hivePath, recursive = TRUE)
-    message(paste("Created hive default directory under:", hivePath))
+  hadoopPath <- "\\tmp\\hadoop"
+  hadoopBinPath <- file.path(hadoopPath, "bin", fsep = "\\")
+  if (!dir.exists(hadoopPath)) {
+    dir.create(hadoopBinPath, recursive = TRUE)
+    message(paste("Created default hadoop bin directory under:", hadoopPath))
+  }
+  
+  if (!"HADOOP_HOME" %in% names(Sys.getenv())) {
+    Sys.setenv("HADOOP_HOME" = hivePath)
+    message(paste("Set HADOOP_HOME to:", hadoopPath))
   }
   
   vcRedistDownload <- "http://www.microsoft.com/download/en/details.aspx?id=13523"
   winutilsDownload <- "https://code.google.com/archive/p/rrd-hadoop-win32/source/default/source"
-  if (!grepl(x64, version$platform)) {
+  if (!grepl("x64", version$platform)) {
     vcRedistDownload <- "http://www.microsoft.com/download/en/details.aspx?id=8328"
     winutilsDownload <- "https://github.com/steveloughran/winutils/tree/master/hadoop-2.6.0/bin"
   }
@@ -21,9 +27,11 @@ spark_install_windows_local <- function() {
     "\n\n",
     paste("  ", winutilsDownload),
     "\n\n",
+    paste("3. Copy winutils.exe to", hadoopBinPath),
+    "\n\n",
     "3. From the command prompt application run:",
     "\n\n",
-    paste("   <path-to-file>\\winutils.exe chmod 777", hivePath),
+    paste("   <path-to-file>\\winutils.exe chmod 777", hadoopPath),
     "\n\n",
     "References: ",
     "\n\n",
