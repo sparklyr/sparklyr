3409a26d819eb6bcf0dad9370ae857ddf64c01f6
Javier Luraschi <javierluraschi@hotmail.com>
2016-06-24 14:41:12 -0700

    create default hive dir in windows

--- R/install_spark_windows.R
+++ R/install_spark_windows.R
@@ -1,3 +1,7 @@
 spark_install_windows_local <- function() {
-  
+  hivePath <- "\\tmp\\hive"
+  if (!dir.exists(hivePath)) {
+    dir.create(hivePath, recursive = TRUE)
+    message(paste("Created hive default directory under:", hivePath))
+  }
 }
