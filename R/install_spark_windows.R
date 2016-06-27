7a6a53f88764a62afb02395bc3386820a6a0c257
JJ Allaire <jj@rstudio.com>
2016-06-27 09:22:22 -0400

    use https for vcredist download link

--- R/install_spark_windows.R
+++ R/install_spark_windows.R
@@ -18,10 +18,10 @@ spark_install_windows_local <- function() {
     message(paste("Set HADOOP_HOME to:", hadoopPath))
   }
   
-  vcRedistDownload <- "http://www.microsoft.com/download/en/details.aspx?id=13523"
+  vcRedistDownload <- "https://www.microsoft.com/download/en/details.aspx?id=13523"
   winutilsDownload <- "https://code.google.com/archive/p/rrd-hadoop-win32/source/default/source"
   if (!grepl("x64", version$platform)) {
-    vcRedistDownload <- "http://www.microsoft.com/download/en/details.aspx?id=8328"
+    vcRedistDownload <- "https://www.microsoft.com/download/en/details.aspx?id=8328"
     winutilsDownload <- "https://github.com/steveloughran/winutils/tree/master/hadoop-2.6.0/bin"
   }
    
