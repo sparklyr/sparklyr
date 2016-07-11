b1c99064dde700a028ac5bf2487aaaed6d7c0c06
Kevin Ushey <kevinushey@gmail.com>
2016-07-11 13:36:17 -0700

    implement 'sdf_create_dummy_variables'

--- R/sdf_interface.R
+++ R/sdf_interface.R
@@ -1,21 +1,21 @@
 #' Copy an Object into Spark
-#' 
+#'
 #' Copy an object into Spark, and return an \R object wrapping the
 #' copied object (typically, a Spark DataFrame).
-#' 
+#'
 #' @section Advanced Usage:
-#' 
+#'
 #' \code{sdf_copy_to} is an S3 generic that, by default, dispatches to
 #' \code{sdf_import}. Package authors that would like to implement
 #' \code{sdf_copy_to} for a custom object type can accomplish this by
 #' implementing the associated method on \code{sdf_import}.
-#' 
+#'
 #' @param sc The associated Spark connection.
 #' @param x An \R object from which a Spark DataFrame can be generated.
 #' @param ... Optional arguments, passed to implementing methods.
-#' 
+#'
 #' @family Spark data frames
-#' 
+#'
 #' @name sdf_copy_to
 #' @export
 sdf_copy_to <- function(sc, x, ...) {
@@ -47,7 +47,7 @@ sdf_import.default <- function(x, sc, ...,
       optional = TRUE
     )
   }
-  
+
   # generate a CSV file from the associated data frame
   # note that these files need to live for the R session
   # duration so we don't clean these up eagerly
@@ -56,7 +56,7 @@ sdf_import.default <- function(x, sc, ...,
   hash <- digest::digest(x, algo = "sha256")
   filename <- paste("spark_csv_", hash, ".csv", sep = "")
   tempfile <- file.path(tempdir(), filename)
-  
+
   if (!file.exists(tempfile)) {
     readr::write_csv(
       x,
@@ -64,71 +64,71 @@ sdf_import.default <- function(x, sc, ...,
       col_names = TRUE
     )
   }
-  
+
   # generate path that Spark can use
   path <- normalizePath(tempfile, winslash = "/", mustWork = TRUE)
-  
+
   # generate the Spark CSV reader
   ctx <- hive_context(sc)
   reader <- invoke(ctx, "read")
-  
+
   # construct schema
   # TODO: move to separate function?
   fields <- lapply(names(x), function(name) {
-    
+
     # infer the type
     value <- x[[name]]
     type <- if (is.factor(value))
       "character"
     else
       typeof(value)
-    
+
     # create struct field
     invoke_static(
       sc,
-      
+
       "org.apache.spark.sql.api.r.SQLUtils",
       "createStructField",
-      
+
       name,
       type,
       TRUE
-      
+
     )
   })
-  
+
   schema <- invoke_static(
     sc,
-    
+
     "org.apache.spark.sql.api.r.SQLUtils",
     "createStructType",
-    
+
     fields
   )
-  
+
   # invoke CSV reader with our schema
   sdf <- reader %>%
     invoke("format", "com.databricks.spark.csv") %>%
     invoke("option", "header", "true") %>%
     invoke("schema", schema) %>%
     invoke("load", path)
-  
+
   if (cache)
     sdf <- invoke(sdf, "cache")
-  
+
   sdf_register(sdf, name)
 }
 
 #' Register a Spark DataFrame
-#' 
+#'
 #' Registers a Spark DataFrame (giving it a table name for the
 #' Spark SQL context), and returns a \code{tbl_spark}.
-#' 
+#'
 #' @template roxlate-sdf
-#' 
+#'
 #' @param x A Spark DataFrame.
 #' @param name A name to assign this table.
-#' 
+#'
 #' @family Spark data frames
 #' @export
 sdf_register <- function(x, name = NULL) {
@@ -162,16 +162,16 @@ sdf_register.spark_jobj <- function(x, name = NULL) {
 #'
 #' Partition a Spark DataFrame into multiple groups. This routine is useful
 #' for splitting a DataFrame into, for example, training and test datasets.
-#' 
+#'
 #' The sampling weights define the probability that a particular observation
 #' will be assigned to a particular partition, not the resulting size of the
 #' partition. This implies that partitioning a DataFrame with, for example,
-#' 
+#'
 #' \code{sdf_partition(x, training = 0.5, test = 0.5)}
-#' 
+#'
 #' is not guaranteed to produce \code{training} and \code{test} partitions
 #' of equal size.
-#' 
+#'
 #' @template roxlate-sdf
 #'
 #' @param x An object coercable to a Spark DataFrame.
@@ -183,7 +183,7 @@ sdf_register.spark_jobj <- function(x, name = NULL) {
 #'   this if you want your partitioning to be reproducible on repeated runs.
 #'
 #' @return An \R \code{list} of \code{tbl_spark}s.
-#' 
+#'
 #' @family Spark data frames
 #'
 #' @export
@@ -198,7 +198,7 @@ sdf_register.spark_jobj <- function(x, name = NULL) {
 #' partitions <- diamonds_tbl %>%
 #'   sdf_partition(training = 0.6, test = 0.4)
 #' print(partitions)
-#' 
+#'
 #' # alternate way of specifying weights
 #' weights <- c(training = 0.6, test = 0.4)
 #' diamonds_tbl %>% sdf_partition(weights = weights)
@@ -220,24 +220,24 @@ sdf_partition <- function(x,
 }
 
 #' Randomly Sample Rows from a Spark DataFrame
-#' 
+#'
 #' Draw a random sample of rows (with or without replacement)
 #' from a Spark DataFrame.
-#' 
+#'
 #' @template roxlate-sdf
-#' 
+#'
 #' @param x An object coercable to a Spark DataFrame.
 #' @param fraction The fraction to sample.
 #' @param replacement Boolean; sample with replacement?
 #' @param seed An (optional) integer seed.
-#' 
+#'
 #' @family Spark data frames
-#' 
+#'
 #' @export
 sdf_sample <- function(x, fraction = 1, replacement = TRUE, seed = NULL)
 {
   sdf <- spark_dataframe(x)
-  
+
   sampled <- if (is.null(seed)) {
     sdf %>%
       invoke("sample", as.logical(replacement), as.double(fraction))
@@ -245,37 +245,37 @@ sdf_sample <- function(x, fraction = 1, replacement = TRUE, seed = NULL)
     sdf %>%
       invoke("sample", as.logical(replacement), as.double(fraction), as.integer(seed))
   }
-  
+
   sdf_register(sampled)
 }
 
 #' Sort a Spark DataFrame
-#' 
+#'
 #' Sort a Spark DataFrame by one or more columns, with each column
 #' sorted in ascending order.
-#' 
+#'
 #' @template roxlate-sdf
-#' 
+#'
 #' @param x An object coercable to a Spark DataFrame.
 #' @param columns The column(s) to sort by.
-#' 
+#'
 #' @family Spark data frames
-#' 
+#'
 #' @export
 sdf_sort <- function(x, columns) {
   df <- spark_dataframe(x)
-  
+
   columns <- as.character(columns)
   n <- length(columns)
   if (n == 0)
     stop("must supply one or more column names")
-  
+
   sorted <- if (n == 1) {
     invoke(df, "sort", columns, list())
   } else {
     invoke(df, "sort", columns[[1]], as.list(columns[-1]))
   }
-  
+
   sdf_register(sorted)
 }
 
@@ -283,7 +283,7 @@ sdf_sort <- function(x, columns) {
 #'
 #' Use Spark's \href{http://spark.apache.org/docs/latest/ml-features.html}{feature transformers}
 #' to mutate a Spark DataFrame.
-#' 
+#'
 #' @template roxlate-sdf
 #'
 #' @param .data A \code{spark_tbl}.
@@ -305,10 +305,10 @@ sdf_sort <- function(x, columns) {
 #'   mutate(squared = temp ^ 2) %>%
 #'   sdf_mutate(warm = ft_binarizer(squared, 1000)) %>%
 #'   sdf_register("mutated")
-#' 
+#'
 #' # view our newly constructed tbl
 #' head(beaver_tbl)
-#' 
+#'
 #' # note that we have two separate tbls registered
 #' dplyr::src_tbls(sc)
 #' }
@@ -321,13 +321,13 @@ sdf_mutate <- function(.data, ...) {
 sdf_mutate_ <- function(.data, ..., .dots) {
   dots <- lazyeval::all_dots(.dots, ..., all_named = TRUE)
   data <- .data
-  
+
   for (i in seq_along(dots)) {
-    
+
     # extract expression to be evaluated
     lazy_expr <- dots[[i]]$expr
     lazy_env  <- dots[[i]]$env
-    
+
     # figure out the input column -- we aren't being very
     # principled about non-standard evaluation here
     el <- lazy_expr[[2]]
@@ -336,9 +336,9 @@ sdf_mutate_ <- function(.data, ..., .dots) {
     } else {
       as.character(el)
     }
-    
+
     output_col <- as.character(names(dots)[[i]])
-    
+
     # construct a new call with the input variable injected
     # for evaluation
     preamble <- list(
@@ -347,30 +347,30 @@ sdf_mutate_ <- function(.data, ..., .dots) {
       input_col,      # input column
       output_col      # output column
     )
-    
+
     call <- as.call(c(
       preamble,
       as.list(lazy_expr[-c(1, 2)])
     ))
-    
+
     # evaluate call
     data <- eval(call, envir = lazy_env)
   }
-  
+
   # return mutated dataset
   sdf_register(data)
 }
 
 #' Model Predictions with Spark DataFrames
-#' 
+#'
 #' Given a \code{ml_model} fit alongside a new data set, produce a new Spark
 #' DataFrame with predicted values encoded in the \code{"prediction"} column.
-#' 
+#'
 #' @param object,newdata An object coercable to a Spark DataFrame.
 #' @param ... Optional arguments; currently unused.
-#' 
+#'
 #' @family Spark data frames
-#' 
+#'
 #' @export
 sdf_predict <- function(object, newdata, ...) {
   if (missing(newdata) || is.null(newdata))
@@ -381,4 +381,104 @@ sdf_predict <- function(object, newdata, ...) {
   transformed <- invoke(object$.model, "transform", assembled)
   dropped <- invoke(transformed, "drop", params$features)
   sdf_register(dropped)
+}
+
+#' Create Dummy Variables
+#'
+#' Given a column in a Spark DataFrame, generate a new Spark DataFrame
+#' containing dummy variable columns.
+#'
+#' The dummy variables are generated in a similar mechanism to
+#' \code{\link{model.matrix}}, where categorical variables are expanded into a
+#' set of binary (dummy) variables. These dummy variables can be used for
+#' regression of categorical variables within the various regression routines
+#' provided by \code{sparklyr}.
+#'
+#' @section Auxiliary Information:
+#'
+#' The \code{envir} argument can be used as a mechanism for returning
+#' optional information. Currently, the following pieces are returned:
+#'
+#' \tabular{ll}{
+#' \code{levels}:\tab The set of unique values discovered within the input column.\cr
+#' \code{columns}:\tab The column names generated.\cr
+#' }
+#'
+#' If the \code{envir} argument is supplied, the names of any dummy variables
+#' generated will be included, under the \code{labels} key.
+#'
+#' @template roxlate-ml-x
+#' @param input The name of the input column.
+#' @param reference The reference label. This variable is omitted when
+#'   generating dummy variables (to avoid perfect multi-collinearity if
+#'   all dummy variables were to be used in the model fit); to generate
+#'   dummy variables for all columns this can be explicitly set as \code{NULL}.
+#' @param labels An optional \R list, mapping values in the \code{input}
+#'   column to column names to be assigned to the associated dummy variable.
+#' @param envir An optional \R environment; when provided, it will be filled
+#'   with useful auxiliary information. See \strong{Auxiliary Information} for
+#'   more information.
+#'
+#' @export
+sdf_create_dummy_variables <- function(x, input, reference = NULL, labels = list(),
+                                       envir = new.env(parent = emptyenv())) {
+  sdf <- spark_dataframe(x)
+
+  # validate inputs
+  input <- ensure_scalar_character(input)
+  reference <- if (!is.null(reference)) ensure_scalar_character(reference)
+
+  # read unique values (levels) for our categorical variable
+  uniq <- sdf %>%
+    invoke("select", input, list()) %>%
+    invoke("groupBy", input, list()) %>%
+    invoke("count") %>%
+    invoke("orderBy", "count", list()) %>%
+    sdf_collect()
+
+  # validate that 'reference' is a valid label for this column
+  levels <- uniq[[input]]
+  if (!reference %in% levels) {
+    fmt <- "no label called '%s' in column '%s'; valid labels are:\n- %s\n"
+    msg <- sprintf(fmt, reference, input, paste(shQuote(levels), collapse = ", "))
+    stop(msg, call. = FALSE)
+  }
+
+  # generate dummy variables for each non-reference level
+  # TODO: this could likely be optimized, but might require
+  # some Scala-specific code
+  tbl <- sdf_register(sdf)
+  columns <- unlist(lapply(setdiff(levels, reference), function(level) {
+
+    # generate and evaluate mutate call
+    call <- substitute(
+      mutate(tbl, name = as.numeric(column == value)),
+      list(
+        column = as.name(input),
+        value = level
+      )
+    )
+
+    # determine a name for this column -- if not supplied,
+    # we construct it by concatenating the column name with
+    # the level
+    column <- labels[[level]] %||% paste(input, level, sep = "")
+    names(call)[[3]] <- column
+
+    # evaluate call to augment our table
+    tbl <<- eval(call)
+
+    # return the generated column name
+    column
+
+  }))
+
+  # report useful information in output env
+  if (is.environment(envir)) {
+    envir$levels <- levels
+    envir$columns <- columns
+  }
+
+  # return our new table
+  tbl
 }
