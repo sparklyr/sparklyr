eada71521d825532e7fb65459bf2c41fbc99240c
Kevin Ushey <kevinushey@gmail.com>
2016-07-14 14:05:55 -0700

    implement 'names<-' method for tbl_spark

--- R/mutation.R
+++ R/mutation.R
@@ -14,3 +14,14 @@ rbind.tbl_spark <- function(..., deparse.level = 1, name = random_string("sparkl
 
   sdf_register(sdf, name = name)
 }
+
+mutate_names <- function(x, value) {
+  sdf <- spark_dataframe(x)
+  renamed <- invoke(sdf, "toDF", as.list(value))
+  sdf_register(renamed, name = as.character(x$ops$x))
+}
+
+#' @export
+`names<-.tbl_spark` <- function(x, value) {
+  mutate_names(x, value)
+}
