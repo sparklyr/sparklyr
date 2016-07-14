407184247e6cd89d32bc74f85ec0ac963832e016
Kevin Ushey <kevinushey@gmail.com>
2016-07-14 13:58:20 -0700

    implement rbind method

---
+++ R/mutation.R
@@ -0,0 +1,16 @@
+#' @export
+rbind.tbl_spark <- function(..., deparse.level = 1, name = random_string("sparklyr_tmp_"))
+{
+  dots <- list(...)
+  n <- length(dots)
+  self <- dots[[1]]
+
+  if (n == 1)
+    return(self)
+
+  sdf <- spark_dataframe(self)
+  for (i in 2:n)
+    sdf <- invoke(sdf, "unionAll", spark_dataframe(dots[[i]]))
+
+  sdf_register(sdf)
+}
