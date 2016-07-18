89d69fd0a560748e7fc531400da284d2512d39c9
Javier Luraschi <javierluraschi@hotmail.com>
2016-07-18 15:22:32 -0700

    add list of common spark-submit dependencies

--- inst/scala/backend.scala
+++ inst/scala/backend.scala
@@ -1,5 +1,21 @@
 package sparklyr
 
+import java.io.{DataOutputStream, File, FileOutputStream, IOException}
+import java.net.{InetAddress, InetSocketAddress, ServerSocket}
+import java.util.concurrent.TimeUnit
+
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.channel.{ChannelFuture, ChannelInitializer, EventLoopGroup}
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.SocketChannel
+import io.netty.channel.socket.nio.NioServerSocketChannel
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder
+import io.netty.handler.codec.bytes.{ByteArrayDecoder, ByteArrayEncoder}
+
+import org.apache.spark.api.r;
+import org.apache.spark.SparkConf
+import org.apache.spark.Logging
+
 object submit {
   def main(args: Array[String]): Unit = {
     System.err.println("Running from spark-submit")
