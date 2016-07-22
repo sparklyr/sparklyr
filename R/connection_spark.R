0e0bdb514a4ac7a7ee3c362b820d617a0071658c
Javier Luraschi <javierluraschi@hotmail.com>
2016-07-22 15:39:23 -0700

    add support for optional versions in sparkapi

--- R/connection_spark.R
+++ R/connection_spark.R
@@ -132,6 +132,8 @@ spark_connect <- function(master,
     scon <- sparkapi::start_shell(
       master = master,
       spark_home = spark_home,
+      spark_version = version,
+      hadoop_version = hadoop_version,
       app_name = app_name,
       config = config,
       jars = spark_default_jars(versionSparkHome),
