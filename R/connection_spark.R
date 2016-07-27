dd855ca1db14ee440b7205e1d3763f6e95dd02dc
Javier Luraschi <javierluraschi@hotmail.com>
2016-07-26 17:08:03 -0700

    allow using version with SPARK_HOME to fix #129

--- R/connection_spark.R
+++ R/connection_spark.R
@@ -63,7 +63,7 @@ spark_connect <- function(master,
 
   # for local mode we support SPARK_HOME via locally installed versions
   if (spark_master_is_local(master)) {
-    if (!nzchar(spark_home) || !is.null(version) || !is.null(hadoop_version)) {
+    if (!nzchar(spark_home) && (!is.null(version) || !is.null(hadoop_version))) {
       installInfo <- spark_install_find(version, hadoop_version, latest = FALSE, connecting = TRUE)
       spark_home <- installInfo$sparkVersionDir
     }
@@ -126,7 +126,7 @@ spark_connect <- function(master,
       list(paste0("--", name), shell_args[[name]])
     }))
 
-    versionSparkHome <- sparkapi::spark_version_from_home(spark_home)
+    versionSparkHome <- sparkapi::spark_version_from_home(spark_home, default = version)
 
     # start shell
     scon <- sparkapi::start_shell(
