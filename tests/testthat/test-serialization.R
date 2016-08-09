61a34b2088faf0e0a8732e5f90ebfe17b70333f6
Kevin Ushey <kevinushey@gmail.com>
2016-08-09 16:42:00 -0700

    more serialization tests

--- tests/testthat/test-serialization.R
+++ tests/testthat/test-serialization.R
@@ -1,22 +1,58 @@
 context("Serialization")
 
+ensure_round_trip <- function(sc, data) {
+  # round-trip data through Spark
+  copied <- copy_to(sc, data, overwrite = TRUE)
+  collected <- as.data.frame(collect(copied))
+
+  # compare without row.names (as we don't preserve those)
+  lhs <- data
+  row.names(lhs) <- NULL
+  rhs <- collected
+  row.names(rhs) <- NULL
+
+  expect_equal(unname(lhs), unname(rhs))
+}
+
 test_that("objects survive Spark roundtrips", {
   sc <- spark_connect(master = "local[*]", version = "1.6.1")
 
   datasets <- list(mtcars = mtcars)
 
-  for (dataset in datasets) {
-    # round-trip data through Spark
-    copied <- copy_to(sc, dataset, overwrite = TRUE)
-    collected <- as.data.frame(collect(copied))
+  for (dataset in datasets)
+    ensure_round_trip(sc, dataset)
 
-    # compare without row.names (as we don't preserve those)
-    lhs <- dataset
-    row.names(lhs) <- NULL
-    rhs <- collected
-    row.names(rhs) <- NULL
+})
 
-    expect_equal(unname(lhs), unname(rhs))
-  }
+test_that("primitive values survive Spark roundtrips", {
 
+  n <- 10
+  df <- data.frame(
+    int = as.integer(1:n),
+    dbl = as.double(1:n),
+    lgl = rep_len(c(TRUE, FALSE), n),
+    chr = letters[1:10],
+    stringsAsFactors = FALSE
+  )
+
+  sc <- spark_connect(master = "local[*]", version = "1.6.1")
+  ensure_round_trip(sc, df)
+
+})
+
+test_that("NA values survive Spark roundtrips", {
+
+  n <- 10
+  df <- data.frame(
+    int = as.integer(1:n),
+    dbl = as.double(1:n),
+    # lgl = rep_len(c(TRUE, FALSE), n), # TODO
+    # chr = letters[1:10],              # TODO
+    stringsAsFactors = FALSE
+  )
+
+  df[n / 2, ] <- NA
+
+  sc <- spark_connect(master = "local[*]", version = "1.6.1")
+  ensure_round_trip(sc, df)
 })
