03881c9b7afec3079c490901affe875bd9a9ac42
Kevin Ushey <kevinushey@gmail.com>
2016-08-10 10:37:48 -0700

    avoid caching RDD (bad for performance)

--- R/sdf_wrapper.R
+++ R/sdf_wrapper.R
@@ -46,8 +46,7 @@ sdf_read_column <- function(object, colName) {
   sc <- spark_connection(jobj)
   rdd <- jobj %>%
     invoke("select", colName, list()) %>%
-    invoke("rdd") %>%
-    invoke("cache")
+    invoke("rdd")
 
   column <- invoke_static(sc, "sparklyr.Utils", method, rdd)
 
