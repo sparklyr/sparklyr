01d11f07ac4e09bddbf1ba23af8c2a65a32febf1
Kevin Ushey <kevinushey@gmail.com>
2016-10-05 18:20:21 -0700

    test with multiple id columns

--- tests/testthat/test-reshape.R
+++ tests/testthat/test-reshape.R
@@ -5,10 +5,14 @@ sc <- testthat_spark_connection()
 test_that("'gather()' works on tbl_sparks", {
   skip_on_cran()
   skip_if_not_installed("tidyr")
+
   library(tidyr)
 
   stocks <- data_frame(
-    id   = letters[1:10],
+    id1  = letters[1:10],
+    id2  = letters[11:20],
+    id3  = LETTERS[1:10],
+    id4  = LETTERS[11:20],
     X    = rnorm(10, 0, 1),
     Y    = rnorm(10, 0, 2),
     Z    = rnorm(10, 0, 4)
@@ -19,12 +23,12 @@ test_that("'gather()' works on tbl_sparks", {
   R <-
     gather(stocks, stock, price, X:Z) %>%
     collect() %>%
-    arrange(id, stock)
+    arrange(id1, id2, id3, id4, stock)
 
   S <-
     gather(stocks_tbl, stock, price, X:Z) %>%
     collect() %>%
-    arrange(id, stock)
+    arrange(id1, id2, id3, id4, stock)
 
   # NOTE: all.equal seems to demand equivalece
   # for numeric columns here?
