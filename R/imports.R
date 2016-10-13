234a2a85abe5edc2b20ae4e9b0b732b394d90672
Kevin Ushey <kevinushey@gmail.com>
2016-10-13 16:17:02 -0700

    import and re-export copy_to (#267)

--- R/imports.R
+++ R/imports.R
@@ -4,3 +4,7 @@
 #' @importFrom tibble data_frame
 #' @importFrom stats as.formula coefficients gaussian na.fail na.omit predict quantile
 NULL
+
+#' @importFrom dplyr copy_to
+#' @export
+dplyr::copy_to
