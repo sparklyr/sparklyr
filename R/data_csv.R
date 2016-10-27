8cd638c688c70366d6c9be4432b8ac2474d07cd7
Kevin Kuo <kevinykuo@gmail.com>
2016-10-27 06:41:54 +0000

    add a check in spark_read_csv for number of columns specified

--- R/data_csv.R
+++ R/data_csv.R
@@ -5,6 +5,17 @@ spark_csv_read <- function(sc,
   read <- invoke(hive_context(sc), "read")
   options <- invoke(read, "format", "com.databricks.spark.csv")
 
+  if (!identical(columns, NULL)) {
+    ncol_ds <- options %>%
+      invoke("load", path) %>%
+      invoke("schema") %>%
+      invoke("length")
+
+    if (ncol_ds != length(columns)) {
+      warning("Dataset has ", ncol_ds, " columns but 'columns' has length ", length(columns))
+    }
+  }
+
   lapply(names(csvOptions), function(csvOptionName) {
     options <<- invoke(options, "option", csvOptionName, csvOptions[[csvOptionName]])
   })
