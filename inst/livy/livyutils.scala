4f47241e4069cb7dc5712c4138961fcd16c04bd4
Javier Luraschi <javierluraschi@hotmail.com>
2016-11-21 11:09:12 -0800

    fix to LivyUtils.invokeFromBase64

--- inst/livy/livyutils.scala
+++ inst/livy/livyutils.scala
@@ -1,10 +1,12 @@
 // Base64 requires Java 8
 import java.util.Base64
 
+import sparklyr.StreamHandler._
+
 object LivyUtils {
   def invokeFromBase64(msg: String): String = {
     val decoded: Array[Byte] = Base64.getDecoder().decode(msg)
-    val result = read(decoded)
+    val result = StreamHandler.read(decoded)
 
     Base64.getEncoder().encodeToString(result);
   }
