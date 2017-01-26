4bb40962e34b1f1ce07678fad7be48e58b89ccf6
Javier Luraschi <javierluraschi@hotmail.com>
2017-01-25 20:48:08 -0800

    add livy to new connections ui

---
+++ inst/rstudio/connections/livy.R
@@ -0,0 +1,3 @@
+library(sparklyr)
+config = livy_config(username = "${2:User}", password = "${3:Password}")
+sc <- spark_connect(master = "${1:Livy URL=https://<server>/livy/}", method = "livy", config = config)
