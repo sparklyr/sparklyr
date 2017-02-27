16c780e3bb2093d3914381b19c15279c5a8e77b5
Javier Luraschi <javierluraschi@hotmail.com>
2017-02-27 13:55:19 -0800

    remove unused spark_sql_count_rows

--- R/dplyr_sql.R
+++ R/dplyr_sql.R
@@ -1,17 +1,5 @@
 # Helper functions to support dplyr sql operations
 
-spark_sql_count_rows <- function(op, con) {
-  countQuery <- select_spark_query(
-    from = sql_build(op$x, con = con),
-    select = sql("count(*) as rowcount")
-  )
-
-  countSql <- sql_render(countQuery, con = con)
-  countResult <- dbGetQuery(con, countSql)
-
-  countResult[[1]][[1]]
-}
-
 #' @export
 sql_build.op_sample_n <- function(op, con, ...) {
   select_spark_query(
