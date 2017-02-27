98142779db7c6a71a9024df4bec0d5792bbcda5c
Javier Luraschi <javierluraschi@hotmail.com>
2017-02-27 14:46:12 -0800

    support for mutate in dplyr 0.6

--- R/dplyr_spark_query.R
+++ R/dplyr_spark_query.R
@@ -33,6 +33,7 @@ select_spark_query <- function(from,
 #' @export
 mutate_.tbl_spark <- function(.data, ..., .dots) {
   dots <- lazyeval::all_dots(.dots, ..., all_named = TRUE)
+  dots <- partial_eval(dots, vars = op_vars(.data))
 
   if (packageVersion("dplyr") > "0.5.0")
     dots <- partial_eval(dots, op_vars(.data))
