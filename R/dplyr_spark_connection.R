791e46a554ddea3823bd1b02c52ed8ee559451ad
Javier Luraschi <javierluraschi@hotmail.com>
2017-02-27 14:08:59 -0800

    remove custom sql_select added to support limit

--- R/dplyr_spark_connection.R
+++ R/dplyr_spark_connection.R
@@ -93,60 +93,6 @@ sql_translate_env.spark_connection <- function(con) {
 }
 
 #' @export
-#' @import assertthat
-sql_select.spark_connection <- function(con, select, from, where = NULL,
-                                        group_by = NULL, having = NULL,
-                                        order_by = NULL, limit = NULL,
-                                        distinct = FALSE, ...) {
-  out <- vector("list", 6)
-  names(out) <- c("select", "from", "where", "group_by", "having", "order_by")
-
-  assert_that(is.character(select), length(select) > 0L)
-  out$select <- build_sql(
-    "SELECT ",
-    if (distinct) sql("DISTINCT "),
-    escape(select, collapse = ", ", con = con)
-  )
-
-  assert_that(is.character(from), length(from) == 1L)
-  out$from <- build_sql("FROM ", from, con = con)
-
-  if (length(where) > 0L) {
-    assert_that(is.character(where))
-
-    where_paren <- escape(where, parens = TRUE, con = con)
-    out$where <- build_sql("WHERE ", dplyr::sql_vector(where_paren, collapse = " AND "))
-  }
-
-  if (length(group_by) > 0L) {
-    assert_that(is.character(group_by))
-    out$group_by <- build_sql("GROUP BY ",
-                              escape(group_by, collapse = ", ", con = con))
-  }
-
-  if (length(having) > 0L) {
-    assert_that(is.character(having))
-    out$having <- build_sql("HAVING ",
-                            escape(having, collapse = ", ", con = con))
-  }
-
-  if (length(order_by) > 0L) {
-    assert_that(is.character(order_by))
-    out$order_by <- build_sql("ORDER BY ",
-                              escape(order_by, collapse = ", ", con = con))
-  }
-
-  if (length(limit) > 0L) {
-    assert_that(is.numeric(limit))
-    out$limit <- build_sql("LIMIT ", escape(as.integer(limit), con = con))
-  }
-
-  compact <- function(x) Filter(Negate(is.null), x)
-
-  escape(unname(compact(out)), collapse = "\n", parens = FALSE, con = con)
-}
-
-#' @export
 sql_join.spark_connection <- function(con, x, y, type = "inner", by = NULL, ...) {
   # TODO: This function needs to be removed once dplyr can workaround this issue by avoiding USING statements.
 
