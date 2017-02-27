89b5300b8c36f2c9f73649c30255d8ba0f5c67ed
Javier Luraschi <javierluraschi@hotmail.com>
2017-02-27 15:26:03 -0800

    backport 6edd771 to cran

--- R/dbi_spark_table.R
+++ R/dbi_spark_table.R
@@ -53,6 +53,7 @@ setMethod("dbRemoveTable", c("spark_connection", "character"),
 #' @export
 mutate_.tbl_spark <- function(.data, ..., .dots) {
   dots <- lazyeval::all_dots(.dots, ..., all_named = TRUE)
+  dots <- partial_eval(dots, vars = op_vars(.data))
 
   data <- .data
   lapply(seq_along(dots), function(i) {
