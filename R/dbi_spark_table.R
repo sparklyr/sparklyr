5d415e67bd2feb5508914a49f770123abe439195
Javier Luraschi <javierluraschi@hotmail.com>
2017-03-08 07:26:20 -0800

    na change in dplyr will cause r cmd check note

--- R/dbi_spark_table.R
+++ R/dbi_spark_table.R
@@ -55,7 +55,7 @@ mutate_.tbl_spark <- function(.data, ..., .dots) {
   dots <- lazyeval::all_dots(.dots, ..., all_named = TRUE)
 
   if (packageVersion("dplyr") > "0.5.0")
-    dots <- partial_eval(dots, vars = op_vars(.data))
+    dots <- partial_eval(dots, op_vars(.data))
 
   data <- .data
   lapply(seq_along(dots), function(i) {
