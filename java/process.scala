05a623d467d17a4316c8281dcc6449adeaec3479
Javier Luraschi <javierluraschi@hotmail.com>
2017-04-07 08:26:43 -0700

    read r binary location from spark.r.command

---
+++ java/process.scala
@@ -0,0 +1,30 @@
+package SparkWorker
+
+import java.util.Arrays
+
+import org.apache.spark._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql._
+
+object Process {
+  def start() = {
+    val sparkConf = SparkEnv.get.conf
+    val command = sparkConf.get("spark.r.command")
+
+    val script = "" +
+      "log_file <-  file.path(\"~/spark/\", basename(tempfile(fileext = \".log\")))" +
+      "log <- function(message) {\n" +
+      "   write(log_file, message)\n" +
+      "}\n" +
+      "\n" +
+      "log(\"sparklyr worker starting\")\n" +
+      "log(\"sparklyr worker finished\")\n"
+
+    val processBuilder = new ProcessBuilder(Arrays.asList(
+      command,
+      "--vanilla",
+      script
+    ))
+  }
+}
+
