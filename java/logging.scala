d70399b51282b2a465918819685c999ee2fec199
Javier Luraschi <javierluraschi@hotmail.com>
2017-04-08 09:48:33 -0700

    use logging class from sparklyr

---
+++ java/logging.scala
@@ -0,0 +1,29 @@
+package SparkWorker
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+
+object Logging {
+  def getDate() : String = {
+    val now = Calendar.getInstance().getTime()
+    val logFormat = new SimpleDateFormat("yy/MM/dd HH:mm:ss")
+    return logFormat.format(now)
+  }
+
+  def log(message: String) = {
+    System.out.println(getDate() + " INFO sparklyr: " + message)
+  }
+
+  def logError(message: String) = {
+    System.err.println(getDate() + " ERROR sparklyr: " + message)
+  }
+
+  def logError(message: String, e: Exception) = {
+    System.err.println(getDate() + " ERROR sparklyr: " + message, e.toString)
+  }
+
+  def logWarning(message: String) = {
+    System.err.println(getDate() + " WARN sparklyr: " + message)
+  }
+}
