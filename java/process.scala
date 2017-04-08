3fead7e92f9610c306909865bf692b0d451fa583
Javier Luraschi <javierluraschi@hotmail.com>
2017-04-08 12:40:56 -0700

    write r sources to temp file and use path in rscript

--- java/process.scala
+++ java/process.scala
@@ -1,31 +1,46 @@
 package SparkWorker
 
+import java.io.{File, FileWriter}
 import java.util.Arrays
 
 import org.apache.spark._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
 
+import FileUtils._
 import Logging._
 
 object Process {
-  def start() = {
-    val sparkConf = SparkEnv.get.conf
-    val command = sparkConf.get("spark.r.command", "Rscript")
-
-    val script = "" +
+  def workerSourceFile(): String = {
+    val source = "" +
       "log_file <-  file.path(\"~\", \"spark\", basename(tempfile(fileext = \".log\")))" +
       "log <- function(message) {\n" +
-      "   write(log_file, message)\n" +
+      "   write(message, file = log_file)\n" +
+      "   cat(\"sparkrworker:\", message)" +
       "}\n" +
       "\n" +
       "log(\"sparklyr worker starting\")\n" +
       "log(\"sparklyr worker finished\")\n"
 
+    val tempFile: File = File.createTempFile("sparkrworker.R", "", createTempDir)
+    val outStream: FileWriter = new FileWriter(tempFile)
+    outStream.write(source)
+    outStream.flush()
+
+    tempFile.getAbsolutePath()
+  }
+
+  def start() = {
+    val sparkConf = SparkEnv.get.conf
+    val command: String = sparkConf.get("spark.r.command", "Rscript")
+
+    val sourceFilePath: String = workerSourceFile()
+    log("Path to source file " + sourceFilePath)
+
     val processBuilder = new ProcessBuilder(Arrays.asList(
       command,
       "--vanilla",
-      script
+      sourceFilePath
     ))
 
     log("R process starting")
