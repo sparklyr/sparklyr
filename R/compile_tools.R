b7e02bcf9e6b9998694038d58dbb34df931ee079
Javier Luraschi <javierluraschi@hotmail.com>
2017-05-05 23:37:24 -0700

    fix scalac stack overflow compiler bug that requires additional memory

--- R/compile_tools.R
+++ R/compile_tools.R
@@ -4,5 +4,10 @@ compile_sparkworker_jars <- function() {
   spec <- spec[[4]]
   spec$jar_dep <- system.file("java/sparklyr-2.1-2.11.jar", package = "sparklyr")
   update_sources_class()
+
+  javaopts <- Sys.getenv("CLASSPATH")
+  Sys.setenv(JAVA_OPTS = "-Xss256m -Xmx4096m")
+  on.exit(Sys.setenv(JAVA_OPTS = javaopts), add = TRUE)
+
   sparklyr::compile_package_jars(spec = spec)
 }
