d01d3c0514151d3825ba28035dc10ee7c2a6d527
Javier Luraschi <javierluraschi@hotmail.com>
2017-05-09 07:24:27 -0700

    get split data from backend

--- R/worker_apply.R
+++ R/worker_apply.R
@@ -1,4 +1,7 @@
 spark_worker_apply <- function(sc) {
   spark_context <- invoke_static(sc, "sparklyr.Backend", "getSparkContext")
   log("sparklyr worker retrieved context")
+
+  spark_split <- invoke_static(sc, "sparklyr.WorkerRDD", "getSplit")
+  log("sparklyr worker retrieved split")
 }
