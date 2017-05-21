3927077cfa9c0bc59b4393606139c666ec5e8eb7
Javier Luraschi <javierluraschi@hotmail.com>
2017-05-21 11:33:50 -0700

    support spark style logging from r worker

--- R/worker_log.R
+++ R/worker_log.R
@@ -1,6 +1,29 @@
-log_file <- file.path("~", "spark", basename(tempfile(fileext = ".log")))
-
-log <- function(message) {
-  write(paste0(message, "\n"), file = log_file, append = TRUE)
-  cat("sparkworker:", message, "\n")
+log_format <- function(message, level = "INFO") {
+  paste(format(Sys.time(), "%y/%m/%d %H:%M:%S"), level, "sparklyr-rworker:", message)
 }
+
+log_level <- function(..., level) {
+  args = list(...)
+  message <- paste(args, sep = "", collapse = "")
+  formatted <- log_format(message, level)
+  cat(formatted, "\n")
+}
+
+log <- function(...) {
+  log_level(..., level = "INFO")
+}
+
+log_warning<- function(...) {
+  log_level(..., level = "WARN")
+}
+
+log_error <- function(...) {
+  log_level(..., level = "ERROR")
+}
+
+unlockBinding("stop",  as.environment("package:base"))
+assign("stop", function(...) {
+  log_error(...)
+  quit()
+}, as.environment("package:base"))
+lockBinding("stop",  as.environment("package:base"))
