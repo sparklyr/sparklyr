d637d9b7ddcc785d1c9525f66b9e20bf9cc3d68e
Javier Luraschi <javierluraschi@hotmail.com>
2017-05-21 09:15:53 -0700

    make logging available to all worker modules

---
+++ R/worker_log.R
@@ -0,0 +1,6 @@
+log_file <- file.path("~", "spark", basename(tempfile(fileext = ".log")))
+
+log <- function(message) {
+  write(paste0(message, "\n"), file = log_file, append = TRUE)
+  cat("sparkworker:", message, "\n")
+}
--- R/worker_main.R
+++ R/worker_main.R
@@ -1,11 +1,4 @@
 spark_worker_main <- function(sessionId) {
-  log_file <- file.path("~", "spark", basename(tempfile(fileext = ".log")))
-
-  log <- function(message) {
-    write(paste0(message, "\n"), file = log_file, append = TRUE)
-    cat("sparkworker:", message, "\n")
-  }
-
   log("sparklyr worker starting")
 
   sc <- spark_worker_connect(sessionId)
