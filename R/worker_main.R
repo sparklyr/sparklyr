50a656555536687f81aa7ced2a01ada659f72fe3
Javier Luraschi <javierluraschi@hotmail.com>
2017-05-21 20:39:03 -0700

    catch all exceptions and call stop explicitly to trigger logging hook

--- R/worker_main.R
+++ R/worker_main.R
@@ -1,11 +1,16 @@
 spark_worker_main <- function(sessionId) {
   log("sparklyr worker starting")
 
-  sc <- spark_worker_connect(sessionId)
+  tryCatch({
 
-  log("sparklyr worker connected")
+    sc <- spark_worker_connect(sessionId)
+    log("sparklyr worker connected")
 
-  spark_worker_apply(sc)
+    spark_worker_apply(sc)
+
+  }, error = function(e) {
+      stop("Worker terminated unexpectedly: " + e)
+  })
 
   log("sparklyr worker finished")
 }
