c458504563066f504beec00fa21e575a2e807fb9
Javier Luraschi <javierluraschi@hotmail.com>
2017-05-21 20:38:13 -0700

    read sessionid from correct command line argument

--- R/compile_scala.R
+++ R/compile_scala.R
@@ -4,7 +4,7 @@ update_sources_class <- function() {
   rlines <- unlist(lapply(worker_files, function(e) readLines(e)))
   rlines <- gsub("\\\\", "\\\\\\\\", rlines)
   rlines <- gsub("\\\"", "\\\\\"", rlines)
-  rlines <- c(rlines, "spark_worker_main(commandArgs(trailingOnly = TRUE)[2])")
+  rlines <- c(rlines, "spark_worker_main(commandArgs(trailingOnly = TRUE)[1])")
 
   lines <- c(
     "package SparkWorker",
