dae0a78eacf8430f032799dd75f48c502feb26d2
Javier Luraschi <javierluraschi@hotmail.com>
2017-05-25 22:07:01 -0700

    Revert "Merge pull request #701 from kevinykuo/feature/broom"
    This reverts commit e90f93b2805351dd22d4a5a8852c7361cc0c5bbc, reversing
    changes made to 45ca7dc34a6804da02a019644e787dceae694c72.

--- R/imports.R
+++ R/imports.R
@@ -3,17 +3,4 @@
 #' @import utils
 #' @importFrom stats as.formula coefficients gaussian na.fail na.omit predict quantile
 #' @importFrom dplyr type_sum
-#' @importFrom broom fix_data_frame
 NULL
-
-#' @importFrom broom tidy
-#' @export
-broom::tidy
-
-#' @importFrom broom augment
-#' @export
-broom::augment
-
-#' @importFrom broom glance
-#' @export
-broom::glance
--- tests/testthat/helper-initialize.R
+++ tests/testthat/helper-initialize.R
@@ -46,25 +46,3 @@ test_requires <- function(...) {
 
   invisible(TRUE)
 }
-
-# helper functions for testing from broom
-# test the basics of tidy/augment/glance output: is a data frame, no row names
-check_tidiness <- function(o) {
-  expect_is(o, "data.frame")
-  expect_equal(rownames(o), as.character(seq_len(nrow(o))))
-}
-
-# check the output of a tidy function
-check_tidy <- function(o, exp.row = NULL, exp.col = NULL, exp.names = NULL) {
-  check_tidiness(o)
-
-  if (!is.null(exp.row)) {
-    expect_equal(nrow(o), exp.row)
-  }
-  if (!is.null(exp.col)) {
-    expect_equal(ncol(o), exp.col)
-  }
-  if (!is.null(exp.names)) {
-    expect_true(all(exp.names %in% colnames(o)))
-  }
-}
