c0066649aa190a2d483b5c01a2a29de6d7c20959
kevinykuo <kevinykuo@gmail.com>
2017-05-26 10:21:11 -0400

    Revert "Revert "Merge pull request #701 from kevinykuo/feature/broom""
    This reverts commit dae0a78eacf8430f032799dd75f48c502feb26d2.

---
+++ R/tidiers_utils.R
@@ -0,0 +1,33 @@
+extract_estimate_statistics <- function(object, stat_names, new_names) {
+  stat_names %>%
+    lapply(function(x) object[[x]]) %>%
+    as.data.frame() %>%
+    broom::fix_data_frame(newnames = new_names)
+}
+
+extract_model_metrics <- function(object, metric_names, new_names) {
+  sapply(metric_names, function(x) object[[x]]) %>%
+    t() %>%
+    broom::fix_data_frame(newnames = new_names)
+}
+
+#' @export
+tidy.ml_model <- function(x, ...) {
+  stop(paste0("'tidy()' not yet supported for ",
+              setdiff(class(x), "ml_model"))
+  )
+}
+
+#' @export
+augment.ml_model <- function(x, ...) {
+  stop(paste0("'augment()' not yet supported for ",
+              setdiff(class(x), "ml_model"))
+  )
+}
+
+#' @export
+glance.ml_model <- function(x, ...) {
+  stop(paste0("'glance()' not yet supported for ",
+              setdiff(class(x), "ml_model"))
+  )
+}
