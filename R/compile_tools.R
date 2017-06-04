a0b92ba9cd8de30eae9e5aedef3cd48ae7826b8c
Javier Luraschi <javierluraschi@hotmail.com>
2017-06-04 00:27:16 -0700

    make compilation spec compatible with new spec

--- R/compile_tools.R
+++ R/compile_tools.R
@@ -3,6 +3,7 @@ compile_sparkworker_jars <- function() {
   spec <- sparklyr::spark_default_compilation_spec()
   spec <- spec[[4]]
   spec$jar_dep <- system.file("java/sparklyr-2.1-2.11.jar", package = "sparklyr")
+  spec$scala_filter <- NULL
   update_sources_class()
 
   javaopts <- Sys.getenv("CLASSPATH")
