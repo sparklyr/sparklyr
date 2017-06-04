8fd36f4bc7092350a8343ab8efb39583c3ec0541
kevinykuo <kevinykuo@gmail.com>
2017-06-04 14:42:09 -0400

    adds sdf_num_partitions

--- tests/testthat/test-partitioning.R
+++ tests/testthat/test-partitioning.R
@@ -1,13 +1,12 @@
-context("repartition")
+context("partitioning")
 
 test_that("sdf_repartition works", {
   iris_tbl <- testthat_tbl("iris")
 
   expect_equal(iris_tbl %>%
                  sdf_repartition(4L) %>%
-                 sdf_query_plan() %>%
-                 dplyr::first(),
-               "Repartition 4, true")
+                 sdf_num_partitions(),
+               4L)
 
   expect_equal(iris_tbl %>%
                  sdf_repartition(columns = c("Species", "Petal_Width")) %>%
@@ -23,4 +22,9 @@ test_that("sdf_repartition works", {
                  dplyr::first() %>%
                  gsub("#[0-9]+", "", ., perl = TRUE),
                "RepartitionByExpression [Species, Petal_Width], 5")
+
+  expect_equal(iris_tbl %>%
+                 sdf_repartition(7L, columns = "Species") %>%
+                 sdf_num_partitions(),
+               7L)
 })
