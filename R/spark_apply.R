4ed766f55b9a2b574fa5768047dc56b807bdce43
Javier Luraschi <javierluraschi@hotmail.com>
2017-06-07 07:22:25 -0700

    by default cache spark_apply rdd fix #733

--- R/spark_apply.R
+++ R/spark_apply.R
@@ -29,15 +29,20 @@ spark_schema_from_rdd <- function(sc, rdd, column_names) {
 #' @param f A function that transforms a data frame partition into a data frame.
 #' @param names The column names for the transformed object, defaults to the
 #'   names from the original object.
+#' @param memory Boolean; should the table be cached into memory?
 #'
 #' @export
-spark_apply <- function(x, f, names = colnames(x)) {
+spark_apply <- function(x, f, names = colnames(x), memory = TRUE) {
   sc <- spark_connection(x)
   sdf <- spark_dataframe(x)
 
   closure <- serialize(f, NULL)
 
   rdd <- invoke_static(sc, "sparklyr.WorkerHelper", "computeRdd", sdf, closure)
+
+  # while workers need to relaunch sparklyr backends, cache by default
+  if (memory) rdd <- invoke(rdd, "cache")
+
   schema <- spark_schema_from_rdd(sc, rdd, names)
 
   transformed <- invoke(hive_context(sc), "createDataFrame", rdd, schema)
