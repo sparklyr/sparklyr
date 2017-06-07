908f5dfb0651f9342e2498e129da4054cc80bd4e
kevinykuo <kevinykuo@gmail.com>
2017-06-07 10:35:48 -0400

    sdf_partition takes numeric without L prefix for partitions arg, fixes #735

--- tests/testthat/test-partitioning.R
+++ tests/testthat/test-partitioning.R
@@ -22,9 +22,12 @@ test_that("sdf_repartition works", {
                  dplyr::first() %>%
                  gsub("#[0-9]+", "", ., perl = TRUE),
                "RepartitionByExpression [Species, Petal_Width], 5")
+})
 
+test_that("'sdf_partition' -- 'partitions' argument should take numeric (#735)", {
+  iris_tbl <- testthat_tbl("iris")
   expect_equal(iris_tbl %>%
-                 sdf_repartition(7L, columns = "Species") %>%
+                 sdf_repartition(6, columns = "Species") %>%
                  sdf_num_partitions(),
-               7L)
+               6)
 })
