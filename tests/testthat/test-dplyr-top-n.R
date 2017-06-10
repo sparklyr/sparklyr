0d7d7388c212a17032373de3a0307e872de04579
Javier Luraschi <javierluraschi@hotmail.com>
2017-06-09 23:39:39 -0700

    fix dplyt top_n test by arranging results

--- tests/testthat/test-dplyr-top-n.R
+++ tests/testthat/test-dplyr-top-n.R
@@ -16,5 +16,8 @@ test_that("top_n works as expected", {
   tn1 <- test_tbl %>% count(X) %>% top_n(10) %>% collect()
   tn2 <- test_data %>% count(X) %>% top_n(10)
 
-  expect_true(tn1 == tn2)
+  tn2 <- tn2 %>% arrange(X)
+  tn1 <- tn1 %>% arrange(X)
+
+  testthat::expect_true(all(tn1 == tn2))
 })
