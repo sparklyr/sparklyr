2c298173fa0fc658ffa838fac06349b3f4b40394
Javier Luraschi <javierluraschi@hotmail.com>
2017-06-09 23:08:53 -0700

    add dplyr top_n test

---
+++ tests/testthat/test-dplyr-top-n.R
@@ -0,0 +1,20 @@
+context("dplyr top_n")
+sc <- testthat_spark_connection()
+
+test_that("top_n works as expected", {
+  test_requires("dplyr")
+
+  test_data <- c()
+  for (i in 1:length(LETTERS))
+  {
+    test_data <- c(test_data, rep.int(LETTERS[i], times = i * 10))
+  }
+
+  test_data <- data.frame("X" = test_data)
+  test_tbl <- copy_to(sc, test_data)
+
+  tn1 <- test_tbl %>% count(X) %>% top_n(10) %>% collect()
+  tn2 <- test_data %>% count(X) %>% top_n(10)
+
+  assertthat::are_equal(tn1, tn2)
+})
