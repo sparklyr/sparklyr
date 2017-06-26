8b95c8237d386b9d99a309fc54e4527539852515
Javier Luraschi <javierluraschi@hotmail.com>
2017-06-26 16:43:57 -0700

    dplyr tests for cor, cov, sd and var

---
+++ tests/testthat/test-dplyr-stats.R
@@ -0,0 +1,60 @@
+context("dplyr stats")
+sc <- testthat_spark_connection()
+
+test_that("cor, cov, sd and var works as expected", {
+  test_requires("dplyr")
+
+  stats <- data.frame(x=1:10, y=10:1)
+  stats_tbl <- copy_to(sc, stats, overwrite = TRUE)
+
+  s1 <- stats %>% mutate(
+    cor = cor(x, y),
+    cov = cov(x, y),
+    sd = sd(x),
+    var = var(x)
+  )
+
+  s2 <- stats_tbl %>%
+    mutate(
+      cor = cor(x, y),
+      cov = cov(x, y),
+      sd = sd(x),
+      var = var(x)
+    ) %>%
+    collect() %>%
+    as.data.frame()
+
+  expect_equal(s1, s2)
+})
+
+test_that("cor, cov, sd and var works as expected over groups", {
+  test_requires("dplyr")
+
+  stats <- data.frame(id=rep(c(1,2), 5), x=1:10, y=10:1)
+  stats_tbl <- copy_to(sc, stats, overwrite = TRUE)
+
+  s1 <- stats %>%
+    group_by(id) %>%
+    mutate(
+      cor = cor(x, y),
+      cov = cov(x, y),
+      sd = sd(x),
+      var = var(x)
+    ) %>%
+    arrange(id, x, y) %>%
+    as.data.frame()
+
+  s2 <- stats_tbl %>%
+    group_by(id) %>%
+    mutate(
+      cor = cor(x, y),
+      cov = cov(x, y),
+      sd = sd(x),
+      var = var(x)
+    ) %>%
+    arrange(id, x, y) %>%
+    collect() %>%
+    as.data.frame()
+
+  expect_equal(s1, s2)
+})
