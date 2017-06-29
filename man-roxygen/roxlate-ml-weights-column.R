55db2c8d687b3e9987294f983f660e14fac32f9d
kevinykuo <kevinykuo@gmail.com>
2017-06-29 14:16:51 -0400

    add weights_column for glm

---
+++ man-roxygen/roxlate-ml-weights-column.R
@@ -0,0 +1,1 @@
+#' @param weights_column The name of the column to use as weights for the model fit.
--- tests/testthat/test-ml-generalized-linear-regression.R
+++ tests/testthat/test-ml-generalized-linear-regression.R
@@ -35,3 +35,28 @@ test_that("'ml_generalized_linear_regression' and 'glm' produce similar fits and
   expect_equal(df_r, df_s)
 
 })
+
+test_that("weights column works for glm", {
+  set.seed(42)
+  iris_weighted <- iris %>%
+    dplyr::mutate(weights = rpois(nrow(iris), 1) + 1,
+                  ones = rep(1, nrow(iris)),
+                  versicolor = ifelse(Species == "versicolor", 1L, 0L))
+  iris_weighted_tbl <- testthat_tbl("iris_weighted")
+
+  r <- glm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width,
+           family = gaussian, weights = weights, data = iris_weighted)
+  s <- ml_generalized_linear_regression(iris_weighted_tbl,
+                                        response = "Sepal_Length",
+                                        features = c("Sepal_Width", "Petal_Length", "Petal_Width"),
+                                        weights_column = "weights")
+  expect_equal(unname(coef(r)), unname(coef(s)))
+
+  r <- glm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width,
+           family = gaussian, data = iris_weighted)
+  s <- ml_generalized_linear_regression(iris_weighted_tbl,
+                                        response = "Sepal_Length",
+                                        features = c("Sepal_Width", "Petal_Length", "Petal_Width"),
+                                        weights_column = "ones")
+  expect_equal(unname(coef(r)), unname(coef(s)))
+})
