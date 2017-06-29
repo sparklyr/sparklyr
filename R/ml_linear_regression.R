e41d492b7d20a4f2473aa7234a86075d2119e9b1
kevinykuo <kevinykuo@gmail.com>
2017-06-29 14:17:06 -0400

    add weights_column for linear regression

--- R/ml_linear_regression.R
+++ R/ml_linear_regression.R
@@ -7,6 +7,7 @@
 #' @template roxlate-ml-features
 #' @template roxlate-ml-intercept
 #' @template roxlate-ml-regression-penalty
+#' @template roxlate-ml-weights-column
 #' @template roxlate-ml-iter-max
 #' @template roxlate-ml-options
 #' @template roxlate-ml-dots
@@ -20,6 +21,7 @@ ml_linear_regression <- function(x,
                                  intercept = TRUE,
                                  alpha = 0,
                                  lambda = 0,
+                                 weights_column = NULL,
                                  iter.max = 100L,
                                  ml.options = ml_options(),
                                  ...)
@@ -42,6 +44,7 @@ ml_linear_regression <- function(x,
 
   alpha <- ensure_scalar_double(alpha)
   lambda <- ensure_scalar_double(lambda)
+  weights_column <- ensure_scalar_character(weights_column, allow.null = TRUE)
   iter.max <- ensure_scalar_integer(iter.max)
   only.model <- ensure_scalar_boolean(ml.options$only.model)
 
@@ -64,6 +67,10 @@ ml_linear_regression <- function(x,
     invoke("setFitIntercept", as.logical(intercept)) %>%
     invoke("setElasticNetParam", as.double(alpha)) %>%
     invoke("setRegParam", as.double(lambda))
+
+  if (!is.null(weights_column))
+    model <- model %>%
+    invoke("setWeightCol", weights_column)
 
   if (is.function(ml.options$model.transform))
     model <- ml.options$model.transform(model)
@@ -110,6 +117,7 @@ ml_linear_regression <- function(x,
            features = features,
            response = response,
            intercept = intercept,
+           weights_column = weights_column,
            coefficients = coefficients,
            standard.errors = errors,
            t.values = tvalues,
