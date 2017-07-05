d07b6bf939346f81d1b2de7edd3251177212b6e6
Javier Luraschi <javierluraschi@hotmail.com>
2017-07-05 11:58:35 -0700

    add test to validate import of long precision decimals related to #815

--- tests/testthat/test-read-write.R
+++ tests/testthat/test-read-write.R
@@ -18,13 +18,14 @@ test_that("spark_read_csv() succeeds when column contains similar non-ascii", {
 })
 
 test_that("spark_read_json() can load data using column names", {
-  json <- file("test.json", "w+", encoding = "latin1")
+  json <- file("test.json", "w+")
   cat("{\"Sepal_Length\":5.1,\"Species\":\"setosa\", \"Other\": { \"a\": 1, \"b\": \"x\"}}\n", file = json)
   cat("{\"Sepal_Length\":4.9,\"Species\":\"setosa\", \"Other\": { \"a\": 2, \"b\": \"y\"}}\n", file = json)
   close(json)
 
   df <- spark_read_json(
-    sc,name = "iris_json_named",
+    sc,
+    name = "iris_json_named",
     path = "test.json",
     columns = c("a", "b", "c")
   )
@@ -35,13 +36,14 @@ test_that("spark_read_json() can load data using column names", {
 })
 
 test_that("spark_read_json() can load data using column types", {
-  json <- file("test.json", "w+", encoding = "latin1")
+  json <- file("test.json", "w+")
   cat("{\"Sepal_Length\":5.1,\"Species\":\"setosa\", \"Other\": { \"a\": 1, \"b\": \"x\"}}\n", file = json)
   cat("{\"Sepal_Length\":4.9,\"Species\":\"setosa\", \"Other\": { \"a\": 2, \"b\": \"y\"}}\n", file = json)
   close(json)
 
   df <- spark_read_json(
-    sc,name = "iris_json_typed",
+    sc,
+    name = "iris_json_typed",
     path = "test.json",
     columns = list("Sepal_Length" = "character", "Species" = "character", "Other" = "struct<a:integer,b:character>")
   ) %>% collect()
@@ -51,4 +53,20 @@ test_that("spark_read_json() can load data using column types", {
   expect_true(is.list(df$Other))
 
   file.remove("test.json")
+})
+
+test_that("spark_read_csv() can read long decimals", {
+  csv <- file("test.csv", "w+")
+  cat("decimal\n1\n12312312312312300000000000000", file = csv)
+  close(csv)
+
+  df <- spark_read_csv(
+    sc,
+    name = "test_big_decimals",
+    path = "test.csv"
+  )
+
+  expect_equal(nrow(df), 2)
+
+  file.remove("test.csv")
 })
