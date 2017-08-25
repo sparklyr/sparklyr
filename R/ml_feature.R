3a3f712edb9ace57eb7a52c622d744f91ad48522
kevinykuo <kevinykuo@gmail.com>
2017-08-25 15:37:51 -0400

    make stage constructor method generic

--- R/ml_feature.R
+++ R/ml_feature.R
@@ -1,5 +1,10 @@
 #' @export
-ml_tokenizer <- function(sc, input_col, output_col, name = NULL) {
+ml_tokenizer <- function(...) {
+  UseMethod("ml_tokenizer")
+}
+
+#' @export
+ml_tokenizer.spark_connection <- function(sc, input_col, output_col, name = NULL, ...) {
 
   .stage <- ml_new_transformer(sc, "org.apache.spark.ml.feature.Tokenizer",
                                input_col, output_col)
