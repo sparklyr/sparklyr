eed49ad6fb824711331e26f7ed214f381004725b
kevinykuo <kevinykuo@gmail.com>
2017-08-25 23:06:34 -0400

    modify ml_logistic_regression() params test to match specified params instead of all params

--- tests/testthat/test-ml-classification.R
+++ tests/testthat/test-ml-classification.R
@@ -5,5 +5,6 @@ sc <- testthat_spark_connection()
 test_that("ml_logistic_regression() returns params", {
   lr <- ml_logistic_regression(sc, intercept = TRUE, alpha = 0, name = "lr")
   params <- list(intercept = TRUE, alpha = 0)
-  expect_true(dplyr::setequal(lr$stages$lr$params, params))
+  params_from_object <- lr$stages$lr$params
+  expect_equal(list(), setdiff(params, params_from_object))
 })
