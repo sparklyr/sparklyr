bef41f5178fc95f80f27ea1b867f9367205c5966
kevinykuo <kevinykuo@gmail.com>
2017-08-28 10:49:08 -0400

    check class of ml_stages() output

--- tests/testthat/test-ml-pipeline.R
+++ tests/testthat/test-ml-pipeline.R
@@ -10,4 +10,5 @@ test_that("ml_stages() combines pipelines", {
   pipeline_stage_names <- names(pipeline$stages)
   individual_stage_names <- c(names(tokenizer$stages), names(binarizer$stages))
   expect_equal(pipeline_stage_names, individual_stage_names)
+  expect_equal(class(pipeline), "ml_pipeline")
 })
