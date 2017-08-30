d582da955990b054d86f50ae9553167a9d7030f3
kevinykuo <kevinykuo@gmail.com>
2017-08-30 11:45:37 -0400

    fix unested pipeline persistence

--- R/ml_pipeline.R
+++ R/ml_pipeline.R
@@ -53,56 +53,6 @@ spark_connection.ml_pipeline_stage <- function(x, ...) {
 }
 
 #' @export
-ml_save_pipeline <- function(x, path, overwrite = FALSE, ...) {
-  UseMethod("ml_save_pipeline")
-}
-
-#' @export
-ml_save_pipeline.ml_pipeline <- function(x, path, overwrite = FALSE, ...) {
-  ensure_scalar_character(path)
-  ml_writer <- x$.pipeline %>%
-    invoke("write")
-
-  if (overwrite) {
-    ml_writer %>%
-      invoke("overwrite") %>%
-      invoke("save", path)
-  } else {
-    ml_writer %>%
-      invoke("save", path)
-  }
-}
-
-#' @export
-ml_load_pipeline <- function(sc, path) {
-  jobj <- invoke_new(sc, "org.apache.spark.ml.Pipeline") %>%
-    invoke("load", path)
-  ml_pipeline(jobj)
-}
-
-#' @export
-ml_save_model <- function(x, path, overwrite = FALSE, ...) {
-  ensure_scalar_character(path)
-  ml_writer <- x$.pipeline %>%
-    invoke("write")
-
-  if (overwrite) {
-    ml_writer %>%
-      invoke("overwrite") %>%
-      invoke("save", path)
-  } else {
-    ml_writer %>%
-      invoke("save", path)
-  }
-}
-
-#' @export
-ml_load_model <- function(sc, path) {
-  jobj <- invoke_static(sc, "org.apache.spark.ml.PipelineModel", "load", path)
-  ml_pipeline(jobj)
-}
-
-#' @export
 ml_fit <- function(x, data, ...) {
   jobj <- x$.pipeline %>%
     invoke("fit", spark_dataframe(data))
---
+++ R/ml_pipeline_persistence.R
@@ -0,0 +1,49 @@
+#' @export
+ml_save_pipeline <- function(x, path, overwrite = FALSE, ...) {
+  UseMethod("ml_save_pipeline")
+}
+
+#' @export
+ml_save_pipeline.ml_pipeline <- function(x, path, overwrite = FALSE, ...) {
+  ensure_scalar_character(path)
+  ml_writer <- x$.jobj %>%
+    invoke("write")
+
+  if (overwrite) {
+    ml_writer %>%
+      invoke("overwrite") %>%
+      invoke("save", path)
+  } else {
+    ml_writer %>%
+      invoke("save", path)
+  }
+}
+
+#' @export
+ml_load_pipeline <- function(sc, path) {
+  jobj <- invoke_new(sc, "org.apache.spark.ml.Pipeline") %>%
+    invoke("load", path)
+  ml_pipeline_info(jobj)
+}
+
+#' @export
+ml_save_model <- function(x, path, overwrite = FALSE, ...) {
+  ensure_scalar_character(path)
+  ml_writer <- x$.jobj %>%
+    invoke("write")
+
+  if (overwrite) {
+    ml_writer %>%
+      invoke("overwrite") %>%
+      invoke("save", path)
+  } else {
+    ml_writer %>%
+      invoke("save", path)
+  }
+}
+
+#' @export
+ml_load_model <- function(sc, path) {
+  jobj <- invoke_static(sc, "org.apache.spark.ml.PipelineModel", "load", path)
+  ml_pipeline_info(jobj)
+}
