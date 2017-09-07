5976f46a49f1f4ac60683fdfc47484934c261a76
Javier Luraschi <javierluraschi@hotmail.com>
2017-09-07 12:27:34 -0700

    don't use factors in df for livy tests

--- tests/testthat/test-livy.R
+++ tests/testthat/test-livy.R
@@ -4,7 +4,7 @@ test_requires("dplyr")
 test_that("'copy_to()' works under Livy connections", {
   lc <- testthat_livy_connection()
 
-  df <- data.frame(a = c(1, 2), b = c("A", "B"))
+  df <- data.frame(a = c(1, 2), b = c("A", "B"), stringsAsFactors = FALSE)
   df_tbl <- copy_to(lc, df)
 
   expect_equal(df_tbl %>% collect(), df)
