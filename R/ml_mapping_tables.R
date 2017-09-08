5e29b8fd1f4f5227138857bf38a45e4e946d258a
kevinykuo <kevinykuo@gmail.com>
2017-09-08 09:19:10 -0400

    refactor validator mapping

--- R/ml_mapping_tables.R
+++ R/ml_mapping_tables.R
@@ -25,6 +25,20 @@ ml_create_mapping_tables <- function() {
                      param_mapping_s_to_r[[param_mapping_list[[x]]]] <- x
                    }))
 
+  validator_mapping_list <- list(
+    "org.apache.spark.ml.feature.HashingTF" = "ml_validator_hashing_tf",
+    "org.apache.spark.ml.classification.LogisticRegression" = "ml_validator_logistic_regression"
+  )
+
+  validator_mapping <- new.env(parent = emptyenv(),
+                               size = length(validator_mapping_list))
+
+  invisible(lapply(names(validator_mapping_list),
+                   function(x) {
+                     validator_mapping[[x]] <- validator_mapping_list[[x]]
+                   }))
+
   devtools::use_data(param_mapping_r_to_s, param_mapping_s_to_r,
+                     validator_mapping,
                      internal = TRUE, overwrite = TRUE)
 }
--- R/ml_validator_utils.R
+++ R/ml_validator_utils.R
@@ -1,6 +1,3 @@
-ml_get_stage_validator <- function(cl) {
-  if (identical(cl, "org.apache.spark.ml.feature.HashingTF"))
-    "ml_validator_hashing_tf"
-  else if (identical(cl, "org.apache.spark.ml.classification.LogisticRegression"))
-    "ml_validator_logistic_regression"
+ml_get_stage_validator <- function(x) {
+  validator_mapping[[x]] %||% stop("validator mapping failed")
 }
