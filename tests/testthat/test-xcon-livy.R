d5c5047a7c4388c6c3b7a640516bdbbf776328b1
Javier Luraschi <javierluraschi@hotmail.com>
2017-09-07 17:38:12 -0700

    couple fixes to livy tests

--- tests/testthat/test-xcon-livy.R
+++ tests/testthat/test-xcon-livy.R
@@ -1,11 +1,11 @@
 context("livy")
 test_requires("dplyr")
 
-test_that("'copy_to()' works under Livy connections", {
+test_that("'spark_version()' works under Livy connections", {
   lc <- testthat_livy_connection()
 
   version <- spark_version(lc)
-  expect_equal(version, "2.1.0")
+  expect_equal(version, numeric_version("2.1.0"))
 })
 
 test_that("'copy_to()' works under Livy connections", {
@@ -20,5 +20,5 @@ test_that("'copy_to()' works under Livy connections", {
 test_that("'livy_config()' works with extended parameters", {
   config <- livy_config(num_executors = 1)
 
-  expect_equal(config$livy.numExecutors, 1)
+  expect_equal(as.integer(config$livy.numExecutors), 1)
 })
