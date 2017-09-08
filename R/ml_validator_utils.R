ced3ccd613994fa903b8da04fdd5f94f1cc3d5c8
kevinykuo <kevinykuo@gmail.com>
2017-09-08 15:32:54 -0400

    ml_validate_args -- evaluate formals before passing to validator

--- R/ml_validator_utils.R
+++ R/ml_validator_utils.R
@@ -19,9 +19,12 @@ ml_validate_args <- function(env = rlang::caller_env(2)) {
     rlang::lang_standardise() %>%
     rlang::lang_args() %>%
     lapply(rlang::eval_tidy, env = env)
+  default_args <- Filter(Negate(rlang::is_symbol), # filter out args without defaults
+                         rlang::fn_fmls(constructor_frame$fn)) %>%
+    lapply(rlang::eval_tidy, env = env)
 
   validated_args <- rlang::invoke(
-    validator_fn, args = args, current_args = rlang::fn_fmls(constructor_frame$fn)
+    validator_fn, args = args, current_args = default_args
   )
 
   invisible(
