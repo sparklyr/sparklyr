df854b959d1de5b11a990340fcc686f719765308
Javier Luraschi <javierluraschi@hotmail.com>
2017-09-08 01:16:19 -0700

    compress test to display additional output in travis

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,14 +1,8 @@
 Sys.setenv("R_TESTS" = "")
-
 library(testthat)
 library(sparklyr)
 
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
   test_check("sparklyr")
-
-  on.exit({
-    spark_disconnect_all()
-    livy_service_stop()
-    Sys.sleep(3)
-  })
+  on.exit({ spark_disconnect_all() ; livy_service_stop() })
 }
