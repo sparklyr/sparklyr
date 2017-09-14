2729a0aa8620f9b5fab9815857a2042a3bf7ccc9
Javier Luraschi <javierluraschi@hotmail.com>
2017-09-13 19:35:35 -0700

    fix yarn-cluster test

--- tests/testthat/test-yarn-cluster.R
+++ tests/testthat/test-yarn-cluster.R
@@ -1,8 +1,13 @@
 context("yarn cluster")
 
 test_that("'spark_yarn_cluster_get_resource_manager_webapp' fails under HA configuration", {
-  Sys.setenv(YARN_CONF_DIR = dir(getwd(), recursive = TRUE, pattern = "yarn-site.xml", full.names = TRUE))
+  Sys.setenv(
+    YARN_CONF_DIR = dirname(dir(getwd(), recursive = TRUE, pattern = "yarn-site.xml", full.names = TRUE))
+  )
+
   testthat::expect_error(
     spark_yarn_cluster_get_resource_manager_webapp()
   )
+
+  Sys.unsetenv("YARN_CONF_DIR")
 })
