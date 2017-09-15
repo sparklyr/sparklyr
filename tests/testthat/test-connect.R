1f7b1d32cb499df32baf289ab370175fc5f8e69b
Javier Luraschi <javierluraschi@hotmail.com>
2017-09-15 09:05:16 -0700

    add test for session id generation

--- tests/testthat/test-connect.R
+++ tests/testthat/test-connect.R
@@ -29,3 +29,21 @@ test_that("'spark_connect' fails with bad configuration'", {
     spark_connect(master = "local", app_name = "bad_connection", config = config)
   })
 })
+
+test_that("'spark_session_id' generates different ids for different apps", {
+  expect_true(
+    spark_session_id(app_name = "foo", master = "local") !=
+    spark_session_id(app_name = "foo", master = "local")
+  )
+})
+
+test_that("'spark_session_random' generates different ids even with seeds", {
+  expect_true({
+    set.seed(10)
+    spark_session_random()
+  } != {
+    set.seed(10)
+    spark_session_random()
+  })
+})
+
