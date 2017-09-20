e6dfcf553d8e25de42ca82d9fe30c91005320465
Javier Luraschi <javierluraschi@hotmail.com>
2017-09-19 18:02:01 -0700

    add test and fix casing in invoke tests

--- java/spark-1.5.2/test.scala
+++ java/spark-1.5.2/test.scala
@@ -3,15 +3,15 @@ package sparklyr
 object Test {
   def nullary() = 0
 
-  def unary_primitive_int(i : Int) = i * i
-  def unary_Integer(i : Integer) = i * i
-  def unary_nullable_Integer(i : Integer) = Option(i) match {
+  def unaryPrimitiveInt(i : Int) = i * i
+  def unaryInteger(i : Integer) = i * i
+  def unaryNullableInteger(i : Integer) = Option(i) match {
     case None => -1
     case Some(j) => j * j
   }
 
-  def unary_seq(xs : Seq[Double]) = xs.map(x => x * x).sum
-  def unary_nullable_seq(xs : Seq[Double]) = Option(xs) match {
+  def unarySeq(xs : Seq[Double]) = xs.map(x => x * x).sum
+  def unaryNullable_seq(xs : Seq[Double]) = Option(xs) match {
     case None => -1
     case Some(ys) => ys.map(y => y * y).sum
   }
@@ -19,4 +19,8 @@ object Test {
   def infer(x: Double) = "Double"
   def infer(s: String) = "String"
   def infer(xs: Seq[Double]) = "Seq"
+
+  def roundtrip(data: Array[_]): Array[_] = data
+
+  def unaryArrayToClass(array: Array[_]): String = array.getClass.getName
 }
