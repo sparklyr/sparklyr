0fade68d326b3f84f98d5255ab4da022cfc01dd0
Javier Luraschi <javierluraschi@hotmail.com>
2017-09-22 00:18:17 -0700

    use mock for available.packages

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,8 +3,6 @@ library(testthat)
 library(sparklyr)
 
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
-  options(repos=structure(c(CRAN="https://cran.rstudio.com/")))
-
   test_check("sparklyr")
   on.exit({ spark_disconnect_all() ; livy_service_stop() })
 }
