ec5b63baf77a7c2abc702766086d5f8bfee6b56e
Javier Luraschi <javierluraschi@hotmail.com>
2017-09-21 23:39:54 -0700

    use cran mirror in test initializer

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,6 +3,8 @@ library(testthat)
 library(sparklyr)
 
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
+  options(repos=structure(c(CRAN="https://cran.rstudio.com/")))
+
   test_check("sparklyr")
   on.exit({ spark_disconnect_all() ; livy_service_stop() })
 }
