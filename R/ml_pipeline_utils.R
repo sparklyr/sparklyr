c4518fe8e4f215dc6e1c9824c37eebbd1328b9f5
kevinykuo <kevinykuo@gmail.com>
2017-09-25 15:30:49 -0400

    remove old comments

--- R/ml_pipeline_utils.R
+++ R/ml_pipeline_utils.R
@@ -42,26 +42,14 @@ ml_get_param_map <- function(jobj) {
 
 ml_new_stage_modified_args <- function(envir = rlang::caller_env(2)) {
   caller_frame <- rlang::caller_frame()
-  arg_quos <- caller_frame %>%
+  modified_args <- caller_frame %>%
     rlang::lang_standardise() %>%
     rlang::lang_args() %>%
-    lapply(rlang::new_quosure, env = envir)
-  modified_args <- rlang::modify(
-    arg_quos,
-    # x = quo(spark_connection(!!modified_args[["x"]]))
-    x = rlang::new_quosure(rlang::expr(spark_connection(x)), env = caller_frame$env)
+    lapply(rlang::new_quosure, env = envir) %>%
+    rlang::modify(
+      x = rlang::new_quosure(rlang::expr(spark_connection(x)), env = caller_frame$env)
     )
-  # %>%
-  #   lapply(rlang::eval_tidy)
-  # str(rlang::eval_tidy(quo(!!spark_connection(!!modified_args[["x"]]))))
-  # modified_args <- caller_frame %>%
-  #   rlang::lang_standardise() %>%
-  #   rlang::lang_args() %>%
-  #   rlang::modify(x = rlang::new_quosure(
-  #     rlang::parse_expr("spark_connection(x)"),
-  #     env = caller_frame$env))
   stage_constructor <- sub("\\..*$", ".spark_connection", rlang::lang_name(caller_frame))
-  # rlang::invoke(stage_constructor, modified_args)
   rlang::lang(stage_constructor, !!!modified_args) %>%
     rlang::eval_tidy()
 }
