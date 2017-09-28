b3a05ed075f04bf5a307bca35ac732ed1d134bfa
kevinykuo <kevinykuo@gmail.com>
2017-09-28 09:49:07 -0400

    remove ml_stages() (wrapper for composeStages()) which isn't used

--- R/ml_pipeline.R
+++ R/ml_pipeline.R
@@ -25,18 +25,6 @@ ml_pipeline.ml_pipeline_stage <- function(x, ..., uid = random_string("pipeline_
 }
 
 #' @export
-ml_stages <- function(x, ...) {
-  sc <- spark_connection(x$.pipeline)
-  dots <- list(...) %>%
-    lapply(function(x) x$.pipeline)
-  invoke_static(sc,
-                "sparklyr.MLUtils",
-                "composeStages",
-                x$.pipeline, dots) %>%
-    ml_pipeline()
-}
-
-#' @export
 spark_connection.ml_pipeline <- function(x, ...) {
   spark_connection(x$.jobj)
 }
