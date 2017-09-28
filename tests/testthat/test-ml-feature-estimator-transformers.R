d6224b751036c8cf34056ae287b99f581917888c
kevinykuo <kevinykuo@gmail.com>
2017-09-28 00:25:01 -0400

    move feature transformer tests into new files

---
+++ tests/testthat/test-ml-feature-estimator-transformers.R
@@ -0,0 +1,40 @@
+context("ml feature (estimators)")
+
+sc <- testthat_spark_connection()
+
+test_that("ft_count_vectorizer() works", {
+  df <- data_frame(text = c("a b c", "a a a b b c"))
+  df_tbl <- copy_to(sc, df, overwrite = TRUE)
+
+  counts <- df_tbl %>%
+    ft_tokenizer("text", "words") %>%
+    ft_count_vectorizer("words", "features") %>%
+    pull(features)
+
+  expect_identical(counts, list(c(1, 1, 1), c(3, 2, 1)))
+
+  # correct classes
+  expect_identical(class(ft_count_vectorizer(sc, "words", "features"))[1],
+                   "ml_count_vectorizer")
+
+  cv_model <- ft_count_vectorizer(sc, "words", "features") %>%
+    ml_fit(ft_tokenizer(df_tbl, "text", "words"))
+
+  expect_identical(class(cv_model)[1],
+                   "ml_count_vectorizer_model")
+
+  # vocab extraction
+  expect_identical(cv_model$vocabulary, list("a", "b", "c"))
+})
+
+test_that("ft_quantile_discretizer works", {
+  df <- data_frame(id = 0:4L,
+                   hour = c(18, 19, 8, 5, 2))
+  df_tbl <- copy_to(sc, df, overwrite = TRUE)
+
+  expect_identical(df_tbl %>%
+                     ft_quantile_discretizer("hour", "result", num_buckets = 3) %>%
+                     pull(result),
+                   c(2, 2, 1, 1, 0)
+  )
+})
