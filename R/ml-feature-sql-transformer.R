61b653693ad8924b38acbe430cd9d464e4eb077e
kevinykuo <kevinykuo@gmail.com>
2017-09-28 23:16:44 -0400

    document ft_sql_transformer() and ft_dplyr_transformer()

--- R/ml-feature-sql-transformer.R
+++ R/ml-feature-sql-transformer.R
@@ -1,5 +1,19 @@
 # SQLTransformer
 
+#' Feature Transformation -- SQLTransformer
+#'
+#' Implements the transformations which are defined by SQL statement. Currently we
+#'   only support SQL syntax like 'SELECT ... FROM __THIS__ ...' where '__THIS__' represents
+#'   the underlying table of the input dataset. The select clause specifies the
+#'   fields, constants, and expressions to display in the output, it can be any
+#'   select clause that Spark SQL supports. Users can also use Spark SQL built-in
+#'   function and UDFs to operate on these selected columns.
+#'
+#' @template roxlate-ml-feature-input-output-col
+#' @template roxlate-ml-feature-transformer
+#' @param statement A SQL statement.
+#'
+#' @rdname sql-transformer
 #' @export
 ft_sql_transformer <- function(x, statement,
                                uid = random_string("sql_transformer_"), ...) {
@@ -45,6 +59,15 @@ ft_extract_sql <- function(x) {
     (function(x) gsub(table_name, "__THIS__", x))
 }
 
+#' @rdname sql-transformer
+#'
+#' @details \code{ft_dplyr_transformer()} is a wrapper around \code{ft_sql_transformer()} that
+#'   takes a \code{tbl_spark} instead of a SQL statement. Internally, the \code{ft_dplyr_transformer()}
+#'   extracts the \code{dplyr} transformations used to generate \code{tbl} as a SQL statement
+#'   then passes it on to \code{ft_sql_transformer()}. Note that only single-table \code{dplyr} verbs
+#'   are supported and that the \code{sdf_} family of functions are not.
+#'
+#' @param tbl A \code{tbl_spark} generated using \code{dplyr} transformations.
 #' @export
 ft_dplyr_transformer <- function(
   x, tbl,
