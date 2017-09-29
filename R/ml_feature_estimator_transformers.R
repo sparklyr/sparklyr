29191f89cf93a64600cc716c5c6c4f481ffe8d8c
kevinykuo <kevinykuo@gmail.com>
2017-09-29 01:13:35 -0400

    document ft_count_vectorizer()

--- R/ml_feature_estimator_transformers.R
+++ R/ml_feature_estimator_transformers.R
@@ -183,6 +183,33 @@ ft_string_indexer.tbl_spark <- function(
 
 # CountVectorizer
 
+#' Feature Tranformation -- CountVectorizer (Estimator)
+#'
+#' Extracts a vocabulary from document collections.
+#'
+#' @template roxlate-ml-feature-input-output-col
+#' @template roxlate-ml-feature-transformer
+#' @template roxlate-ml-feature-estimator-transformer
+#'
+#' @param binary Binary toggle to control the output vector values.
+#'   If \code{TRUE}, all nonzero counts (after \code{min_tf} filter applied)
+#'   are set to 1. This is useful for discrete probabilistic models that
+#'    model binary events rather than integer counts. Default: \code{FALSE}
+#' @param min_df Specifies the minimum number of different documents a
+#'   term must appear in to be included in the vocabulary. If this is an
+#'   integer greater than or equal to 1, this specifies the number of
+#'   documents the term must appear in; if this is a double in [0,1), then
+#'   this specifies the fraction of documents. Default: 1.
+#' @param min_tf Filter to ignore rare words in a document. For each
+#'   document, terms with frequency/count less than the given threshold
+#'   are ignored. If this is an integer greater than or equal to 1, then
+#'   this specifies a count (of times the term must appear in the document);
+#'   if this is a double in [0,1), then this specifies a fraction (out of
+#'   the document's token count). Default: 1.
+#' @param vocab_size Build a vocabulary that only considers the top
+#'   \code{vocab_size} terms ordered by term frequency across the corpus.
+#'   Default: \code{2^18}.
+#'
 #' @export
 ft_count_vectorizer <- function(
   x, input_col, output_col, binary = FALSE, min_df = 1, min_tf = 1,
@@ -242,7 +269,7 @@ ft_count_vectorizer.tbl_spark <- function(
 
 # QuantileDiscretizer
 
-#' Feature Transformation -- QuantileDiscretizer
+#' Feature Transformation -- QuantileDiscretizer (Estimator)
 #'
 #' \code{ft_quantile_discretizer} takes a column with continuous features and outputs
 #'   a column with binned categorical features. The number of bins can be
