cf870e051f6d41985494328a031915c969a2b6ef
kevinykuo <kevinykuo@gmail.com>
2017-09-29 01:08:00 -0400

    document ft_quantile_discretizer()

--- R/ml_feature_estimator_transformers.R
+++ R/ml_feature_estimator_transformers.R
@@ -242,6 +242,47 @@ ft_count_vectorizer.tbl_spark <- function(
 
 # QuantileDiscretizer
 
+#' Feature Transformation -- QuantileDiscretizer
+#'
+#' \code{ft_quantile_discretizer} takes a column with continuous features and outputs
+#'   a column with binned categorical features. The number of bins can be
+#'   set using the \code{num_buckets} parameter. It is possible that the number
+#'   of buckets used will be smaller than this value, for example, if there
+#'   are too few distinct values of the input to create enough distinct
+#'   quantiles.
+#'
+#'   NaN handling: null and NaN values will be ignored from the column
+#'   during \code{QuantileDiscretizer} fitting. This will produce a \code{Bucketizer}
+#'   model for making predictions. During the transformation, \code{Bucketizer}
+#'   will raise an error when it finds NaN values in the dataset, but the
+#'   user can also choose to either keep or remove NaN values within the
+#'   dataset by setting \code{handle_invalid} If the user chooses to keep NaN values,
+#'   they will be handled specially and placed into their own bucket,
+#'   for example, if 4 buckets are used, then non-NaN data will be put
+#'   into buckets[0-3], but NaNs will be counted in a special bucket[4].
+#'
+#'   Algorithm: The bin ranges are chosen using an approximate algorithm (see
+#'   the documentation for org.apache.spark.sql.DataFrameStatFunctions.approxQuantile
+#'   \link[https://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.sql.DataFrameStatFunctions]{here} for a detailed description). The precision of the approximation can be
+#'   controlled with the \code{relative_error} parameter. The lower and upper bin
+#'   bounds will be -Infinity and +Infinity, covering all real values.
+#'
+#'   Note that the result may be different every time you run it, since the sample
+#'   strategy behind it is non-deterministic.
+#'
+#' @template roxlate-ml-feature-input-output-col
+#' @template roxlate-ml-feature-transformer
+#' @template roxlate-ml-feature-estimator-transformer
+#' @template roxlate-ml-feature-handle-invalid
+#'
+#' @param num_buckets Number of buckets (quantiles, or categories) into which data
+#'   points are grouped. Must be greater than or equal to 2.
+#' @param relative_error Relative error (see documentation for
+#'   org.apache.spark.sql.DataFrameStatFunctions.approxQuantile
+#'   \link[https://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.sql.DataFrameStatFunctions]{here}
+#'   for description). Must be in the range [0, 1]. default: 0.001
+#'
+#' @seealso \code{\link{ft_bucketizer}}
 #' @export
 ft_quantile_discretizer <- function(
   x, input_col, output_col, handle_invalid = "error",
