3ce73c18e7828d5ee674c9cf8eb3b26fe880152e
kevinykuo <kevinykuo@gmail.com>
2017-09-29 00:58:05 -0400

    document ft_hashing_tf()

--- R/ml_feature_standard_transformers.R
+++ R/ml_feature_standard_transformers.R
@@ -83,6 +83,18 @@ ft_binarizer.tbl_spark <- function(x, input_col, output_col, threshold = 0,
 
 # HashingTF
 
+#' Feature Transformation -- HashingTF
+#'
+#' Maps a sequence of terms to their term frequencies using the hashing trick.
+#'
+#' @template roxlate-ml-feature-input-output-col
+#' @template roxlate-ml-feature-transformer
+#' @param binary Binary toggle to control term frequency counts.
+#'   If true, all non-zero counts are set to 1. This is useful for discrete
+#'   probabilistic models that model binary events rather than integer
+#'   counts. (default = \code{FALSE})
+#' @param num_features Number of features. Should be greater than 0. (default = \code{2^18})
+#'
 #' @export
 ft_hashing_tf <- function(x, input_col, output_col, binary = FALSE,
                           num_features = 2^18, uid = random_string("hashing_tf_"), ...) {
