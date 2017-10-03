e44c29ac9d7dbdbab396226dd73cb570ad07e62c
kevinykuo <kevinykuo@gmail.com>
2017-10-03 17:55:02 -0400

    fix broom functions for glm type models to accomodate API changes

--- R/tidiers_utils.R
+++ R/tidiers_utils.R
@@ -1,12 +1,12 @@
 extract_estimate_statistics <- function(object, stat_names, new_names) {
   stat_names %>%
-    lapply(function(x) object[[x]]) %>%
+    lapply(function(x) ml_summary(object, x)) %>%
     as.data.frame() %>%
     broom::fix_data_frame(newnames = new_names)
 }
 
 extract_model_metrics <- function(object, metric_names, new_names) {
-  sapply(metric_names, function(x) object[[x]]) %>%
+  sapply(metric_names, function(x) ml_summary(object, x)) %>%
     t() %>%
     broom::fix_data_frame(newnames = new_names)
 }
