edc707397bd442e00b07b7fd1bf449c91342a71b
kevinykuo <kevinykuo@gmail.com>
2017-10-05 16:45:13 -0400

    fix type in ml_new_classifier()

--- R/ml_predictor.R
+++ R/ml_predictor.R
@@ -18,7 +18,7 @@ ml_new_classifier <- function(sc, class, uid,
                               raw_prediction_col, ...) {
   ensure_scalar_character(probability_col)
   ensure_scalar_character(raw_prediction_col)
-  ml_new_predictor(sc, class, id, features_col, label_col,
+  ml_new_predictor(sc, class, uid, features_col, label_col,
                    prediction_col) %>%
     invoke("setProbabilityCol", probability_col) %>%
     invoke("setRawPredictionCol", raw_prediction_col)
