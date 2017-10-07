5f98493beeae317d7d9b971ff14d9a247b8625fd
kevinykuo <kevinykuo@gmail.com>
2017-10-06 20:17:28 -0400

    implement regression evaluation and document evaluators

--- R/ml_evaluator.R
+++ R/ml_evaluator.R
@@ -1,26 +1,32 @@
-#' Spark ML - Binary Classification Evaluator
+#' Spark ML - Evaluators
 #'
-#' See the Spark ML Documentation \href{https://spark.apache.org/docs/1.6.1/api/scala/index.html#org.apache.spark.ml.evaluation.BinaryClassificationEvaluator}{BinaryClassificationEvaluator}
+#' A set of functions to calculate performance metrics for prediction models. Also see the Spark ML Documentation \href{https://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.ml.evaluation.package}{https://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.ml.evaluation.package}
 #'
 #' @param dataset a \code{tbl_spark} containing a label column and a raw prediction column. This should be the output of \code{\link{sdf_predict}} or \code{\link{ml_predict}}.
-#' @param label_col Name of column string specifying which column contains the true, indexed labels (ie 0 / 1)
-#' @param raw_prediction_col Name of column contains the scored probability of a success (ie 1)
-#' @param metric_name The classification metric - one of: areaUnderRoc (default) or areaUnderPR (not available in Spark 2.X)
+#' @param label_col Name of column string specifying which column contains the true labels or values.
+#' @param metric_name The performance metric. See details.
+#' @param prediction_col Name of the column that contains the predicted
+#'   label or value NOT the scored probability. Column should be of type
+#'   \code{Double}.
+#' @details The following metrics are supported
+#'   \itemize{
+#'    \item Binary Classification: \code{areaUnderROC} (default) or \code{areaUnderPR} (not available in Spark 2.X.)
+#'    \item Multiclass Classification: \code{f1} (default), \code{precision}, \code{recall}, \code{weightedPrecision}, \code{weightedRecall} or \code{accuracy}; for Spark 2.X: \code{f1} (default), \code{weightedPrecision}, \code{weightedRecall} or \code{accuracy}.
+#'    \item Regression: \code{rmse} (root mean squared error, default),
+#'    \code{mse} (mean squared error), \code{r2}, or \code{mae} (mean absolute error.)
+#'   }
 #'
-#' @return  area under the specified curve
+#' @return The calculated performance metric
+#' @name ml_evaluator
+NULL
+
+#' @rdname ml_evaluator
+#' @param raw_prediction_col Name of column contains the scored probability of a success
 #' @export
 ml_binary_classification_evaluator <- function(dataset, label_col, raw_prediction_col, metric_name = "areaUnderROC"){
   ml_ratify_args()
   df <- spark_dataframe(dataset)
   sc <- spark_connection(df)
-
-  # spark_metrics <- list(
-  #   "2.0" = c("areaUnderROC")
-  # )
-  #
-  # if (spark_version(sc) >= "2.0.0" && !metric %in% spark_metrics[["2.0"]]) {
-  #   stop("Metric ", metric, " is unsupported in Spark 2.X")
-  # }
 
   auc <- invoke_new(sc, "org.apache.spark.ml.evaluation.BinaryClassificationEvaluator") %>%
     invoke("setLabelCol", label_col) %>%
@@ -54,18 +60,7 @@ ml_binary_classification_eval <- ml_binary_classification_evaluator
 
 ml_validator_binary_classification_eval <- ml_validator_binary_classification_evaluator
 
-#' Spark ML - Classification Evaluator
-#'
-#' See the Spark ML Documentation \href{https://spark.apache.org/docs/1.6.1/api/scala/index.html#org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator}{MulticlassClassificationEvaluator}
-#'
-#' @param dataset A tbl_spark object that contains a columns with predicted labels
-#' @param label_col Name of the column that contains the true, indexed label. Support for binary and multi-class labels, column should be of double type (use as.double)
-#' @param prediction_col Name of the column that contains the predicted label NOT the scored probability. Support for binary and multi-class labels, column should be of double type (use as.double)
-#' @param metric_name A classification metric, for Spark 1.6: f1 (default), precision, recall, weightedPrecision, weightedRecall or accuracy;
-#'   for Spark 2.X: f1 (default), weightedPrecision, weightedRecall or accuracy
-#'
-#' @return see \code{metric}
-#'
+#' @rdname ml_evaluator
 #' @export
 ml_multiclass_classification_evaluator <- function(dataset, label_col, prediction_col, metric_name = "f1"){
 
@@ -116,3 +111,24 @@ ml_validator_multiclass_classification_evaluator <- function(args, nms) {
 ml_classification_eval <- ml_multiclass_classification_evaluator
 
 ml_validator_classification_eval <- ml_validator_multiclass_classification_evaluator
+
+#' @rdname ml_evaluator
+#' @export
+ml_regression_evaluator <- function(
+  dataset, label_col, prediction_col, metric_name = "rmse") {
+
+  label_col <- ensure_scalar_character(label_col)
+  prediction_col <- ensure_scalar_character(prediction_col)
+  metric_name <- rlang::arg_match(metric_name, c("rmse", "mse", "r2", "mae"))
+
+  df <- spark_dataframe(dataset)
+  sc <- spark_connection(df)
+
+  res <- invoke_new(sc, "org.apache.spark.ml.evaluation.RegressionEvaluator") %>%
+    invoke("setLabelCol", label_col) %>%
+    invoke("setPredictionCol", prediction_col) %>%
+    invoke("setMetricName", metric_name) %>%
+    invoke("evaluate", df)
+
+  res
+}
---
+++ tests/testthat/test-ml-evaluation.R
@@ -0,0 +1,37 @@
+context("ml evaluator")
+sc <- testthat_spark_connection()
+
+iris_tbl <- testthat_tbl("iris")
+
+test_that("basic classification evaluation works", {
+  setosa_tbl <- iris_tbl %>% mutate(Is_Setosa = ifelse(Species == "setosa", 1, 0))
+
+  ml_fit <- ml_logistic_regression(setosa_tbl, Is_Setosa ~ .)
+  ml_pred <- sdf_predict(ml_fit, setosa_tbl)
+  ml_pred <- mutate(ml_pred, actual = ifelse(Species == "setosa", 1, 0))
+  accuracy <- ml_classification_eval(ml_pred, "actual", "prediction", "accuracy")
+
+  expect_equal(accuracy, 1)
+})
+
+test_that("basic binary classification evaluation works", {
+  df <- data.frame(label = c(1, 1, 0, 0), rawPrediction = c(1, 1, 0, 0))
+  df_tbl <- dplyr::copy_to(sc, df, overwrite = TRUE)
+  auc <- ml_binary_classification_evaluator(
+    df_tbl, label_col = "label", raw_prediction_col = "rawPrediction")
+  expect_equal(auc, 1)
+})
+
+test_that("basic regression evaluation works", {
+  df <- data.frame(label = c(1.2, 4.5, 6.7),
+                   prediction = c(3, 5, 7))
+  df_tbl <- dplyr::copy_to(sc, df, overwrite = TRUE)
+  mse_r <- df %>%
+    dplyr::summarize(mse = sum((label-prediction) ^ 2) / 3) %>%
+    dplyr::pull(mse)
+
+  mse_s <- ml_regression_evaluator(
+    df_tbl, label_col = "label", prediction_col = "prediction", metric_name = "mse")
+
+  expect_equal(mse_r, mse_s)
+})
