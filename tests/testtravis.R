2d051fbcaa80e831d79703ec50d141f7894dfb34
Javier Luraschi <javierluraschi@hotmail.com>
2017-10-16 16:23:12 -0700

    source rather than duping code for travis run

--- tests/testtravis.R
+++ tests/testtravis.R
@@ -2,11 +2,4 @@ parent_dir <- dir("../", full.names = TRUE)
 sparklyr_package <- parent_dir[grepl("sparklyr_", parent_dir)]
 install.packages(sparklyr_package, repos = NULL, type = "source")
 
-Sys.setenv("R_TESTS" = "")
-library(testthat)
-library(sparklyr)
-
-if (identical(Sys.getenv("NOT_CRAN"), "true")) {
-  test_check("sparklyr")
-  on.exit({ spark_disconnect_all() ; livy_service_stop() })
-}
+source("testthat.R")
