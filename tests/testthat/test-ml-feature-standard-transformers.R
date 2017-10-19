fcc96691c117f95ef81d5fa77d9978497ea0fbf7
kevinykuo <kevinykuo@gmail.com>
2017-10-18 23:16:57 -0400

    fix type test for tokenizer

--- tests/testthat/test-ml-feature-standard-transformers.R
+++ tests/testthat/test-ml-feature-standard-transformers.R
@@ -6,7 +6,7 @@ sc <- testthat_spark_connection()
 
 test_that("We can instantiate tokenizer object", {
   tokenizer <- ft_tokenizer(sc, "x", "y", uid = "tok")
-  expect_equal(tokenizer$type, "org.apache.spark.ml.feature.Tokenizer")
+  expect_equal(jobj_class(spark_jobj(tokenizer), simple_name = FALSE)[1], "org.apache.spark.ml.feature.Tokenizer")
   expect_equal(tokenizer$uid, "tok")
   expect_equal(class(tokenizer), c("ml_transformer", "ml_pipeline_stage"))
 })
