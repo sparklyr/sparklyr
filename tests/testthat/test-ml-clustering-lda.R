157afc6360c15194427cce3523013755a684b113
kevinykuo <kevinykuo@gmail.com>
2017-11-15 15:40:27 -0500

    more tweaks to lda test

--- tests/testthat/test-ml-clustering-lda.R
+++ tests/testthat/test-ml-clustering-lda.R
@@ -7,7 +7,7 @@ test_that("ml_lda param setting", {
     x = sc, k = 9, max_iter = 11, doc_concentration = 1.2,
     topic_concentration = 1.3, subsampling_rate = 0.04,
     optimizer = "em", checkpoint_interval = 8,
-    keep_last_checkpoint = FALSE, learning_decay = 0.52,
+    learning_decay = 0.52,
     learning_offset = 1000, optimize_doc_concentration = FALSE,
     seed = 89, features_col = "fcol", topic_distribution_col = "tdcol"
   ) %>%
@@ -24,7 +24,8 @@ test_that("ml_lda() default params are correct", {
 
   args <- get_default_args(
     ml_lda,
-    c("x", "uid", "...", "doc_concentration", "topic_concentration", "seed")) %>%
+    c("x", "uid", "...", "doc_concentration", "topic_distribution_col", "seed",
+      "topic_concentration")) %>%
     param_filter_version("2.0.0", "keep_last_checkpoint")
 
   expect_equal(
