d4a6ea53b689a138bf0bb361ce86811fd38e754e
kevinykuo <kevinykuo@gmail.com>
2017-11-15 14:55:19 -0500

    multilayer perceptron backwards compat

--- tests/testthat/test-ml-classification-multilayer-perceptron.R
+++ tests/testthat/test-ml-classification-multilayer-perceptron.R
@@ -7,9 +7,12 @@ test_that("ml_multilayer_perceptron_classifier() parses params correctly", {
   args <- list(
     x = sc, features_col = "fcol", prediction_col = "pcol",
     label_col = "lcol", layers = c(20, 40, 20),
-    max_iter = 90, seed = 56, step_size = 0.01,
+    max_iter = 90, seed = 56,
     tol = 1e-05, block_size = 256, solver = "gd"
-  )
+  ) %>%
+    param_add_version("2.0.0", solver = "gd",
+                      step_size = 0.01)
+
   mlpc <- do.call(ml_multilayer_perceptron_classifier, args)
   expect_equal(ml_params(mlpc, names(args)[-1]), args[-1])
 })
@@ -21,7 +24,8 @@ test_that("ml_multilayer_perceptron() default params are correct", {
 
   args <- get_default_args(ml_multilayer_perceptron,
                            c("x", "uid", "...", "initial_weights", "seed",
-                             "layers", "response", "features"))
+                             "layers", "response", "features")) %>%
+    param_filter_version("2.0.0", c("solver", "step_size"))
 
   expect_equal(
     ml_params(predictor, names(args)),
