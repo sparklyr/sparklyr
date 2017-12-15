804e3abf02760a55edfc89d20d7df8df98c64f21
Javier Luraschi <javierluraschi@hotmail.com>
2017-12-15 10:37:41 -0800

    make new livy connection use rstudioapi::askForPassword()

--- inst/rstudio/connections/Livy.R
+++ inst/rstudio/connections/Livy.R
@@ -1,3 +1,5 @@
 library(sparklyr)
-config = livy_config(username = "${2:User}", password = "${3:Password}")
-sc <- spark_connect(master = "${1:Livy URL=http$colon$//server/livy/}", method = "livy", config = config)
+sc <- spark_connect(master = "${1:Livy URL=http$colon$//server/livy/}",
+                    method = "livy", config = livy_config(
+                      username = "${2:User}",
+                      password = rstudioapi::askForPassword("Livy password:")))
