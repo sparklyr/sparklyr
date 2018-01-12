98395d7457087c1c3bc70d7b8d834b5990bdcb30
kevinykuo <kevinykuo@gmail.com>
2018-01-12 14:23:37 -0500

    initial ft_imputer implementation

---
+++ tests/testthat/test-ml-feature-imputer.R
@@ -0,0 +1,11 @@
+context("ml feature imputer")
+
+sc <- testthat_spark_connection()
+
+test_that("ft_imputer() works properly", {
+  df <- data.frame(id = 1:5, V1 = c(1, 2, NA, 4, 5))
+  df_tbl <- sdf_copy_to(sc, df, overwrite = TRUE)
+  imputed_tbl <- df_tbl %>%
+    ft_imputer("V1", "imputed")
+  expect_equal(dplyr::pull(imputed_tbl, imputed)[[3]], 3)
+})
