2b6e9561a7adbbd11dd30bcb8674607c604fea15
Javier Luraschi <javierluraschi@hotmail.com>
2018-01-16 14:05:05 -0800

    add back support for dplyr compute by implementing db_analyze and fix #1179

--- R/dplyr_spark.R
+++ R/dplyr_spark.R
@@ -134,4 +134,14 @@ db_save_query.spark_connection <- function(con, sql, name, temporary = TRUE, ...
   # dbplyr expects db_save_query to retrieve the table name
   name
 }
+
+#' @export
+#' @importFrom dplyr db_analyze
+#' @importFrom dbplyr build_sql
+#' @importFrom DBI dbExecute
+db_analyze.spark_connection <- function(con, table, ...) {
+  info <- dbGetQuery(con, build_sql("SHOW TABLES LIKE", table, con = con))
+  if (nrow(info) > 0 && identical(info$isTemporary, FALSE)) {
+    dbExecute(con, build_sql("ANALYZE TABLE", table, "COMPUTE STATISTICS", con = con))
+  }
 }
