0a1f9ac0dc0411aa333dd0c4154427e16daff0a6
Javier Luraschi <javierluraschi@hotmail.com>
2018-01-18 08:30:17 -0800

    fix typo

--- tests/testthat/test-connect-shell.R
+++ tests/testthat/test-connect-shell.R
@@ -55,7 +55,7 @@ test_that("'spark_session_random' generates different ids even with seeds", {
 })
 
 test_that("'spark_inspect' can enumerate information from the context", {
-  skip_in_cran()
+  skip_on_cran()
 
   result <- capture.output({
     sparklyr:::spark_inspect(spark_context(sc))
