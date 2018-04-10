1989e191e22eef0c9d41eec8cdee71f2a9ea2cc1
kevinykuo <kevinykuo@gmail.com>
2018-04-10 15:46:39 -0400

    remove logistic regression printing test

--- tests/testthat/test-ml-print-methods.R
+++ tests/testthat/test-ml-print-methods.R
@@ -2,27 +2,6 @@ context("ml print methods")
 
 sc <- testthat_spark_connection()
 
-test_that("printing works for ml_model_logistic_regression", {
-  set.seed(42)
-  iris_weighted <- iris %>%
-    dplyr::mutate(weights = rpois(nrow(iris), 1) + 1,
-                  ones = rep(1, nrow(iris)),
-                  versicolor = ifelse(Species == "versicolor", 1L, 0L))
-  iris_weighted_tbl <- testthat_tbl("iris_weighted")
-
-  s <- ml_logistic_regression(
-    iris_weighted_tbl,
-    response = "versicolor",
-    features = c("Sepal_Width", "Petal_Length", "Petal_Width"))
-
-  expect_known_output(
-    s,
-    output_file("print/logreg.txt"),
-    print = TRUE
-  )
-
-})
-
 test_that("ml_tree_feature_importance() works properly", {
   if (spark_version(sc) < "2.0.0")
     skip("feature importances not available prior to spark 2.0")
