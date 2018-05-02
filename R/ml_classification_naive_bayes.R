4d7bb0c56e2e6793a8f8bd6e6ccee857afabfe91
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2018-05-02 17:00:32 -0300

    Example: ml_naive_bayes()

--- R/ml_classification_naive_bayes.R
+++ R/ml_classification_naive_bayes.R
@@ -10,6 +10,26 @@
 #'   and \code{"bernoulli"}. (default = \code{multinomial})
 #' @param smoothing The (Laplace) smoothing parameter. Defaults to 1.
 #' @param weight_col (Spark 2.1.0+) Weight column name. If this is not set or empty, we treat all instance weights as 1.0.
+#'
+#' @examples
+#' \dontrun{
+#' sc <- spark_connect(master = "local")
+#' iris_tbl <- sdf_copy_to(sc, iris, name = "iris_tbl", overwrite = TRUE)
+#'
+#' partitions <- iris_tbl %>%
+#'   sdf_partition(training = 0.7, test = 0.3, seed = 1111)
+#'
+#' iris_training <- partitions$training
+#' iris_test <- partitions$test
+#'
+#' nb_model <- iris_training %>%
+#'   ml_naive_bayes(Species ~ .)
+#'
+#' pred <- sdf_predict(iris_test, nb_model)
+#'
+#' ml_multiclass_classification_evaluator(pred)
+#' }
+#'
 #' @export
 ml_naive_bayes <- function(
   x,
