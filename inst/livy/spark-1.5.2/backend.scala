0b9072caf506526cfc59bd72f1aa3abd09d48dc6
Javier Luraschi <javierluraschi@hotmail.com>
2018-05-01 18:13:58 -0700

    rebuild jars and sources

--- inst/livy/spark-1.5.2/backend.scala
+++ inst/livy/spark-1.5.2/backend.scala
@@ -462,3 +462,10 @@ class Backend() {
     }
   }
 }
+
+object Backend {
+  /* Leaving this entry for backward compatibility with databricks */
+  def main(args: Array[String]): Unit = {
+    Shell.main(args)
+  }
+}
