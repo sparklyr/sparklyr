6a480b864ca858e4bc8ed189ad346936c5ba5873
Javier Luraschi <javierluraschi@hotmail.com>
2018-05-24 15:04:31 -0700

    use travis.r to run code coverage

--- .travis.R
+++ .travis.R
@@ -1,5 +1,15 @@
-parent_dir <- dir("../", full.names = TRUE)
-sparklyr_package <- parent_dir[grepl("sparklyr_", parent_dir)]
-install.packages(sparklyr_package, repos = NULL, type = "source")
+args <- commandArgs(trailingOnly=TRUE)
 
-source("testthat.R")
+if (length(args) == 0) {
+  stop("Missing arguments")
+} else if (args[[1]] == "--testthat") {
+  parent_dir <- dir("../", full.names = TRUE)
+  sparklyr_package <- parent_dir[grepl("sparklyr_", parent_dir)]
+  install.packages(sparklyr_package, repos = NULL, type = "source")
+
+  source("testthat.R")
+} else if (args[[1]] == "--coverage") {
+  covr::codecov()
+} else {
+  stop("Unsupported arguments")
+}
