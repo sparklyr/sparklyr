cb06b088d7c6a96a6cdc76dc5f2acc759d063c01
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2018-05-29 11:18:41 -0300

    An example using cross validation

--- R/ml_tuning.R
+++ R/ml_tuning.R
@@ -306,6 +306,41 @@ ml_sub_models <- function(model) {
 #' @rdname ml-tuning
 #' @return \code{ml_validation_metrics()} returns a data frame of performance
 #'   metrics and hyperparameter combinations.
+#' @examples
+#' \dontrun{
+#' sc <- spark_connect(master = "local")
+#' iris_tbl <- sdf_copy_to(sc, iris, name = "iris_tbl", overwrite = TRUE)
+#'
+#' # Create a pipeline
+#' pipeline <- ml_pipeline(sc) %>%
+#'   ft_r_formula(Species ~ . ) %>%
+#'   ml_random_forest_classifier()
+#'
+#' # Specify hyperparameter grid
+#' grid <- list(
+#'   random_forest = list(
+#'     num_trees = c(5,10),
+#'     max_depth = c(5,10),
+#'     impurity = c("entropy", "gini")
+#'   )
+#' )
+#'
+#' # Create the cross validator object
+#' cv <- ml_cross_validator(
+#'   sc, estimator = pipeline, estimator_param_maps = grid,
+#'   evaluator = ml_multiclass_classification_evaluator(sc),
+#'   num_folds = 3,
+#'   parallelism = 4
+#' )
+#'
+#' # Train the models
+#' cv_model <- ml_fit(cv, iris_tbl)
+#'
+#' # Print the metrics
+#' ml_validation_metrics(cv_model)
+#'
+#' }
+#'
 #' @export
 ml_validation_metrics <- function(model) {
   if (inherits(model, "ml_cross_validator_model"))
