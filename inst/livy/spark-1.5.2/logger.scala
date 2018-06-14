1defbc5b6dfcbd0090a355a18992179d647b82b4
Javier Luraschi <javierluraschi@hotmail.com>
2018-06-14 11:02:19 +0100

    implement logger for livy

--- inst/livy/spark-1.5.2/logger.scala
+++ inst/livy/spark-1.5.2/logger.scala
@@ -1,16 +1,38 @@
 class Logger(role: String, id: Int) {
+  def logLevel(level: String, message: String) = {
+    var line = ""
+    val format = new java.text.SimpleDateFormat("yy/MM/dd HH:mm:ss")
+    line += format.format(new java.util.Date())
+    line += " "
+    line += level
+    line += " sparklyr: "
+    line += role
+    line += " ("
+    line += id
+    line += ") "
+    line += message
+
+    Console.println(line)
+  }
+
   def log(message: String) = {
+    logLevel("INFO", message)
   }
 
   def log(message: String, e: Exception) = {
+    log(message + ": " + e.getMessage())
+    e.printStackTrace()
   }
 
   def logError(message: String) = {
+    logLevel("INFO", message)
   }
 
   def logError(message: String, e: Exception) = {
+    logLevel("ERROR", message)
   }
 
   def logWarning(message: String) = {
+    logLevel("WARN", message)
   }
 }
