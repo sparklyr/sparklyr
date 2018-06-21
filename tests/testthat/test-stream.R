83d8b4666baef25d5d736b306b8ac7bc5a8afa46
Javier Luraschi <javierluraschi@hotmail.com>
2018-06-21 13:14:33 +0200

    fix stream dplyr test

--- tests/testthat/test-stream.R
+++ tests/testthat/test-stream.R
@@ -4,12 +4,13 @@ sc <- testthat_spark_connection()
 
 test_that("csv stream can be filtered with dplyr", {
   if (spark_version(sc) < "2.0.0") skip("streams not supported before 2.0.0")
+  test_requires("dplyr")
 
   dir.create("iris-in")
   write.csv(iris, "iris-in/iris.csv", row.names = FALSE)
 
   stream_read_csv(sc, "stream", "iris-in") %>%
-    filter(stream._c3 == "virginica") %>%
+    filter(Species == "virginica") %>%
     stream_write_csv("iris-out")
 
   succeed()
