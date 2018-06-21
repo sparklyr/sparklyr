77b5a180eee0c1ba0232833a203768eb2d4b467b
Javier Luraschi <javierluraschi@hotmail.com>
2018-06-22 01:45:54 +0200

    add test for empty group fix

--- tests/testthat/test-spark-apply.R
+++ tests/testthat/test-spark-apply.R
@@ -193,3 +193,32 @@ test_that("'spark_apply' can roundtrip Date-Time", {
     "POSIXct"
   )
 })
+
+test_that("'spark_apply' supports grouped empty results", {
+  process_data <- function(DF, exclude) {
+    DF <- subset(DF, select = colnames(DF)[!colnames(DF) %in% exclude])
+    DF[complete.cases(DF),]
+  }
+
+  data <- data.frame(
+    grp = rep(c("A", "B", "C"), each = 5),
+    x1 = 1:15,
+    x2 = c(1:9, rep(NA, 6)),
+    stringsAsFactors = FALSE
+  )
+
+  data_spark <- sdf_copy_to(sc, data, "grp_data", memory = TRUE)
+
+  collected <- data_spark %>% spark_apply(
+    process_data,
+    group_by = "grp",
+    columns = c("x1", "x2"),
+    packages = FALSE,
+    context = {exclude <- "grp"}
+  ) %>% collect()
+
+  expect_equal(
+    collected,
+    data %>% group_by(grp) %>% do(process_data(., exclude = "grp"))
+  )
+})
