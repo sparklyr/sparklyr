acf8371940c03e4cbe5b41e73eacf9ceedec839b
Javier Luraschi <javierluraschi@hotmail.com>
2018-06-25 17:18:59 +0200

    add placeholder for spark_apply dataset mapping helper

---
+++ java/spark-1.6.0/workermap.scala
@@ -0,0 +1,24 @@
+package sparklyr
+
+import org.apache.spark._
+
+class WorkerMap() {
+
+  import org.apache.spark.sql._
+  import org.apache.spark.sql.types._
+  import org.apache.spark.sql.catalyst.encoders.RowEncoder
+
+  def mapPartitions(input: Dataset[Row]): Dataset[Row] = {
+    val schema = StructType(Seq(
+      StructField("A", IntegerType),
+      StructField("B", IntegerType),
+      StructField("C", IntegerType)
+    ))
+
+    val encoder = RowEncoder(schema)
+
+    input.mapPartitions(lines => {
+      Iterator(Row(1,2,3), Row(10,20,30))
+    })(encoder)
+  }
+}
