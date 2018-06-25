ea8c8a9f6a40be4e8e701372fb37ea7e132e092c
Javier Luraschi <javierluraschi@hotmail.com>
2018-06-25 19:22:02 +0200

    add sdf_is_streaming to check if a data frame streams data

---
+++ R/sdf_streaming.R
@@ -0,0 +1,10 @@
+#' Spark DataFrame is Streaming
+#'
+#' Is the given Spark DataFrame a streaming data?
+#'
+#' @template roxlate-ml-x
+#' @export
+sdf_is_streaming <- function(x)
+{
+  invoke(sdf, "isStreaming")
+}
