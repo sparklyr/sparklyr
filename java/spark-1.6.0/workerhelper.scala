0edefbe4474f15546d3e1e693530cf7e01e001d5
Javier Luraschi <javierluraschi@hotmail.com>
2018-06-26 10:34:25 +0200

    use worker helper to correctly map spark_apply under sdf

--- java/spark-1.6.0/workerhelper.scala
+++ java/spark-1.6.0/workerhelper.scala
@@ -51,4 +51,56 @@ object WorkerHelper {
 
     computed
   }
+
+  def computeSdf(
+    sdf: org.apache.spark.sql.DataFrame,
+    schema: StructType,
+    closure: Array[Byte],
+    config: String,
+    port: Int,
+    columns: Array[String],
+    groupBy: Array[String],
+    closureRLang: Array[Byte],
+    bundlePath: String,
+    customEnv: java.util.Map[Object, Object],
+    connectionTimeout: Int,
+    context: Array[Byte],
+    options: java.util.Map[Object, Object]
+  ): org.apache.spark.sql.DataFrame = {
+
+    var customEnvMap = scala.collection.mutable.Map[String, String]();
+    customEnv.foreach(kv => customEnvMap.put(
+      kv._1.asInstanceOf[String],
+      kv._2.asInstanceOf[String])
+    )
+
+    var optionsMap = scala.collection.mutable.Map[String, String]();
+    options.foreach(kv => optionsMap.put(
+      kv._1.asInstanceOf[String],
+      kv._2.asInstanceOf[String])
+    )
+
+    val customEnvImmMap = (Map() ++ customEnvMap).toMap
+    val optionsImmMap = (Map() ++ optionsMap).toMap
+
+    val encoder = RowEncoder(schema)
+
+    val workerApply: WorkerApply = new WorkerApply(
+      closure: Array[Byte],
+      columns: Array[String],
+      config: String,
+      port: Int,
+      groupBy: Array[String],
+      closureRLang: Array[Byte],
+      bundlePath: String,
+      customEnv: Map[String, String],
+      connectionTimeout: Int,
+      context: Array[Byte],
+      options: Map[String, String]
+    )
+
+    input.mapPartitions(lines => {
+      workerApply.apply(lines)
+    })(encoder)
+  }
 }
