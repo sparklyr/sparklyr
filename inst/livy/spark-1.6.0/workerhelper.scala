f2487d6c96141a9b5d268858d03f3d41aec8a8b6
Javier Luraschi <javierluraschi@hotmail.com>
2018-06-26 12:50:28 +0200

    rebuild jars and sources

--- inst/livy/spark-1.6.0/workerhelper.scala
+++ inst/livy/spark-1.6.0/workerhelper.scala
@@ -6,6 +6,8 @@
 object WorkerHelper {
   import org.apache.spark.rdd.RDD
   import org.apache.spark.sql._
+  import org.apache.spark.sql.catalyst.encoders.RowEncoder
+  import org.apache.spark.sql.types._
   import scala.collection.JavaConversions._
 
   def computeRdd(
@@ -53,5 +55,57 @@ object WorkerHelper {
       optionsImmMap)
 
     computed
+  }
+
+  def computeSdf(
+    sdf: org.apache.spark.sql.Dataset[org.apache.spark.sql.Row],
+    schema: StructType,
+    closure: Array[Byte],
+    config: String,
+    port: Int,
+    columns: Array[String],
+    groupBy: Array[String],
+    closureRLang: Array[Byte],
+    bundlePath: String,
+    customEnv: java.util.Map[Object, Object],
+    connectionTimeout: Int,
+    context: Array[Byte],
+    options: java.util.Map[Object, Object]
+  ): org.apache.spark.sql.Dataset[org.apache.spark.sql.Row] = {
+
+    var customEnvMap = scala.collection.mutable.Map[String, String]();
+    customEnv.foreach(kv => customEnvMap.put(
+      kv._1.asInstanceOf[String],
+      kv._2.asInstanceOf[String])
+    )
+
+    var optionsMap = scala.collection.mutable.Map[String, String]();
+    options.foreach(kv => optionsMap.put(
+      kv._1.asInstanceOf[String],
+      kv._2.asInstanceOf[String])
+    )
+
+    val customEnvImmMap = (Map() ++ customEnvMap).toMap
+    val optionsImmMap = (Map() ++ optionsMap).toMap
+
+    val encoder = RowEncoder(schema)
+
+    sdf.mapPartitions(rows => {
+      val workerApply: WorkerApply = new WorkerApply(
+        closure,
+        columns,
+        config,
+        port,
+        groupBy,
+        closureRLang,
+        bundlePath,
+        customEnvImmMap,
+        connectionTimeout,
+        context,
+        optionsImmMap
+      )
+
+      workerApply.apply(rows)
+    })(encoder)
   }
 }
--- inst/livy/spark-1.6.0/workerrdd.scala
+++ inst/livy/spark-1.6.0/workerrdd.scala
@@ -22,114 +22,24 @@ class WorkerRDD(
 
   import org.apache.spark._;
 
-  private[this] var exception: Option[Exception] = None
-  private[this] var backendPort: Int = 0
-
   override def getPartitions = firstParent.partitions
 
   override def compute(split: Partition, task: TaskContext): Iterator[org.apache.spark.sql.Row] = {
 
-    val sessionId: Int = scala.util.Random.nextInt(10000)
-    val logger = new Logger("Worker", sessionId)
-    val lock: AnyRef = new Object()
-
-    val workerContext = new WorkerContext(
-      firstParent.iterator(split, task).toArray,
-      lock,
-      closure,
-      columns,
-      groupBy,
-      closureRLang,
-      bundlePath,
-      context
+    val workerApply: WorkerApply = new WorkerApply(
+      closure: Array[Byte],
+      columns: Array[String],
+      config: String,
+      port: Int,
+      groupBy: Array[String],
+      closureRLang: Array[Byte],
+      bundlePath: String,
+      customEnv: Map[String, String],
+      connectionTimeout: Int,
+      context: Array[Byte],
+      options: Map[String, String]
     )
 
-    val tracker = new JVMObjectTracker()
-    val contextId = tracker.put(workerContext)
-    logger.log("is tracking worker context under " + contextId)
-
-    logger.log("initializing backend")
-    val backend: Backend = new Backend()
-    backend.setTracker(tracker)
-
-    /*
-     * initialize backend as worker and service, since exceptions and
-     * terminating the r session should not shutdown the process
-     */
-    backend.setType(
-      true,   /* isService */
-      false,  /* isRemote */
-      true    /* isWorker */
-    )
-
-    backend.setHostContext(
-      contextId
-    )
-
-    backend.init(
-      port,
-      sessionId,
-      connectionTimeout
-    )
-
-    backendPort = backend.getPort()
-
-    new Thread("starting backend thread") {
-      override def run(): Unit = {
-        try {
-          logger.log("starting backend")
-
-          backend.run()
-        } catch {
-          case e: Exception =>
-            logger.logError("failed while running backend: ", e)
-            exception = Some(e)
-            lock.synchronized {
-              lock.notify
-            }
-        }
-      }
-    }.start()
-
-    new Thread("starting rscript thread") {
-      override def run(): Unit = {
-        try {
-          logger.log("is starting rscript")
-
-          val rscript = new Rscript(logger)
-          rscript.init(
-            sessionId,
-            backendPort,
-            config,
-            customEnv,
-            options
-          )
-
-          lock.synchronized {
-            lock.notify
-          }
-        } catch {
-          case e: Exception =>
-            logger.logError("failed to run rscript: ", e)
-            exception = Some(e)
-            lock.synchronized {
-              lock.notify
-            }
-        }
-      }
-    }.start()
-
-    logger.log("is waiting using lock for RScript to complete")
-    lock.synchronized {
-      lock.wait()
-    }
-    logger.log("completed wait using lock for RScript")
-
-    if (exception.isDefined) {
-      throw exception.get
-    }
-
-    logger.log("is returning RDD iterator with " + workerContext.getResultArray().length + " rows")
-    return workerContext.getResultArray().iterator
+    return workerApply.apply(firstParent.iterator(split, task))
   }
 }
