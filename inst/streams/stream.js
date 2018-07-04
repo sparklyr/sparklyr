2c977d3413ecf0eb7777ea9da11615a0d99fdd0a
Javier Luraschi <javierluraschi@hotmail.com>
2018-07-04 13:03:53 -0100

    support to display row per seconds and demo mode

--- inst/streams/stream.js
+++ inst/streams/stream.js
@@ -1,4 +1,4 @@
-// !preview r2d3 data=list(sources = list("FileStreamSource[file]", "Other[file]"), sinks = list("FileSink[file]", "x"))
+// !preview r2d3 data=list(sources = list("FileStreamSource[file]", "Other[file]"), sinks = list("FileSink[file]", "x"), demo = TRUE)
 
 var barHeight = Math.ceil(height / data.length);
 
@@ -30,6 +30,14 @@ var sourceText = svg
     .attr("class", "label")
     .text(function(d) { return data.sources[data.sources.length - 1]; });
 
+var rowsPerSecondIn = svg
+  .append("text")
+    .attr("y", 60).attr("x", 24 + data.sources.length * 24)
+    .attr("class", "rps")
+    .text("0 rps");
+
+rowsPerSecondIn.append("title").text("rows per second");
+
 var sinkCircles = svg.selectAll(".sink")
   .data(data.sinks)
   .enter()
@@ -57,3 +65,33 @@ var sinkText = svg
     .attr("y", height - 25).attr("x", 24 + data.sinks.length * 24)
     .attr("class", "label sinkText")
     .text(function(d) { return data.sources[data.sinks.length - 1]; });
+
+var rowsPerSecondOut = svg
+  .append("text")
+    .attr("y", height - 50).attr("x", 24 + data.sources.length * 24)
+    .attr("class", "rps")
+    .text("0 rps");
+
+rowsPerSecondOut.append("title").text("rows per second");
+
+function updateStats(data) {
+  rowsPerSecondIn.text(data.rps.in + " rps");
+  rowsPerSecondOut.text(data.rps.out + " rps");
+}
+
+if (typeof(Shiny) !== "undefined") {
+  Shiny.addCustomMessageHandler("sparklyr_stream_view", updateStats);
+}
+
+if (data.demo) {
+  setInterval(function() {
+    var stats = {
+      rps: {
+        in: Math.floor(Math.random() * 1000),
+        out: Math.floor(Math.random() * 1000)
+      }
+    };
+
+    updateStats(stats);
+  }, 1000);
+}
