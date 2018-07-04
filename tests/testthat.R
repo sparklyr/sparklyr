565c406f5fd89cfd9dbe5748e23195601ee58859
Javier Luraschi <javierluraschi@hotmail.com>
2018-07-04 10:48:07 +0200

    investigate travis specific spark_apply test

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,6 +3,8 @@ library(testthat)
 library(sparklyr)
 
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
-  test_check("sparklyr")
+  # test_check("sparklyr")
+  testthat::test_file("testthat/test-spark-apply.R")
+
   on.exit({ spark_disconnect_all() ; livy_service_stop() })
 }
