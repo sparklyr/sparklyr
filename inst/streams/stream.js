3b939e3310364369cb1a5c6cac7f5e219cdeebdd
Javier Luraschi <javierluraschi@hotmail.com>
2018-07-04 13:03:53 -0100

    add support to resize stream rendering

--- inst/streams/stream.js
+++ inst/streams/stream.js
@@ -1,86 +1,8 @@
-// !preview r2d3 data=list(sources = list("FileStreamSource[file]", "Other[file]"), sinks = list("FileSink[file]"), demo = TRUE)
-
-var barWidth = 10;
-var margin = 20;
-var remotesCircle = 10;
-var remotesMargin = margin + remotesCircle;
-var remotesHeight = 80;
-
-var sourceCircles = svg.selectAll(".source")
-  .data(data.sources)
-  .enter()
-  .append("g")
-    .attr("class", "source")
-    .attr("transform", function(d, i) {
-      return "translate(" + (remotesMargin + i * 24) + ", " + remotesMargin + ")";
-    })
-    .append("circle");
-
-sourceCircles.attr("cx", "0").attr("cy", "0").attr("r", "10")
-  .attr("stroke", function (d, i) {
-    return (i == data.sources.length - 1) ? "orange" : "white";
-  })
-  .attr("stroke-width", "2")
-  .attr("fill", "orange")
-  .on("mouseover", function (d, i) {
-    sourceCircles.attr("stroke", "white");
-    d3.select(this).attr("stroke", "orange");
-    sourceText.text(d);
-  });
-
-var sourceText = svg
-  .append("text")
-    .attr("y", 35).attr("x", 24 + data.sources.length * 24)
-    .attr("class", "label")
-    .text(function(d) { return data.sources[data.sources.length - 1]; });
-
-var rowsPerSecondIn = svg
-  .append("text")
-    .attr("y", 60).attr("x", 24 + data.sources.length * 24)
-    .attr("class", "rps")
-    .text("0 rps");
-
-rowsPerSecondIn.append("title").text("rows per second");
-
-var sinkCircles = svg.selectAll(".sink")
-  .data(data.sinks)
-  .enter()
-  .append("g")
-    .attr("class", "sink")
-    .attr("transform", function(d, i) {
-      return "translate(" + (remotesMargin + i * 24) + ","  + (height - remotesMargin) + ")";
-    })
-    .append("circle");
-
-sinkCircles.attr("cx", "0").attr("cy", "0").attr("r", "10")
-  .attr("stroke", function (d, i) {
-    return (i == data.sinks.length - 1) ? "steelblue" : "white";
-  })
-  .attr("stroke-width", "2")
-  .attr("fill", "steelblue")
-  .on("mouseover", function (d, i) {
-    sinkCircles.attr("stroke", "white");
-    d3.select(this).attr("stroke", "steelblue");
-    sinkText.text(d);
-  });
-
-var sinkText = svg
-  .append("text")
-    .attr("y", height - 25).attr("x", 24 + data.sinks.length * 24)
-    .attr("class", "label sinkText")
-    .text(function(d) { return data.sources[data.sinks.length - 1]; });
-
-var rowsPerSecondOut = svg
-  .append("text")
-    .attr("y", height - 50).attr("x", 24 + data.sinks.length * 24)
-    .attr("class", "rps")
-    .text("0 rps");
-
-rowsPerSecondOut.append("title").text("rows per second");
+// !preview r2d3 data=list(sources = list("FileStreamSource[file]", "Other[file]"), sinks = list("FileSink[file]")), options = list(demo = TRUE)
 
 function StreamStats() {
-  var rpmIn = [];
-  var rpmOut = [];
+  var rpmIn = [0];
+  var rpmOut = [0];
 
   var maxIn = 100;
   var maxOut = 100;
@@ -108,65 +30,179 @@ function StreamStats() {
   this.maxOut = function() {
     return maxOut;
   };
-}
 
-var chartIn = svg.append("g");
-var chartOut = svg.append("g");
+  this.latestRpsIn = function() {
+    return rpmIn[0];
+  };
 
-function udpateChart(stats) {
-  var chartHeight = height - 2 * remotesHeight;
-
-  var dataIn = chartIn.selectAll('rect')
-    .data(stats.rpmIn());
-
-  dataIn
-    .enter()
-      .append('rect')
-    .merge(dataIn)
-      .attr('width', barWidth)
-      .attr('height', function(d, i) { return chartHeight / 2 * d / stats.maxIn();})
-      .attr('x', function(d, i) { return margin + i * barWidth; })
-      .attr('y', function(d, i) { return remotesHeight + chartHeight / 2 - chartHeight / 2 * d / stats.maxIn(); })
-      .attr('fill', 'orange');
-
-  var dataOut = chartOut.selectAll('rect')
-    .data(stats.rpmOut());
-
-  dataOut.
-    enter()
-      .append('rect')
-    .merge(dataOut)
-      .attr('width', barWidth)
-      .attr('height', function(d, i) { return chartHeight / 2 * d / stats.maxOut();})
-      .attr('x', function(d, i) { return margin + i * barWidth; })
-      .attr('y', function(d, i) { return remotesHeight + chartHeight / 2; })
-      .attr('fill', 'steelblue');
+  this.latestRpsOut = function() {
+    return rpmOut[0];
+  };
 }
 
 var stats = new StreamStats();
 
-function updateStats(data) {
-  stats.add(data.rps);
+function StreamRenderer(stats) {
+  var width = 0;
+  var height = 0;
 
-  rowsPerSecondIn.text(data.rps.in + " rps");
-  rowsPerSecondOut.text(data.rps.out + " rps");
+  var root;
+  var chartIn;
+  var chartOut;
+  var rowsPerSecondIn;
+  var rowsPerSecondOut;
 
-  udpateChart(stats);
+  var barWidth = 10;
+  var margin = 20;
+  var remotesCircle = 10;
+  var remotesMargin = margin + remotesCircle;
+  var remotesHeight = 80;
+
+  this.setSize = function(newWidth, newHeight) {
+    width = newWidth;
+    height =  newHeight;
+  };
+
+  this.update = function() {
+    rowsPerSecondIn.text(stats.latestRpsIn() + " rps");
+    rowsPerSecondOut.text(stats.latestRpsOut() + " rps");
+
+    var chartHeight = height - 2 * remotesHeight;
+
+    var dataIn = chartIn.selectAll('rect')
+      .data(stats.rpmIn());
+
+    dataIn
+      .enter()
+        .append('rect')
+      .merge(dataIn)
+        .attr('width', barWidth)
+        .attr('height', function(d, i) { return chartHeight / 2 * d / stats.maxIn();})
+        .attr('x', function(d, i) { return margin + i * barWidth; })
+        .attr('y', function(d, i) { return remotesHeight + chartHeight / 2 - chartHeight / 2 * d / stats.maxIn(); })
+        .attr('fill', 'orange');
+
+    var dataOut = chartOut.selectAll('rect')
+      .data(stats.rpmOut());
+
+    dataOut.
+      enter()
+        .append('rect')
+      .merge(dataOut)
+        .attr('width', barWidth)
+        .attr('height', function(d, i) { return chartHeight / 2 * d / stats.maxOut();})
+        .attr('x', function(d, i) { return margin + i * barWidth; })
+        .attr('y', function(d, i) { return remotesHeight + chartHeight / 2; })
+        .attr('fill', 'steelblue');
+  };
+
+  this.render = function() {
+    svg.selectAll("g").remove();
+    root = svg.append("g");
+
+    chartIn = root.append("g");
+    chartOut = root.append("g");
+
+    rowsPerSecondIn = root.append("text");
+    rowsPerSecondOut = root.append("text");
+
+    var sourceCircles = root.selectAll(".source")
+      .data(data.sources)
+      .enter()
+      .append("g")
+        .attr("class", "source")
+        .attr("transform", function(d, i) {
+          return "translate(" + (remotesMargin + i * 24) + ", " + remotesMargin + ")";
+        })
+        .append("circle");
+
+    sourceCircles.attr("cx", "0").attr("cy", "0").attr("r", "10")
+      .attr("stroke", function (d, i) {
+        return (i == data.sources.length - 1) ? "orange" : "white";
+      })
+      .attr("stroke-width", "2")
+      .attr("fill", "orange")
+      .on("mouseover", function (d, i) {
+        sourceCircles.attr("stroke", "white");
+        d3.select(this).attr("stroke", "orange");
+        sourceText.text(d);
+      });
+
+    var sourceText = root
+      .append("text")
+        .attr("y", 35).attr("x", 24 + data.sources.length * 24)
+        .attr("class", "label")
+        .text(function(d) { return data.sources[data.sources.length - 1]; });
+
+    rowsPerSecondIn
+      .attr("y", 60).attr("x", 24 + data.sources.length * 24)
+      .attr("class", "rps")
+      .text(stats.latestRpsIn() + " rps");
+
+    rowsPerSecondIn.append("title").text("rows per second");
+
+    var sinkCircles = root.selectAll(".sink")
+      .data(data.sinks)
+      .enter()
+      .append("g")
+        .attr("class", "sink")
+        .attr("transform", function(d, i) {
+          return "translate(" + (remotesMargin + i * 24) + ","  + (height - remotesMargin) + ")";
+        })
+        .append("circle");
+
+    sinkCircles.attr("cx", "0").attr("cy", "0").attr("r", "10")
+      .attr("stroke", function (d, i) {
+        return (i == data.sinks.length - 1) ? "steelblue" : "white";
+      })
+      .attr("stroke-width", "2")
+      .attr("fill", "steelblue")
+      .on("mouseover", function (d, i) {
+        sinkCircles.attr("stroke", "white");
+        d3.select(this).attr("stroke", "steelblue");
+        sinkText.text(d);
+      });
+
+    var sinkText = root
+      .append("text")
+        .attr("y", height - 25).attr("x", 24 + data.sinks.length * 24)
+        .attr("class", "label sinkText")
+        .text(function(d) { return data.sources[data.sinks.length - 1]; });
+
+    rowsPerSecondOut
+      .attr("y", height - 50).attr("x", 24 + data.sinks.length * 24)
+      .attr("class", "rps")
+      .text(stats.latestRpsOut() + " rps");
+
+    rowsPerSecondOut.append("title").text("rows per second");
+
+    this.update();
+  };
 }
 
-if (typeof(Shiny) !== "undefined") {
-  Shiny.addCustomMessageHandler("sparklyr_stream_view", updateStats);
-}
+var renderer = new StreamRenderer(stats);
 
-if (data.demo) {
+r2d3.onRender(function(data, svg, width, height, options) {
+  renderer.setSize(width, height);
+  renderer.render();
+});
+
+r2d3.onResize(function(width, height) {
+  renderer.setSize(width, height);
+  renderer.render();
+});
+
+if (options.demo) {
   setInterval(function() {
-    var stats = {
+    var data = {
       rps: {
         in: Math.floor(Math.random() * 100),
         out: Math.floor(Math.random() * 100)
       }
     };
 
-    updateStats(stats);
+    stats.add(data.rps);
+
+    renderer.update();
   }, 1000);
 }
