5d1b54c1777a5f9dde2aa5287ea29298e0bf6932
Javier Luraschi <javierluraschi@hotmail.com>
2018-07-05 17:25:17 +0100

    onhover support for d3 sources and sinks

--- inst/streams/stream.js
+++ inst/streams/stream.js
@@ -1,8 +1,8 @@
-// !preview r2d3 data=list(sources = list("FileStreamSource[file]"), sinks = list("FileSink[file]"))
+// !preview r2d3 data=list(sources = list("FileStreamSource[file]", "Other[file]"), sinks = list("FileSink[file]", "x"))
 
 var barHeight = Math.ceil(height / data.length);
 
-var sources = svg.selectAll(".source")
+var sourceCircles = svg.selectAll(".source")
   .data(data.sources)
   .enter()
   .append("g")
@@ -10,11 +10,17 @@ var sources = svg.selectAll(".source")
     .attr("transform", function(d, i) {
       return "translate(" + (30 + i * 24) + ", 30)";
     })
-    .append("circle")
-    .attr("cx", "0").attr("cy", "0").attr("r", "10")
-    .attr("stroke", "white")
-    .attr("stroke-width", "2")
-    .attr("fill", "orange");
+    .append("circle");
+
+sourceCircles.attr("cx", "0").attr("cy", "0").attr("r", "10")
+  .attr("stroke", "white")
+  .attr("stroke-width", "2")
+  .attr("fill", "orange")
+  .on("mouseover", function (d, i) {
+    sourceCircles.attr("stroke", "white");
+    d3.select(this).attr("stroke", "orange");
+    sourceText.text(d);
+  });
 
 var sourceText = svg
   .append("text")
@@ -22,7 +28,7 @@ var sourceText = svg
     .attr("class", "label")
     .text(function(d) { return data.sources[data.sources.length - 1]; });
 
-var sinks = svg.selectAll(".sink")
+var sinkCircles = svg.selectAll(".sink")
   .data(data.sinks)
   .enter()
   .append("g")
@@ -30,14 +36,20 @@ var sinks = svg.selectAll(".sink")
     .attr("transform", function(d, i) {
       return "translate(" + (30 + i * 24) + ","  + (height - 30) + ")";
     })
-    .append("circle")
-    .attr("cx", "0").attr("cy", "0").attr("r", "10")
-    .attr("stroke", "white")
-    .attr("stroke-width", "2")
-    .attr("fill", "steelblue");
+    .append("circle");
+
+sinkCircles.attr("cx", "0").attr("cy", "0").attr("r", "10")
+  .attr("stroke", "white")
+  .attr("stroke-width", "2")
+  .attr("fill", "steelblue")
+  .on("mouseover", function (d, i) {
+    sinkCircles.attr("stroke", "white");
+    d3.select(this).attr("stroke", "steelblue");
+    sinkText.text(d);
+  });
 
 var sinkText = svg
   .append("text")
-    .attr("y", height - 25).attr("x", 24 + data.sources.length * 24)
-    .attr("class", "label")
+    .attr("y", height - 25).attr("x", 24 + data.sinks.length * 24)
+    .attr("class", "label sinkText")
     .text(function(d) { return data.sources[data.sinks.length - 1]; });
