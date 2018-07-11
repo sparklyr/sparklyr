96a3e37419c8334aa50c86711206790a4317a610
Javier Luraschi <javierluraschi@hotmail.com>
2018-07-10 18:09:29 -0700

    do not rely on query progress for reactive polling since it's unreliable

--- R/stream_shiny.R
+++ R/stream_shiny.R
@@ -1,7 +1,7 @@
 #' Reactive spark reader
 #'
 #' Given a spark object, returns a reactive data source for the contents
-#' of the spark object. This function is most Useful to read Spark streams.
+#' of the spark object. This function is most useful to read Spark streams.
 #'
 #' @param x An object coercable to a Spark DataFrame.
 #' @param intervalMillis Approximate number of milliseconds to wait to retrieve
@@ -39,24 +39,19 @@ reactiveSpark <- function(x,
     intervalMillis = intervalMillis,
     session = session,
     checkFunc = function() {
-      progress <- invoke(stream, "lastProgress")
-      if (is.null(progress) || identical(invoke(progress, "numInputRows"), 0L)) {
-        ""
-      } else {
-        spark_session(sc) %>%
-          invoke("table", name) %>%
-          invoke(
-            "agg",
-            invoke_static(
-              sc,
-              "org.apache.spark.sql.functions",
-              "expr",
-              "max(reactive_timestamp)"
-            ),
-            list()
-          ) %>%
-          sdf_collect()
-      }
+      spark_session(sc) %>%
+        invoke("table", name) %>%
+        invoke(
+          "agg",
+          invoke_static(
+            sc,
+            "org.apache.spark.sql.functions",
+            "expr",
+            "max(reactive_timestamp)"
+          ),
+          list()
+        ) %>%
+        sdf_collect()
     },
     valueFunc = function() {
       spark_session(sc) %>%
