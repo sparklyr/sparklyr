fa4e6a8ffea4c46b93b0f86e9d02b06e8b3ebb32
Javier <javierluraschi@hotmail.com>
2018-07-19 11:39:02 -0700

    start with naive conversion using feather

---
+++ R/arrow-data.R
@@ -0,0 +1,14 @@
+arrow_copy_to <- function(sc, df, parallelism = 8L)
+{
+  # save as a feather file and reload, need to consider PR to retrieve in-memory
+  featherFile <- tempfile(fileext = ".feather")
+  feather::write_feather(df, featherFile)
+  con = file(featherFile, "rb")
+  bytes <- readBin(con, "raw", n = 1e10)
+
+  # create batches data frame
+  batches <- list(bytes)
+
+  # load arrow file in scala
+  spark_context(sc) %>% invoke("parallelize", batches, parallelism)
+}
