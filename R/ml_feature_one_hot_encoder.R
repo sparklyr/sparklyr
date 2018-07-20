e98177c1f3885f690c81f34f34f95e66aae31fb9
kevinykuo <kevinykuo@gmail.com>
2018-07-20 05:11:06 -0400

    ft_one_hot_encoder() optional params and add test

--- R/ml_feature_one_hot_encoder.R
+++ R/ml_feature_one_hot_encoder.R
@@ -11,17 +11,14 @@
 #' @param drop_last Whether to drop the last category. Defaults to \code{TRUE}.
 #'
 #' @export
-ft_one_hot_encoder <- function(
-  x, input_col, output_col, drop_last = TRUE,
-  uid = random_string("one_hot_encoder_"), ...) {
+ft_one_hot_encoder <- function(x, input_col = NULL, output_col = NULL,
+                               drop_last = TRUE, uid = random_string("one_hot_encoder_"), ...) {
   UseMethod("ft_one_hot_encoder")
 }
 
 #' @export
-ft_one_hot_encoder.spark_connection <- function(
-  x, input_col, output_col, drop_last = TRUE,
-  uid = random_string("one_hot_encoder_"), ...) {
-
+ft_one_hot_encoder.spark_connection <- function(x, input_col = NULL, output_col = NULL,
+                                                drop_last = TRUE, uid = random_string("one_hot_encoder_"), ...) {
   .args <- list(
     input_col = input_col,
     output_col = output_col,
@@ -33,17 +30,16 @@ ft_one_hot_encoder.spark_connection <- function(
 
   jobj <- ml_new_transformer(
     x, "org.apache.spark.ml.feature.OneHotEncoder",
-    .args[["input_col"]], .args[["output_col"]], .args[["uid"]]) %>%
+    input_col = .args[["input_col"]], output_col = .args[["output_col"]], uid = .args[["uid"]]
+  ) %>%
     invoke("setDropLast", .args[["drop_last"]])
 
   new_ml_one_hot_encoder(jobj)
 }
 
 #' @export
-ft_one_hot_encoder.ml_pipeline <- function(
-  x, input_col, output_col, drop_last = TRUE,
-  uid = random_string("one_hot_encoder_"), ...) {
-
+ft_one_hot_encoder.ml_pipeline <- function(x, input_col = NULL, output_col = NULL,
+                                           drop_last = TRUE, uid = random_string("one_hot_encoder_"), ...) {
   stage <- ft_one_hot_encoder.spark_connection(
     x = spark_connection(x),
     input_col = input_col,
@@ -56,10 +52,8 @@ ft_one_hot_encoder.ml_pipeline <- function(
 }
 
 #' @export
-ft_one_hot_encoder.tbl_spark <- function(
-  x, input_col, output_col, drop_last = TRUE,
-  uid = random_string("one_hot_encoder_"), ...) {
-
+ft_one_hot_encoder.tbl_spark <- function(x, input_col = NULL, output_col = NULL,
+                                         drop_last = TRUE, uid = random_string("one_hot_encoder_"), ...) {
   stage <- ft_one_hot_encoder.spark_connection(
     x = spark_connection(x),
     input_col = input_col,
@@ -78,6 +72,6 @@ new_ml_one_hot_encoder <- function(jobj) {
 ml_validator_one_hot_encoder <- function(.args) {
   .args <- validate_args_transformer(.args) %>%
     ml_backwards_compatibility(list(drop.last = "drop_last"))
-  .args[["drop_last"]] <- forge::cast_scalar_boolean(.args[["drop_last"]])
+  .args[["drop_last"]] <- forge::cast_scalar_logical(.args[["drop_last"]])
   .args
 }
