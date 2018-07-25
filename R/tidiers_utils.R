e347bd81adcf5c252edbdc65c0ca15097d3b25b3
kevinykuo <kevinykuo@gmail.com>
2018-07-25 02:11:36 -0400

    remove extract_estimate_statistics()

--- R/tidiers_utils.R
+++ R/tidiers_utils.R
@@ -1,10 +1,3 @@
-extract_estimate_statistics <- function(object, stat_names, new_names) {
-  stat_names %>%
-    lapply(function(x) ml_summary(object, x, allow_null = TRUE)) %>%
-    as.data.frame() %>%
-    broom::fix_data_frame(newnames = new_names)
-}
-
 extract_model_metrics <- function(object, metric_names, new_names) {
   sapply(metric_names, function(x) ml_summary(object, x, allow_null = TRUE)) %>%
     t() %>%
