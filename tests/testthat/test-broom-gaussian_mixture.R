e8562e7ebcb13357d98ec02b2d6524f1c48ebcbe
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2018-08-17 22:35:17 -0300

    test gaussian_mixture (augment)

--- tests/testthat/test-broom-gaussian_mixture.R
+++ tests/testthat/test-broom-gaussian_mixture.R
@@ -14,3 +14,25 @@ test_that("gaussian_mixture.tidy() works", {
   expect_equal(td1$size, c(3, 14, 4, 11))
 
 })
+
+test_that("gaussian_mixture.augment() works", {
+  test_requires_version("2.0.0")
+  sc <- testthat_spark_connection()
+  mtcars_tbl <- testthat_tbl("mtcars")
+
+  au1 <- mtcars_tbl %>%
+    ml_gaussian_mixture(~ mpg + cyl, k = 4L, seed = 123) %>%
+    augment() %>%
+    dplyr::collect()
+
+  check_tidy(au1, exp.row = nrow(mtcars),
+             exp.name = c(names(mtcars), ".cluster"))
+
+  au2 <- mtcars_tbl %>%
+    ml_gaussian_mixture(~ mpg + cyl, k = 4L, seed = 123) %>%
+    augment(newdata = head(mtcars_tbl, 25)) %>%
+    dplyr::collect()
+
+  check_tidy(au2, exp.row = 25,
+             exp.name = c(names(mtcars), ".cluster"))
+})
