fb62fc263d122053eda69482b24e65ea11eebea9
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2018-08-17 22:22:33 -0300

    test bisecting_kmeans (augment)

--- tests/testthat/test-broom-bisecting_kmeans.R
+++ tests/testthat/test-broom-bisecting_kmeans.R
@@ -14,3 +14,25 @@ test_that("bisecting_kmeans.tidy() works", {
   expect_equal(td1$size, c(11, 9, 7, 5))
 
 })
+
+test_that("bisecting_kmeans.augment() works", {
+  test_requires_version("2.0.0")
+  sc <- testthat_spark_connection()
+  mtcars_tbl <- testthat_tbl("mtcars")
+
+  au1 <- mtcars_tbl %>%
+    ml_bisecting_kmeans(~ mpg + cyl, k = 4L, seed = 123) %>%
+    augment() %>%
+    dplyr::collect()
+
+  check_tidy(au1, exp.row = nrow(mtcars),
+             exp.name = c(names(mtcars), ".cluster"))
+
+  au2 <- mtcars_tbl %>%
+    ml_bisecting_kmeans(~ mpg + cyl, k = 4L, seed = 123) %>%
+    augment(newdata = head(mtcars_tbl, 25)) %>%
+    dplyr::collect()
+
+  check_tidy(au2, exp.row = 25,
+             exp.name = c(names(mtcars), ".cluster"))
+})
