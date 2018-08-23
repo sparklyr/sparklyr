1d20e633eb1c1101fef4573fb95a47b4cb0cf4a3
kevinykuo <kevinykuo@gmail.com>
2018-08-23 13:25:26 -0400

    cache hive context for livy connection

--- R/livy_connection.R
+++ R/livy_connection.R
@@ -787,7 +787,7 @@ initialize_connection.livy_connection <- function(sc) {
     # cache spark version
     sc$spark_version <- spark_version(sc)
 
-    sc$hive_context <- session %||% create_hive_context(sc)
+    sc$state$hive_context <- session %||% create_hive_context(sc)
 
     if (spark_version(sc) < "2.0.0") {
       params <- connection_config(sc, "spark.sql.")
