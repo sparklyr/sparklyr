ef19d767b75bae9e30a926e4110d3eb31307f9a2
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2018-08-23 21:49:57 -0300

    decision trees (augment)

--- R/tidiers_ml_tree_models.R
+++ R/tidiers_ml_tree_models.R
@@ -23,3 +23,43 @@ tidy.ml_model_decision_tree_regression <- function(x,
   dplyr::as_tibble(ml_feature_importances(x))
 }
 
+#' @rdname ml_tree_tidiers
+#' @param newdata a tbl_spark of new data to use for prediction.
+#' @importFrom rlang syms
+#'
+#' @export
+augment.ml_model_decision_tree_classification <- function(x, newdata = NULL,
+                                              ...){
+
+  # if the user doesn't provide a new data, this funcion will
+  # use the training set
+  if (is.null(newdata)){
+    newdata <- x$dataset
+  }
+
+  vars <- c(dplyr::tbl_vars(newdata), "predicted_label")
+
+  ml_predict(x, newdata) %>%
+    dplyr::select(!!!syms(vars)) %>%
+    dplyr::rename(.predicted_label = !!"predicted_label")
+}
+
+#' @rdname ml_tree_tidiers
+#' @importFrom rlang syms
+#'
+#' @export
+augment.ml_model_decision_tree_regression <- function(x, newdata = NULL,
+                                                          ...){
+
+  # if the user doesn't provide a new data, this funcion will
+  # use the training set
+  if (is.null(newdata)){
+    newdata <- x$dataset
+  }
+
+  vars <- c(dplyr::tbl_vars(newdata), "prediction")
+
+  ml_predict(x, newdata) %>%
+    dplyr::select(!!!syms(vars)) %>%
+    dplyr::rename(.prediction = !!"prediction")
+}
