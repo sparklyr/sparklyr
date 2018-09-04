3b6d47f2fe0841d4e4e7201ec3cf8520823e1c88
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2018-09-03 22:16:52 -0300

    broom for random forest
    * glance is equal for regression and classification
    
    * create glance_decision_tree() and glance_random_forest()

--- R/tidiers_ml_tree_models.R
+++ R/tidiers_ml_tree_models.R
@@ -49,20 +49,19 @@ augment.ml_model_decision_tree_regression <- function(x, newdata = NULL,
 #' @export
 glance.ml_model_decision_tree_classification <- function(x,
                                              ...) {
-
-  num_nodes <- x$model$num_nodes()
-  depth <- x$model$depth()
-  impurity <- x$model$param_map$impurity
-
-  dplyr::tibble(num_nodes = num_nodes,
-                depth = depth,
-                impurity = impurity)
+  glance_decision_tree(x)
 }
 
 #' @rdname ml_tree_tidiers
 #' @export
 glance.ml_model_decision_tree_regression <- function(x,
                                              ...) {
+  glance_decision_tree(x)
+}
+
+# glance() code for decision tree is equal for regression
+# and classification
+glance_decision_tree <- function(x){
 
   num_nodes <- x$model$num_nodes()
   depth <- x$model$depth()
@@ -72,3 +71,68 @@ glance.ml_model_decision_tree_regression <- function(x,
                 depth = depth,
                 impurity = impurity)
 }
+
+
+#' @rdname ml_tree_tidiers
+#' @export
+tidy.ml_model_random_forest_classification <- function(x,
+                                                       ...){
+  dplyr::as_tibble(ml_feature_importances(x))
+}
+
+#' @rdname ml_tree_tidiers
+#' @export
+tidy.ml_model_random_forest_regression <- function(x,
+                                                   ...){
+  dplyr::as_tibble(ml_feature_importances(x))
+}
+
+#' @rdname ml_tree_tidiers
+#' @importFrom rlang syms
+#'
+#' @export
+augment.ml_model_random_forest_classification <- function(x, newdata = NULL,
+                                                          ...){
+  broom_augment_supervised(x, newdata = newdata)
+}
+
+#' @rdname ml_tree_tidiers
+#' @importFrom rlang syms
+#'
+#' @export
+augment.ml_model_random_forest_regression <- function(x, newdata = NULL,
+                                                      ...){
+  broom_augment_supervised(x, newdata = newdata)
+}
+
+#' @rdname ml_tree_tidiers
+#' @export
+glance.ml_model_random_forest_classification <- function(x,
+                                                         ...) {
+  glance_random_forest(x)
+}
+
+#' @rdname ml_tree_tidiers
+#' @export
+glance.ml_model_random_forest_regression <- function(x,
+                                                     ...) {
+  glance_random_forest(x)
+}
+
+# glance() code for random forest is equal for regression
+# and classification
+glance_random_forest <- function(x){
+
+  num_trees <- x$model$param_map$num_trees
+  total_num_nodes <- x$model$total_num_nodes()
+  max_depth <- x$model$param_map$max_depth
+  impurity <- x$model$param_map$impurity
+  subsampling_rate <- x$model$param_map$subsampling_rate
+
+  dplyr::tibble(num_trees = num_trees,
+                total_num_nodes = total_num_nodes,
+                max_depth = max_depth,
+                impurity = impurity,
+                subsampling_rate = subsampling_rate)
+}
+
