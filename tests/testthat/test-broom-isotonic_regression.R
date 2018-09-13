7832bf243e62f1c05d8f6061a1367fba720d1a1e
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2018-09-13 19:53:11 -0300

    testthat for ml_isotonic_regression()

---
+++ tests/testthat/test-broom-isotonic_regression.R
@@ -0,0 +1,42 @@
+context("broom-isotonic_regression")
+
+test_that("isotonic_regression.tidy() works", {
+  sc <- testthat_spark_connection()
+  test_requires_version("2.0.0")
+  iris_tbl <- testthat_tbl("iris")
+
+  expect_error(
+    iris_tbl %>%
+      ml_isotonic_regression(Petal_Length ~ Petal_Width) %>%
+      tidy()
+  )
+
+})
+
+test_that("isotonic_regression.augment() works", {
+  sc <- testthat_spark_connection()
+  test_requires_version("2.0.0")
+  iris_tbl <- testthat_tbl("iris")
+
+  au1 <- iris_tbl %>%
+    ml_isotonic_regression(Petal_Length ~ Petal_Width) %>%
+    augment() %>%
+    dplyr::collect()
+
+  check_tidy(au1, exp.row = 150,
+             exp.name = c(dplyr::tbl_vars(iris_tbl),
+                          ".prediction"))
+})
+
+test_that("isotonic_regression.glance() works", {
+  sc <- testthat_spark_connection()
+  test_requires_version("2.0.0")
+  iris_tbl <- testthat_tbl("iris")
+
+  gl1 <- iris_tbl %>%
+    ml_isotonic_regression(Petal_Length ~ Petal_Width) %>%
+    glance()
+
+  check_tidy(gl1, exp.row = 1,
+             exp.names = c("isotonic", "num_boundaries"))
+})
