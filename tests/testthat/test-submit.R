51bb6f2597eff1a4d6d69e657acde55bb54c637e
Javier Luraschi <javierluraschi@hotmail.com>
2018-09-25 14:57:57 -0700

    reenable spark_submit() test

--- tests/testthat/test-submit.R
+++ tests/testthat/test-submit.R
@@ -1,9 +1,17 @@
 context("submit")
 
 test_that("spark_submit() can submit batch jobs", {
-  skip("NYI")
-
   batch_file <- dir(getwd(), recursive = TRUE, pattern = "batch.R", full.names = TRUE)
 
+  if (dir.exists("batch.csv")) unlink("batch.csv", recursive = TRUE)
+
   spark_submit(master = "local", file = batch_file)
+
+  retries <- 30
+  while (!dir.exists("batch.csv") && retries > 0) {
+    Sys.sleep(1)
+    retries <- retries - 1
+  }
+
+  expect_gt(retries, 0)
 })
