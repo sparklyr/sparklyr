5d510ec9986ad1e16d3cb13fd27297040c82c71e
Javier Luraschi <javierluraschi@hotmail.com>
2018-10-01 20:57:48 -0700

    avoid use of spartswith to support R 3.2

--- R/core_config.R
+++ R/core_config.R
@@ -7,7 +7,7 @@
 #' @keywords internal
 #' @export
 spark_config_value <- function(config, name, default = NULL) {
-  if (getOption("sparklyr.test.enforce.config", FALSE) && any(startsWith(name, "sparklyr."))) {
+  if (getOption("sparklyr.test.enforce.config", FALSE) && any(grepl("^sparklyr.", name))) {
     settings <- get("spark_config_settings")()
     if (!any(name %in% settings$name)) {
       stop("Config value '", name[[1]], "' not described in spark_config_settings()")
