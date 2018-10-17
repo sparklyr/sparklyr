eae6bfec24fdc3942843ec5456cc6d027bf12f06
Javier Luraschi <javierluraschi@hotmail.com>
2018-10-17 14:12:05 -0700

    transition to use new streaming bindings pr

--- R/worker_apply.R
+++ R/worker_apply.R
@@ -99,6 +99,7 @@ spark_worker_build_types <- function(sc, columns) {
 
 spark_worker_apply_arrow <- function(sc, config) {
   worker_log("using arrow serializer")
+  if (!require(arrow)) stop("Package 'arrow' not available in worker node.")
 
   context <- spark_worker_context(sc)
   spark_worker_init_packages(sc, context)
@@ -120,18 +121,20 @@ spark_worker_apply_arrow <- function(sc, config) {
     time_zone
   )
 
-  read_record_batch_stream <- get("read_record_batch_stream", envir = as.environment(asNamespace("arrow")))
-  dfs <- read_record_batch_stream(record_batch_raw)
+  reader <- record_batch_stream_reader(record_batch_raw)
+  record_entry <- read_record_batch(reader)
 
   all_batches <- list()
   total_rows <- 0
 
   schema_output <- NULL
 
-  for (i in 1:length(dfs)) {
-    worker_log("is processing batch ", i)
+  batch_idx <- 0
+  while (!record_entry$is_null()) {
+    batch_idx <- batch_idx + 1
+    worker_log("is processing batch ", batch_idx)
 
-    df <- dfs[[i]]
+    df <- tibble::as_tibble(record_entry)
     colnames(df) <- columnNames[1: length(colnames(df))]
 
     result <- spark_worker_execute_closure(closure, df, funcContext, grouped_by)
@@ -142,12 +145,13 @@ spark_worker_apply_arrow <- function(sc, config) {
       schema_output <- spark_worker_build_types(sc, lapply(result, class))
     }
 
-    record_batch <- get("record_batch", envir = as.environment(asNamespace("arrow")))
     record <- record_batch(result)
-    raw_batch <- record$to_stream()
+    raw_batch <- write_record_batch(record, raw())
 
-    all_batches[[i]] <- raw_batch
+    all_batches[[length(all_batches) + 1]] <- raw_batch
     total_rows <- total_rows + nrow(result)
+
+    record_entry <- read_record_batch(reader)
   }
 
   if (length(all_batches) > 0) {
