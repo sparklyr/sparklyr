1cf11c7f135a6d42bd17ae14c825f5cc6601a61b
Javier Luraschi <javierluraschi@hotmail.com>
2018-10-18 17:38:07 -0700

    rebuild livy sources

---
+++ inst/livy/spark-2.3.0/arrowhelper.scala
@@ -0,0 +1,19 @@
+//
+// This file was automatically generated using livy_sources_refresh()
+// Changes to this file will be reverted.
+//
+
+object ArrowHelper {
+  import org.apache.spark.SparkContext
+
+  def rddFromBinaryBatches(sc: SparkContext, batches: Seq[Array[Byte]], parallelism: Int):
+      org.apache.spark.rdd.RDD[Array[Byte]] = {
+    sc.parallelize(batches, parallelism)
+  }
+
+  def javaRddFromBinaryBatches(sc: SparkContext, batches: Seq[Array[Byte]], parallelism: Int):
+    org.apache.spark.api.java.JavaRDD[Array[Byte]] = {
+    val rdd = rddFromBinaryBatches(sc, batches, parallelism)
+    org.apache.spark.api.java.JavaRDD.fromRDD(rdd)
+  }
+}
