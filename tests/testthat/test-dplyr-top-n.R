650b9588002614cc35e30d52811818417fe936a4
Javier Luraschi <javierluraschi@hotmail.com>
2018-10-26 08:45:53 -0700

    adjust tolerance and factors in tests

--- tests/testthat/test-dplyr-top-n.R
+++ tests/testthat/test-dplyr-top-n.R
@@ -11,14 +11,14 @@ test_that("top_n works as expected", {
     test_data <- c(test_data, rep.int(LETTERS[i], times = i * 10))
   }
 
-  test_data <- data.frame("X" = test_data)
+  test_data <- data.frame("X" = test_data, stringsAsFactors = F)
   test_tbl <- copy_to(sc, test_data)
 
   tn1 <- test_tbl %>% count(X) %>% top_n(10) %>% collect()
   tn2 <- test_data %>% count(X) %>% top_n(10)
 
-  tn2 <- tn2 %>% mutate(n = as.integer(n)) %>% arrange(X)
-  tn1 <- tn1 %>% arrange(X)
+  tn2 <- tn2 %>% arrange(X)
+  tn1 <- tn1 %>% mutate(n = as.integer(n)) %>% arrange(X)
 
   testthat::expect_true(all(tn1 == tn2))
 })
