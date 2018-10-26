98c19d2a7351e7376dc185f1b6891af40894f268
Javier Luraschi <javierluraschi@hotmail.com>
2018-10-26 16:00:13 -0700

    implement performance reporter for testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,6 +2,53 @@ Sys.setenv("R_TESTS" = "")
 library(testthat)
 library(sparklyr)
 
+PerformanceReporter <- R6::R6Class("PerformanceReporter",
+                                   inherit = Reporter,
+                                   public = list(
+                                     results = list(),
+                                     last_context = NA_character_,
+                                     last_test = NA_character_,
+                                     last_time = Sys.time(),
+
+                                     start_context = function(context) {
+                                       self$last_context <- context
+                                       self$last_time <- Sys.time()
+                                       cat(paste0("\nContext: ", context, "\n"))
+                                     },
+
+                                     add_result = function(context, test, result) {
+                                       elapsed_time <- as.numeric(Sys.time() - self$last_time)
+
+                                       if (identical(self$last_test, test)) {
+                                         total_time <- self$results[[length(self$results)]]$time + elapsed_time
+                                         self$results[[length(self$results)]]$time <- total_time
+                                       }
+                                       else {
+                                         if (length(self$results) >= 1) {
+                                           previous_result <- self$results[[length(self$results)]]
+                                           cat(paste0(previous_result$test, ": ", previous_result$time, "\n"))
+                                         }
+
+                                         self$results[[length(self$results) + 1]] <- list(
+                                           context = self$last_context,
+                                           test = test,
+                                           result = result,
+                                           time = elapsed_time
+                                         )
+                                       }
+
+                                       self$last_test <- test
+                                       self$last_time <- Sys.time()
+                                     },
+
+                                     end_reporter = function() {
+                                       cat("\n")
+                                       data <- do.call("rbind", args = self$results)[, c("context", "test", "time")]
+                                       print(as.data.frame(data, row.names = F))
+                                     }
+                                   )
+)
+
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
   # enforce all configuration settings are described
   options(sparklyr.test.enforce.config = TRUE)
@@ -28,6 +75,5 @@ if (identical(Sys.getenv("NOT_CRAN"), "true")) {
 
   on.exit({ spark_disconnect_all() ; livy_service_stop() })
 
-  test_filter <- "^config$"
-  test_check("sparklyr", filter = test_filter)
+  test_check("sparklyr", filter = test_filter, reporter = "performance")
 }
