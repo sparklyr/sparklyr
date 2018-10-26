3ed795ccb30e60cbea95de140385d617b5dd9a68
Javier Luraschi <javierluraschi@hotmail.com>
2018-10-26 11:55:11 -0700

    explicit cast on dplyr top_n() test

--- tests/testthat/test-dplyr-top-n.R
+++ tests/testthat/test-dplyr-top-n.R
@@ -2,8 +2,6 @@ context("dplyr top_n")
 sc <- testthat_spark_connection()
 
 test_that("top_n works as expected", {
-  skip_on_arrow()
-
   test_requires_version("2.0.0", "bug in spark-csv")
   test_requires("dplyr")
 
@@ -19,8 +17,8 @@ test_that("top_n works as expected", {
   tn1 <- test_tbl %>% count(X) %>% top_n(10) %>% collect()
   tn2 <- test_data %>% count(X) %>% top_n(10)
 
-  tn2 <- tn2 %>% arrange(X)
-  tn1 <- tn1 %>% arrange(X)
+  tn2 <- tn2 %>% mutate(n = as.integer(n)) %>% arrange(X)
+  tn1 <- tn1 %>% mutate(n = as.integer(n)) %>% arrange(X)
 
   expect_equal(tn1, tn2)
 })
