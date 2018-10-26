3f8db267336d6a79c3700f9fcadb9e6f18aabf39
Javier Luraschi <javierluraschi@hotmail.com>
2018-10-25 17:41:36 -0700

    fix broom tests under arrow

--- tests/testthat/test-broom-gaussian_mixture.R
+++ tests/testthat/test-broom-gaussian_mixture.R
@@ -5,13 +5,14 @@ test_that("gaussian_mixture.tidy() works", {
   test_requires_version("2.0.0")
   mtcars_tbl <- testthat_tbl("mtcars")
 
-  td1 <- mtcars_tbl %>%
-    ml_gaussian_mixture(~ mpg + cyl, k = 4L, seed = 123) %>%
-    tidy()
+  model <- mtcars_tbl %>%
+    ml_gaussian_mixture(~ mpg + cyl, k = 4L, seed = 123)
+
+  td1 <- model %>% tidy()
 
   check_tidy(td1, exp.row = 4,
              exp.names = c("mpg", "cyl", "weight", "size", "cluster"))
-  expect_equal(td1$size, c(3, 14, 4, 11))
+  expect_equal(td1$size, model$summary$cluster_sizes())
 
 })
 
