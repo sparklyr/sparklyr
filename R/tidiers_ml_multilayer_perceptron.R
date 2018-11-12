5e84c51a34ca6b0c19ed2b05f3d5d08aa0451779
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2018-11-12 16:30:12 -0200

    using map instead of for loop

--- R/tidiers_ml_multilayer_perceptron.R
+++ R/tidiers_ml_multilayer_perceptron.R
@@ -16,22 +16,22 @@ tidy.ml_model_multilayer_perceptron_classification <- function(x,
   weight_param <- NULL
 
   # how many parameters in each layer
-  for (i in 1:(num_layers - 1 )){
-    weight_param[i] <- (x$model$layers[i] + 1) * x$model$layers[i + 1]
-  }
+  weight_param <- purrr::map_dbl(1:(num_layers - 1 ), function(e) {
+    (x$model$layers[e] + 1) * x$model$layers[e + 1]
+  })
+
   # cuts in x$model$weights
   weight_param <- c(0, cumsum(weight_param))
 
   # transform the vector x$model$weights in a list of
   # matrix
   weight_matrix <- list()
-  for(i in 1:(length(weight_param) - 1)){
-
-    weight_matrix[[i]] <- matrix(x$model$weights[(weight_param[i] + 1):weight_param[i + 1]],
-                                 nrow = x$model$layers[i] + 1 ,
-                                 ncol = x$model$layers[i + 1],
-                                 byrow = TRUE)
-  }
+  weight_matrix <- purrr::map(1:(length(weight_param) - 1), function(e){
+    matrix(x$model$weights[(weight_param[e] + 1):weight_param[e + 1]],
+           nrow = x$model$layers[e] + 1 ,
+           ncol = x$model$layers[e + 1],
+           byrow = TRUE)
+  })
 
   layers <- purrr::map_chr(1:(num_layers - 1 ), function(e){
     paste0("layer_", e)
@@ -40,11 +40,6 @@ tidy.ml_model_multilayer_perceptron_classification <- function(x,
   dplyr::tibble(layers, weight_matrix)
 
 }
-
-
-
-
-
 
 #' @rdname ml_multilayer_perceptron_tidiers
 #' @param newdata a tbl_spark of new data to use for prediction.
