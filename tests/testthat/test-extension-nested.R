0f1ea8fb4bc2dd46df17431bb24f266102f26d55
Javier Luraschi <javierluraschi@hotmail.com>
2018-11-16 14:26:55 -0800

    add test to ensure nested queries work with filter

---
+++ tests/testthat/test-extension-nested.R
@@ -0,0 +1,12 @@
+context("extension sparklyr.nested")
+sc <- testthat_spark_connection()
+
+test_that("sparklyr.nested can query nested columns", {
+  iris_tbl <- testthat_tbl("iris")
+  iris_nst <- iris_tbl %>% sparklyr.nested::sdf_nest(Species, Sepal_Width)
+
+  expect_equal(
+    iris_nst %>% filter(data.Species == "setosa") %>% count() %>% pull(n) %>% as.integer(),
+    50
+  )
+})
