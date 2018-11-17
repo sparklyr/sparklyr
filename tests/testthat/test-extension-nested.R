abf03abba4d50b8ea9712d69782b58ba39ac990c
Javier Luraschi <javierluraschi@hotmail.com>
2018-11-16 16:37:07 -0800

    skip sparklyr.nested test in older R versions

--- tests/testthat/test-extension-nested.R
+++ tests/testthat/test-extension-nested.R
@@ -2,6 +2,9 @@ context("extension sparklyr.nested")
 sc <- testthat_spark_connection()
 
 test_that("sparklyr.nested can query nested columns", {
+  if (package_version(paste(R.Version()$major, R.Version()$minor, sep = ".")) < "3.3")
+    skip("sparklyr.nested requires R 3.3")
+
   iris_tbl <- testthat_tbl("iris")
   iris_nst <- iris_tbl %>% sparklyr.nested::sdf_nest(Species, Sepal_Width)
 
