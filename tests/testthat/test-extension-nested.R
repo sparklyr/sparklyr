e69ca571241f4cf0ea2f86d787855e02d10d7401
Javier Luraschi <javierluraschi@hotmail.com>
2018-11-18 14:28:30 -0800

    fix appveyor tests for sparklyr.nested

--- tests/testthat/test-extension-nested.R
+++ tests/testthat/test-extension-nested.R
@@ -2,8 +2,8 @@ context("extension sparklyr.nested")
 sc <- testthat_spark_connection()
 
 test_that("sparklyr.nested can query nested columns", {
-  if (package_version(paste(R.Version()$major, R.Version()$minor, sep = ".")) < "3.3")
-    skip("sparklyr.nested requires R 3.3")
+  if (!"sparklyr.nested" %in% installed.packages())
+    skip("sparklyr.nested not installed.")
 
   iris_tbl <- testthat_tbl("iris")
   iris_nst <- iris_tbl %>% sparklyr.nested::sdf_nest(Species, Sepal_Width)
