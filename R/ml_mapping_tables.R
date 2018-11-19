b34a343d6660e6b85e48cf7ae7e859532bc74afc
kevinykuo <kevinykuo@gmail.com>
2018-11-19 16:24:32 -0500

    refactor ml_create_mapping_tables()

--- R/ml_mapping_tables.R
+++ R/ml_mapping_tables.R
@@ -2,27 +2,27 @@ ml_create_mapping_tables <- function() { # nocov start
   param_mapping_file <- system.file(file.path("sparkml", "param_mapping.json"), package = packageName())
   param_mapping_list <- jsonlite::fromJSON(param_mapping_file, simplifyVector = FALSE)
 
-  param_mapping_r_to_s <- new.env(parent = emptyenv(),
-                                  size = length(param_mapping_list))
-  param_mapping_s_to_r <- new.env(parent = emptyenv(),
-                                  size = length(param_mapping_list))
+  param_mapping_r_to_s <- new.env(
+    parent = emptyenv(),
+    size = length(param_mapping_list)
+  )
+  param_mapping_s_to_r <- new.env(
+    parent = emptyenv(),
+    size = length(param_mapping_list)
+  )
 
-  invisible(lapply(names(param_mapping_list),
-                   function(x) {
-                     param_mapping_r_to_s[[x]] <- param_mapping_list[[x]]
-                     param_mapping_s_to_r[[param_mapping_list[[x]]]] <- x
-                   }))
+  purrr::iwalk(
+    param_mapping_list,
+    function(value, key) {
+      param_mapping_r_to_s[[key]] <- value
+      param_mapping_s_to_r[[value]] <- key
+    }
+  )
 
   class_mapping_file <- system.file(file.path("sparkml", "class_mapping.json"), package = packageName())
   ml_class_mapping_list <- jsonlite::fromJSON(class_mapping_file, simplifyVector = FALSE)
 
-  ml_class_mapping <- new.env(parent = emptyenv(),
-                               size = length(ml_class_mapping_list))
-
-  invisible(lapply(names(ml_class_mapping_list),
-                   function(x) {
-                     ml_class_mapping[[x]] <- ml_class_mapping_list[[x]]
-                   }))
+  ml_class_mapping <- as.environment(ml_class_mapping_list)
 
   rlang::ll(param_mapping_r_to_s = param_mapping_r_to_s,
             param_mapping_s_to_r = param_mapping_s_to_r,
