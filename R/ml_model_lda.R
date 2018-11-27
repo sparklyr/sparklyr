1efef3bfbc2fb655e7d284b36510f21ecf858a34
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2018-11-27 01:29:11 -0300

    Constructor used in ml_clustering_lda.R

---
+++ R/ml_model_lda.R
@@ -0,0 +1,13 @@
+new_ml_model_lda <- function(pipeline, pipeline_model, model,
+                                dataset, formula, feature_names,
+                                call) {
+
+  vocabulary <- pipeline_model$stages[[2]]$vocabulary
+
+  new_ml_model_clustering(
+    pipeline, pipeline_model,
+    model, dataset, formula,
+    vocabulary = vocabulary,
+    subclass = "ml_lda_model"
+  )
+}
