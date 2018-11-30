64705c3c5acaa1ee289b1b3b5e13ee7bfbd58f8c
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2018-11-30 04:12:00 -0300

    new tidy and augment
    * I also change ml_lda_model by ml_model_lda

--- R/tidiers_ml_lda.R
+++ R/tidiers_ml_lda.R
@@ -9,43 +9,46 @@ NULL
 
 #' @rdname ml_lda_tidiers
 #' @export
-tidy.ml_lda_model <- function(x,
+tidy.ml_model_lda <- function(x,
                               ...){
 
- doc_concentration <- sparklyr::invoke(x$model$estimated_doc_concentration(),
-                                       "toArray")
-
- describe_topics <- x$model$describe_topics() %>%
-   dplyr::collect()
-
- cbind(describe_topics,
-       doc_concentrarion = doc_concentration) %>%
-    dplyr::as_tibble()
-}
-
-#' @rdname ml_lda_tidiers
-#'
-#' @export
-augment.ml_lda_model <- function(x,
-                                 ...){
-
-  vocabulary <- ml_vocabulary(x)
+  term <- ml_vocabulary(x)
   topics_matrix <- x$model$topics_matrix() %>%
     dplyr::as_data_frame()
 
   k <- x$model$param_map$k
-  names(topics_matrix) <- purrr::map_chr(0:(k - 1), function(e){
-    paste0("topic_", e) # the topics in spark start with 0
-  })
+  names(topics_matrix) <- 0:(k - 1)
 
-  dplyr::bind_cols(vocabulary = vocabulary,
-                   topics_matrix)
+  dplyr::bind_cols(term = term, topics_matrix) %>%
+    tidyr::gather(topic, beta, -term) %>%
+    dplyr::select(topic, term, beta) %>%
+    dplyr::mutate(topic = as.integer(topic))
+}
+
+#' @rdname ml_lda_tidiers
+#' @param newdata a tbl_spark of new data to use for prediction.
+#' @importFrom rlang syms
+#' @export
+augment.ml_model_lda <- function(x, newdata = NULL,
+                                 ...){
+
+  # if the user doesn't provide a new data, this funcion will
+  # use the training set
+  if (is.null(newdata)){
+    newdata <- x$dataset
+  }
+
+  vars <- c(dplyr::tbl_vars(newdata), "topicDistribution")
+
+  ml_predict(x, newdata ) %>%
+    dplyr::select(!!!syms(vars)) %>%
+    dplyr::rename(.topic = !!"topicDistribution")
 }
 
 #' @rdname ml_lda_tidiers
 #' @export
-glance.ml_lda_model <- function(x,
-                                ...) {
+glance.ml_model_lda <- function(x,
+                                ...){
 
   k = x$model$param_map$k
   vocab_size <- x$model$vocab_size
