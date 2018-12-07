a9c1729e3c9f6ee2a50cffc89ce906e1b7283ef9
kevinykuo <kevinykuo@gmail.com>
2018-12-07 18:10:09 -0500

    remove redundant raw prediction col in probabilistic classifier

--- R/ml_transformer_and_estimator.R
+++ R/ml_transformer_and_estimator.R
@@ -56,8 +56,6 @@ ml_prediction_model <- new_ml_prediction_model
 new_ml_classification_model <- function(jobj, ..., class = character()) {
   new_ml_prediction_model(
     jobj,
-    # `possibly_null` because older versions of Spark might not have this method
-    raw_prediction_col = possibly_null(invoke)(jobj, "getRawPredictionCol"),
     num_classes = possibly_null(invoke)(jobj, "numClasses"),
     ...,
     class = c(class, "ml_classification_model")
@@ -129,7 +127,8 @@ ml_predictor <- new_ml_predictor
 new_ml_classifier <- function(jobj, ..., class = character()) {
   new_ml_predictor(
     jobj,
-    raw_prediction_col = invoke(jobj, "getRawPredictionCol"),
+    # `possibly_null` because older versions of Spark might not have this method
+    raw_prediction_col = possibly_null(invoke)(jobj, "getRawPredictionCol"),
     ...,
     class = c(class, "ml_classifier")
   )
