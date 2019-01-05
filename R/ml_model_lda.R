ab00a9f5f49af63f6bc45be21fa415f373f27260
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-01-05 09:22:12 -0300

    change parameters in new_ml_model_lda
    * according modifications in new_ml_model_clustering()

--- R/ml_model_lda.R
+++ R/ml_model_lda.R
@@ -1,13 +1,14 @@
-new_ml_model_lda <- function(pipeline, pipeline_model, model,
-                                dataset, formula, feature_names,
-                                call) {
+new_ml_model_lda <- function(pipeline_model, formula, dataset,
+                             features_col) {
 
   vocabulary <- pipeline_model$stages[[2]]$vocabulary
 
   new_ml_model_clustering(
-    pipeline, pipeline_model,
-    model, dataset, formula,
+    pipeline_model = pipeline_model,
+    formula = formula,
+    dataset = dataset,
+    features_col = features_col,
     vocabulary = vocabulary,
-    subclass = "ml_model_lda"
+    class = "ml_model_lda"
   )
 }
