d3fde75640469a7adfe3e79fe1e680349a6f0b6b
Javier Luraschi <javierluraschi@hotmail.com>
2019-02-01 21:48:56 -0800

    add batch collection test

--- tests/testthat/test-sdf-collect.R
+++ tests/testthat/test-sdf-collect.R
@@ -8,3 +8,26 @@ test_that("sdf_collect() works properly", {
 
   expect_equivalent(mtcars, mtcars_data)
 })
+
+test_that("sdf_collect() supports callback", {
+  df <- matrix(0, ncol = 5, nrow = 2) %>% dplyr::as_data_frame()
+
+  batch_count <- 0
+  row_count <- 0
+
+  sdf_len(sc, 10, repartition = 2) %>%
+    sdf_collect(callback = function(df) {
+      batch_count <<- batch_count + 1
+      row_count <<- row_count + nrow(df)
+    })
+
+  expect_equal(
+    batch_count,
+    2
+  )
+
+  expect_equal(
+    row_count,
+    10
+  )
+})
