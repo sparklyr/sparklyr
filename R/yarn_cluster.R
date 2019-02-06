bd28a0b627bd4a6a47ce65e1437611a9a1487aea
lgong <lgong@microsoft.com>
2019-02-05 21:01:46 -0800

    expand variables in conf property value strings.

--- R/yarn_cluster.R
+++ R/yarn_cluster.R
@@ -22,6 +22,31 @@ spark_yarn_cluster_get_conf_property <- function(property) {
   )
   )
 
+  get_variables_in_value_strings <- function(strings) {
+    var.regex <- "\\$\\{([^$]+)\\}"
+    parsed <- gregexpr(var.regex, strings, perl = TRUE)
+    lapply(seq_along(parsed), function(j) {
+      p <- parsed[[j]]
+      unlist(sapply(seq_along(p), function(i) {
+        if (p[i] == -1) return(NULL)
+        st <- attr(p, "capture.start")[i, ]
+        substring(strings[j], st, st + attr(p, "capture.length")[i, ] - 1)
+      }))
+    })
+  }
+
+  while (length(vars <- get_variables_in_value_strings(yarnPropertyValue)) && length(vars[[1]])) {
+    for (var in vars[[1]]) {
+      value <- xml2::xml_text(xml2::xml_find_all(
+      	yarnSiteXml,
+      	paste0("//name[.='", var, "']/parent::property/value")
+      	)
+      )
+      
+      yarnPropertyValue <- gsub(paste0("${", var, "}"), value, yarnPropertyValue, fixed = TRUE)
+    }
+  }
+
   yarnPropertyValue
 }
 
