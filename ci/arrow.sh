55846818ba5ca50a95b825f56c78eaa2afef4fb5
Javier Luraschi <javierluraschi@hotmail.com>
2019-02-12 21:58:47 -0800

    build arrow from source

--- ci/arrow.sh
+++ ci/arrow.sh
@@ -1,16 +1,19 @@
-sudo apt update
-sudo apt install -y -V apt-transport-https lsb-release
-curl https://dist.apache.org/repos/dist/dev/arrow/KEYS | sudo apt-key add -
-sudo tee /etc/apt/sources.list.d/apache-arrow.list <<APT_LINE
-deb [arch=amd64] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
-deb-src https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
-APT_LINE
-sudo apt update
-sudo apt install -y -V libarrow-dev # For C++
-sudo apt install -y -V libarrow-glib-dev # For GLib (C)
-sudo apt install -y -V libplasma-dev # For Plasma C++
-sudo apt install -y -V libplasma-glib-dev # For Plasma GLib (C)
-sudo apt install -y -V libgandiva-dev # For Gandiva C++
-sudo apt install -y -V libgandiva-glib-dev # For Gandiva GLib (C)
-sudo apt install -y -V libparquet-dev # For Apache Parquet C++
-sudo apt install -y -V libparquet-glib-dev # For Apache Parquet GLib (C)
+$ARROW_VERSION="0.12.0"
+
+pushd .
+
+sudo apt install -y -V cmake
+sudo apt install -y -V boost-devel
+sudo apt install -y -V autoconf
+sudo apt install -y -V flex
+sudo apt install -y -V bison
+
+wget https://arrowlib.rstudio.com/dist/arrow/arrow-$ARROW_VERSION/apache-arrow-$ARROW_VERSION.tar.gz
+tar -xvzf apache-arrow-$ARROW_VERSION.tar.gz
+cd apache-arrow-$ARROW_VERSION/cpp
+mkdir release
+cd release
+cmake -DARROW_BUILD_TESTS=ON -DARROW_PARQUET=ON ..
+make install
+
+popd
