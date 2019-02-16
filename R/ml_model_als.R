eebf7eafb628beb3c12d2f809f8615b42d89144f
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-02-15 23:20:16 -0300

    remove label_col and features_col

--- R/ml_model_als.R
+++ R/ml_model_als.R
@@ -1,15 +1,11 @@
 new_ml_model_als <- function(pipeline_model,
                              formula,
-                             dataset,
-                             label_col,
-                             features_col) {
- m <- new_ml_model_regression(
+                             dataset) {
+
+ m <- new_ml_model_recommendation(
     pipeline_model, formula, dataset = dataset,
-    label_col = label_col, features_col = features_col,
     class = "ml_model_als")
 
- m$features_col <- NULL
- m$label_col <- NULL
  m$`.jobj` <- spark_jobj(m)
 
  m
