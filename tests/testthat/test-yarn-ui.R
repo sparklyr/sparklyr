aa0c1f9ee136ec37dab31bca6b28be54895b6817
Javier Luraschi <javierluraschi@hotmail.com>
2019-02-19 00:08:29 +0000

    fix yarn ui test

--- tests/testthat/test-yarn-ui.R
+++ tests/testthat/test-yarn-ui.R
@@ -3,6 +3,10 @@ context("yarn ui")
 sc <- testthat_spark_connection()
 
 test_that("'spark_connection_yarn_ui()' can build a default URL", {
+  Sys.setenv(
+    YARN_CONF_DIR = dirname(dir(getwd(), recursive = TRUE, pattern = "yarn-site.xml", full.names = TRUE))
+  )
+
   expect_true(
     nchar(spark_connection_yarn_ui(sc)) > 0
   )
