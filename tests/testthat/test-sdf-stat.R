d157f998e876cff9c3074e67bc42f9ec2784f250
kevinykuo <kevinykuo@gmail.com>
2019-02-28 10:49:27 -0500

    use setequal for test

--- tests/testthat/test-sdf-stat.R
+++ tests/testthat/test-sdf-stat.R
@@ -4,6 +4,6 @@ test_that("sdf_crosstab() works", {
   sc <- testthat_spark_connection()
   mtcars_tbl <- testthat_tbl("mtcars")
   df <- mtcars_tbl %>% sdf_crosstab("cyl", "gear") %>% collect()
-  expect_identical(names(df), c("cyl_gear", "3.0", "4.0", "5.0"))
-  expect_identical(df[,1, drop = TRUE], c("8.0", "4.0", "6.0"))
+  expect_setequal(names(df), c("cyl_gear", "3.0", "4.0", "5.0"))
+  expect_setequal(df[,1, drop = TRUE], c("8.0", "4.0", "6.0"))
 })
