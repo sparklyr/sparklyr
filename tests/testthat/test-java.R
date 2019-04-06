291a69c6aa1f31c2d69dc6ea3af697709e733951
Javier Luraschi <javierluraschi@hotmail.com>
2019-04-05 17:57:25 -0700

    attempt to install openjdk9

--- tests/testthat/test-java.R
+++ tests/testthat/test-java.R
@@ -26,19 +26,6 @@ test_that("'validate_java_version_line' works with 1.8", {
   succeed()
 })
 
-test_that("'validate_java_version_line' fails with java 9 in local mode", {
-  expect_error(
-    validate_java_version_line(
-      "local",
-      c(
-        "java version 9",
-        "Java(TM) SE Runtime Environment (build 1.9.0_000-000)",
-        "Java HotSpot(TM) 64-Bit Server VM (build 25.144-000, mixed mode)"
-      )
-    )
-  )
-})
-
 test_that("'validate_java_version_line' works with java 9 in non-local mode", {
   validate_java_version_line(
     "yarn-client",
