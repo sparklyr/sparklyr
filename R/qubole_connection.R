144e81341ff592424bc11e9eda959c254ae7ef71
Amey Agrawal <agrawalamey12@gmail.com>
2019-06-24 23:28:14 +0530

    Update R/qubole_connection.R
    Co-Authored-By: Javier Luraschi <javierluraschi@hotmail.com>

--- R/qubole_connection.R
+++ R/qubole_connection.R
@@ -1,7 +1,8 @@
 initialize_method.qubole <- function(method, scon) {
   scon$config$sparklyr.web.spark <- invoke(scon$state$spark_context,
                                              "getSparkUIURL")
-  scon$config$sparklyr.web.yarn <- system("/usr/lib/rstudio-utils/bin/get-yarn-url.sh",
+  yarn_url <-  Sys.getenv("QUBOLE_YARN_WEB")
+  scon$config$sparklyr.web.yarn <-if (nchar(yarn_url) > 0) yarn_url else system("/usr/lib/rstudio-utils/bin/get-yarn-url.sh",
                                             intern = TRUE)
   scon
 }
