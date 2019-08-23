9f16589b8db51bde174c81aed614b0f84c8db5be
Jozef <jozef.hajnala@gmail.com>
2019-08-23 18:39:26 +0000

    allow reading multiple files with `spark_read_`

---
+++ tests/testthat/test-read-write-multiple.R
@@ -0,0 +1,67 @@
+context("read-write-multiple")
+
+sc <- testthat_spark_connection()
+
+test_readwrite <- function(sc, writer, reader, name = "testtable", ...) {
+  path <- file.path(dirname(sc$output_file), c("batch_1", "batch_2"))
+  on.exit(unlink(path, recursive = TRUE, force = TRUE), add = TRUE)
+
+  writer(sdf_copy_to(sc, data.frame(line = as.character(1L:3L))), path[1L])
+  writer(sdf_copy_to(sc, data.frame(line = as.character(4L:6L))), path[2L])
+
+  if (is.element("whole", names(list(...))) && isTRUE(list(...)$whole)) {
+    res_1 <- reader(sc, name, path[1L], ...) %>% collect() %>% pull(contents) %>% strsplit("\n") %>% unlist() %>% sort()
+    res_2 <- reader(sc, name, path[2L], ...) %>% collect() %>% pull(contents) %>% strsplit("\n") %>% unlist() %>% sort()
+    res_3 <- reader(sc, name, path, ...) %>% collect() %>% pull(contents) %>% strsplit("\n") %>% unlist() %>% sort()
+  } else {
+    res_1 <- reader(sc, name, path[1L], ...) %>% collect() %>% pull(line) %>% sort()
+    res_2 <- reader(sc, name, path[2L], ...) %>% collect() %>% pull(line) %>% sort()
+    res_3 <- reader(sc, name, path, ...) %>% collect() %>% pull(line) %>% sort()
+  }
+
+  list(
+    all(res_1 == as.character(1:3)),
+    all(res_2 == as.character(4:6)),
+    all(res_3 == as.character(1:6))
+  )
+}
+
+test_that(
+  "spark_read_parquet() reads multiple parquet files",
+    expect_equal(
+      test_readwrite(sc = sc, writer = spark_write_parquet, reader = spark_read_parquet),
+      list(TRUE, TRUE, TRUE)
+    )
+)
+
+test_that(
+  "spark_read_orc() reads multiple orc files",
+  expect_equal(
+    test_readwrite(sc = sc, writer = spark_write_orc, reader = spark_read_orc),
+    list(TRUE, TRUE, TRUE)
+  )
+)
+
+test_that(
+  "spark_read_json() reads multiple json files",
+    expect_equal(
+      test_readwrite(sc = sc, writer = spark_write_json, reader = spark_read_json),
+      list(TRUE, TRUE, TRUE)
+    )
+)
+
+test_that(
+  "spark_read_text() reads multiple text files",
+  expect_equal(
+      test_readwrite(sc = sc, writer = spark_write_text, reader = spark_read_text),
+      list(TRUE, TRUE, TRUE)
+    )
+)
+
+test_that(
+  "spark_read_text() throws a useful error for multiple files with whole=TRUE",
+  expect_error(
+    test_readwrite(sc = sc, writer = spark_write_text, reader = spark_read_text, whole = TRUE),
+    "spark_read_text is only suppored with path of length 1 if whole=TRUE"
+  )
+)
