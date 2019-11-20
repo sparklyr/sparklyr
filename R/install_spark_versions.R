cd11529f7f8d2b325f69ba2923f0208d4767c4e8
Javier Luraschi <javierluraschi@hotmail.com>
2019-11-20 15:35:13 -0800

    use versions from spark repo to remove dep to exterenal reepo

--- R/install_spark_versions.R
+++ R/install_spark_versions.R
@@ -5,7 +5,7 @@ spark_versions_file_pattern <- function() {
 }
 
 spark_versions_url <- function() {
-  "https://raw.githubusercontent.com/rstudio/spark-install/master/common/versions.json"
+  "https://raw.githubusercontent.com/rstudio/sparklyr/master/inst/extdata/versions.json"
 }
 
 #' @importFrom jsonlite fromJSON
