3be39383c135f30de5a45abad94fe74c34f04be7
Yitao Li <yitao@rstudio.com>
2020-02-24 13:07:29 +0000

    parallel backend for `foreach` using `spark_apply`
    Signed-off-by: Yitao Li <yitao@rstudio.com>
    
    parallel backend for `foreach` using `spark_apply`
    
    Signed-off-by: Yitao Li <yitao@rstudio.com>
    
    roxygenise
    
    fixing example and namespace issues

---
+++ tests/testthat/test-do-spark.R
@@ -0,0 +1,47 @@
+context("do-spark")
+
+test_requires("foreach")
+test_requires("iterators")
+test_requires("base64enc")
+
+register_test_spark_connection <- function() {
+  sc <- testthat_spark_connection()
+  registerDoSpark(sc)
+}
+
+register_test_spark_connection()
+
+'%test%' <- function(obj, quoted_expr) {
+  res <- list()
+  for (impl in c("do", "dopar"))
+    res[[impl]] <- eval(parse(text = paste(
+      "obj %", impl, "% { ", deparse(quoted_expr), " }", sep = ""
+    )))
+  expect_equal(res$do, res$dopar)
+}
+
+test_that("doSpark works for simple loop", {
+  foreach(x = 1:10) %test% quote(x * x)
+})
+
+test_that("doSpark works for simple loop with combine function", {
+  foreach(x = 1:10, .combine = sum) %test% quote(x * x)
+})
+
+test_that("doSpark works for simple loop of matrices", {
+  foreach(x = 1:10) %test% quote(as.matrix(x))
+})
+
+.test_objs <- list(list(1, "a"), list("b", 4), list(a = 1, b = list(c = 4, d = 5), 6, list(e = list(7))))
+
+test_that("doSpark works for loop with arbitrary R objects", {
+  foreach(x = .test_objs) %test% quote(x)
+})
+
+test_that("doSpark works for loop with arbitrary R objects with combine function", {
+  foreach(x = .test_objs, .combine = c) %test% quote(x)
+})
+
+test_that("doSpark works for loop with arbitrary R objects with multicombine", {
+  foreach(x = 1:20, .combine = list, .multicombine = TRUE, .maxcombine = 5) %test% quote(x)
+})
