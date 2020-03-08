3815b94b69391624fa6570f9e780b7fab9506adf
Yitao Li <yitao@rstudio.com>
2020-03-08 10:02:43 +0000

    fixing the issue I found in https://github.com/sparklyr/sparklyr/issues/2264
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- tests/testthat/test-spark-apply.R
+++ tests/testthat/test-spark-apply.R
@@ -37,3 +37,34 @@ test_that("'spark_apply' works with 'group_by'", {
     }
   )
 })
+
+test_columns_param <- function(cols) {
+  fn <- function(x) {
+    x * x
+  }
+  sdf <- sdf_copy_to(sc, data.frame("x" = c(seq(1.0, 10.0, 1.0))), overwrite = TRUE)
+  res <- spark_apply(sdf, fn, columns = cols) %>% sdf_collect()
+  if (!identical(names(cols), NULL)) {
+    expect_equal(names(res), names(cols))
+    col_name <- names(cols)[[1]]
+  } else {
+    expect_equal(names(res), cols)
+    col_name <- cols[[1]]
+  }
+  expect_equal(nrow(res), 10)
+  for (x in seq(1, 10)) {
+    expect_equal(res[x,][[col_name]], x * x)
+  }
+}
+
+test_that("'spark_apply' works with columns param of type vector", {
+  test_columns_param(cols = c(result = "double"))
+})
+
+test_that("'spark_apply' works with columns param of type string", {
+  test_columns_param(cols = c("result"))
+})
+
+test_that("'spark_apply' works with columns param of type list", {
+  test_columns_param(cols = list(result = "double"))
+})
