3477c48793f7ce3357eb10b25f4f7101aeb561b2
Yitao Li <yitao@rstudio.com>
2020-03-19 01:32:53 +0000

    replace deprecated SynchronizedMap with ConcurrentHashMap
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- java/spark-1.5.2/tracker.scala
+++ java/spark-1.5.2/tracker.scala
@@ -1,34 +1,75 @@
 package sparklyr
 
-import scala.collection.mutable.{Map, SynchronizedMap, HashMap}
+import java.util.NoSuchElementException
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantReadWriteLock
 import scala.language.existentials
 
 class JVMObjectTracker {
 
-  val objMap = new HashMap[String, Object] with
-                             SynchronizedMap[String, Object]
+  val objMap = new ConcurrentHashMap[String, Object]
 
-  var objCounter: Int = 0
-  val lock: AnyRef = new Object()
+  val objCounter = new AtomicInteger()
+  var writerCnt: Int = 0
+  val rwLock = new ReentrantReadWriteLock()
 
   def getObject(id: String): Object = {
-    objMap(id)
+    rwLock.readLock().lock()
+    try {
+      Option(objMap.get(id)) match {
+        case Some(obj) => obj
+        case None => throw new NoSuchElementException()
+      }
+    } finally {
+      rwLock.readLock().unlock()
+    }
   }
 
   def get(id: String): Option[Object] = {
-    objMap.get(id)
+    rwLock.readLock().lock()
+    try {
+      Option(objMap.get(id))
+    } finally {
+      rwLock.readLock().unlock()
+    }
   }
 
   def put(obj: Object): String = {
-    lock.synchronized {
-      val objId = objCounter.toString
-      objCounter = objCounter + 1
+    acquireWriteLock()
+    try {
+      val objId = objCounter.getAndAdd(1).toString
       objMap.put(objId, obj)
       objId
+    } finally {
+      releaseWriteLock()
     }
   }
 
   def remove(id: String): Option[Object] = {
-    objMap.remove(id)
+    acquireWriteLock()
+    try {
+      Option(objMap.remove(id))
+    } finally {
+      releaseWriteLock()
+    }
+  }
+
+  private[this] def acquireWriteLock(): Unit = this.synchronized {
+    writerCnt += 1
+    if (writerCnt == 1) {
+      // first writer should lock objMap for write for itself and for any
+      // subsequent writers
+      rwLock.writeLock().lock()
+    }
+  }
+
+  private[this] def releaseWriteLock(): Unit = this.synchronized {
+    writerCnt -= 1
+    if (writerCnt == 0) {
+      // last writer should release objMap write lock for subsequent readers
+      rwLock.writeLock().unlock()
+    }
   }
 }
