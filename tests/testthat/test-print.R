369df30b6b453dacb05180984ddbcfc9f5adb0d4
Yitao Li <yitao@rstudio.com>
2020-03-27 12:36:09 +0000

    setting up github CI workflow
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -2,16 +2,20 @@ context("print")
 test_requires("dplyr")
 sc <- testthat_spark_connection()
 
+verify_table_src <- function(printed) {
+  expected <- c(
+    "# Source: spark<\\?> \\[\\?\\? x 1\\]",
+    "id"
+  )
+  for (i in seq_along(expected)) {
+    expect_gt(grep(expected[[i]], printed[[i]]), 0)
+  }
+}
+
 test_that("print supports spark tables", {
   printed <- capture.output(print(sdf_len(sc, 11)))
 
-  expect_equal(
-    printed[1:2],
-    c(
-      "# Source: spark<?> [?? x 1]",
-      "      id"
-    )
-  )
+  verify_table_src(printed)
 
   expect_true(grepl("with more rows", printed[14]))
 })
@@ -19,13 +23,7 @@ test_that("print supports spark tables", {
 test_that("can print more than 10 rows from a spark tables", {
   printed <- capture.output(sdf_len(sc, 100) %>% print(n = 50))
 
-  expect_equal(
-    printed[1:2],
-    c(
-      "# Source: spark<?> [?? x 1]",
-      "      id"
-    )
-  )
+  verify_table_src(printed)
 
   expect_true(grepl("1", printed[4]))
   expect_true(grepl("49", printed[52]))
