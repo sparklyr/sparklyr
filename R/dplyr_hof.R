ef01f7766bcd3aaeb395054812152a5d1fe7756b
yl790 <yitao@rstudio.com>
2020-05-01 19:28:29 +0000

    dplyr_hof
    Signed-off-by: yl790 <yitao@rstudio.com>

---
+++ R/dplyr_hof.R
@@ -0,0 +1,203 @@
+#' dplyr wrappers for Apache Spark higher order functions
+#'
+#' These methods implement dplyr grammars for Apache Spark higher order functions
+#'
+#' @name dplyr_hof
+NULL
+
+# throw an error if func is not a valid lambda expression
+validate_lambda <- function(func) {
+  if (! "spark_sql_lambda" %in% class(func))
+    stop("Expected 'func' to be a lambda expression (e.g., 'a %->% (a + 1)' or '.(a, b) %->% (a + b + 1)').")
+}
+
+#' Infix operator for composing a lambda expression
+#'
+#' Infix operator that allows a lambda expression to be composed in R and be
+#' translated to Spark SQL equivalent using ' \code{dbplyr::translate_sql} functionalities
+#'
+#' Notice when composing a lambda expression in R, the body of the lambda expression
+#' *must always be surrounded with parentheses*, otherwise a parsing error will occur.
+#'
+#' @param params Parameter(s) of the lambda expression, can be either a single
+#'   parameter or a comma separated listed of parameters in the form of
+#'   \code{.(param1, param2, ... )} (see examples)
+#' @param body Body of the lambda expression, *must be within parentheses*
+#'
+#' @examples
+#' \dontrun{
+#'
+#' a %->% (mean(a) + 1) # translates to <SQL> `a` -> (AVG(`a`) OVER () + 1.0)
+#'
+#' .(a, b) %->% (a < 1 && b > 1) # translates to <SQL> `a`,`b` -> (`a` < 1.0 AND `b` > 1.0)
+#'}
+#' @export
+`%->%` <- function(params, body) {
+  `.` <- function(...) {
+    params <- do.call(
+      paste,
+      c(lapply(rlang::ensyms(...), as.character), sep = ", ")
+    )
+    paste0("(", params, ")")
+  }
+  process_params <- function(x) {
+    if ("call" %in% class(x))
+      # params is of the form '.(<comma-separated list of variables>)'
+      eval(x)
+    else
+      # params consists of a single variable
+      x
+  }
+
+  params_sql <- rlang::enexpr(params) %>%
+    process_params() %>%
+    lapply(as.character) %>%
+    c(sep = ",") %>%
+    do.call(paste, .)
+
+  body_sql <- dbplyr::translate_sql_(
+    rlang::enexprs(body),
+    con = dbplyr::simulate_dbi()
+  )
+
+  lambda <- dbplyr::sql(paste(params_sql, "->", body_sql))
+  class(lambda) <- c(class(lambda), "spark_sql_lambda")
+
+  lambda
+}
+
+do.mutate <- function(x, dest_col_name, sql, ...) {
+  args <- list(dbplyr::sql(sql))
+  names(args) <- as.character(dest_col_name)
+
+  do.call(dplyr::mutate, c(list(x), args, list(...)))
+}
+
+#' dplyr wrapper for \code{transform(array<T>, function<T, U>): array<U>} and
+#' \code{transform(array<T>, function<T, Int, U>): array<U>} in Spark SQL
+#'
+#' @param x The Spark data frame to transform
+#' @param dest_col Column to store the transformed result
+#' @param expr The array being transformed (could be any SQL expression evaluating to an array)
+#' @param func The transformation to apply
+#' @param ... Additional params to dplyr::mutate
+#'
+#' @export
+hof_transform <- function(x, dest_col, expr, func, ...) {
+  validate_lambda(func)
+  sql <- paste(
+    "TRANSFORM(",
+    as.character(dbplyr::translate_sql(!! rlang::enexpr(expr))),
+    ",",
+    as.character(func),
+    ")"
+  )
+
+  do.mutate(x, rlang::ensym(dest_col), sql, ...)
+}
+
+#' dplyr wrapper for \code{filter(array<T>, function<T, Boolean>): array<T>} in Spark SQL
+#'
+#' @param x The Spark data frame to filter
+#' @param dest_col Column to store the filtered result
+#' @param expr The array being filtered (could be any SQL expression evaluating to an array)
+#' @param func The filtering function
+#' @param ... Additional params to dplyr::mutate
+#'
+#' @export
+hof_filter <- function(x, dest_col, expr, func, ...) {
+  validate_lambda(func)
+  sql <- paste(
+    "FILTER(",
+    as.character(dbplyr::translate_sql(!! rlang::enexpr(expr))),
+    ",",
+    as.character(func),
+    ")"
+  )
+
+  do.mutate(x, rlang::ensym(dest_col), sql, ...)
+}
+
+#' dplyr wrapper for \code{aggregate(array<T>, A, function<A, T, A>[, function<A, R>]): R}
+#'
+#' @param x The Spark data frame to run aggregation on
+#' @param dest_col Column to store the aggregated result
+#' @param expr The array being aggregated (could be any SQL expression evaluating to an array)
+#' @param start The starting value of the aggregation
+#' @param merge The aggregation function
+#' @param finish Optional param specifying a transformation to apply on the final value of the aggregation
+#' @param ... Additional params to dplyr::mutate
+#'
+#' @export
+hof_aggregate <- function(x, dest_col, expr, start, merge, ...) {
+  validate_lambda(merge)
+  args <- list(...)
+  finish <- NULL
+  if ("finish" %in% names(args)) {
+    finish <- args$finish
+    if (!identical(finish, NULL)) validate_lambda(finish)
+    args <- args[!names(args) %in% "finish"]
+  }
+  sql <- do.call(paste, as.list(c(
+    "AGGREGATE(",
+    as.character(dbplyr::translate_sql(!! rlang::enexpr(expr))),
+    ",",
+    as.character(dbplyr::translate_sql(!! rlang::enexpr(start))),
+    ",",
+    as.character(merge),
+    if (identical(finish, NULL)) NULL else c(",", as.character(finish)),
+    ")"
+  )))
+
+  do.call(
+    do.mutate,
+    c(list(x, as.character(rlang::ensym(dest_col)), sql), args)
+  )
+}
+
+#' dplyr wrapper for \code{exists(array<T>, function<T, Boolean>): Boolean}
+#'
+#' @param x The Spark data frame to search
+#' @param dest_col Column to store the search result
+#' @param expr The array being searched (could be any SQL expression evaluating to an array)
+#' @param pred A boolean predicate
+#' @param ... Additional params to dplyr::mutate
+#'
+#' @export
+hof_exists <- function(x, dest_col, expr, pred, ...) {
+  validate_lambda(pred)
+  sql <- paste(
+    "EXISTS(",
+    as.character(dbplyr::translate_sql(!! rlang::enexpr(expr))),
+    ",",
+    as.character(pred),
+    ")"
+  )
+
+  do.mutate(x, rlang::ensym(dest_col), sql, ...)
+}
+
+#' dplyr wrapper for \code{zip_with(array<T>, array<U>, function<T, U, R>): array<R>}
+#'
+#' @param x The Spark data frame to process
+#' @param dest_col Column to store the query result
+#' @param left Any expression evaluating to an array
+#' @param right Any expression evaluating to an array
+#' @param func Element-wise merge function to be applied
+#' @param ... Additional params to dplyr::mutate
+#'
+#' @export
+hof_zip_with <- function(x, dest_col, left, right, func, ...) {
+  validate_lambda(func)
+  sql <- paste(
+    "ZIP_WITH(",
+    as.character(dbplyr::translate_sql(!! rlang::enexpr(left))),
+    ",",
+    as.character(dbplyr::translate_sql(!! rlang::enexpr(right))),
+    ",",
+    as.character(func),
+    ")"
+  )
+
+  do.mutate(x, rlang::ensym(dest_col), sql, ...)
+}
---
+++ tests/testthat/test-dplyr-hof.R
@@ -0,0 +1,362 @@
+context("Spark SQL higher-order function wrappers for dplyr")
+
+test_requires("dplyr")
+sc <- testthat_spark_connection()
+test_tbl <- testthat_tbl(
+  name = "hof_test_data",
+  data = tibble::tibble(
+    v = c(11, 12),
+    x = list(c(1, 2, 3, 4, 5), c(6, 7, 8, 9, 10)),
+    y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8))
+  )
+)
+
+expect_data_equal <- function(actual, expected) {
+  expect_equal(colnames(actual), colnames(expected))
+  for (col in colnames(expected))
+    expect_equal(actual[[col]], expected[[col]])
+}
+
+test_that("'hof_transform' creating a new column", {
+  test_requires_version("2.4.0")
+  sq <- test_tbl %>%
+    hof_transform(dest_col = z, expr = x, func = x %->% (x * x)) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    sq,
+    tibble::tibble(
+      v = c(11, 12),
+      x = list(c(1, 2, 3, 4, 5), c(6, 7, 8, 9, 10)),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8)),
+      z = list(c(1, 4, 9, 16, 25), c(36, 49, 64, 81, 100))
+    )
+  )
+})
+
+test_that("'hof_transform' overwriting an existing column", {
+  test_requires_version("2.4.0")
+  sq <- test_tbl %>%
+    hof_transform(dest_col = x, expr = x, func = x %->% (x * x)) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    sq,
+    tibble::tibble(
+      v = c(11, 12),
+      x = list(c(1, 4, 9, 16, 25), c(36, 49, 64, 81, 100)),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8))
+    )
+  )
+})
+
+test_that("'hof_transform' works with array(...) expression", {
+  test_requires_version("2.4.0")
+  sq <- test_tbl %>%
+    hof_transform(dest_col = v, expr = array(v - 9, v - 8), func = x %->% (x * x)) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    sq,
+    tibble::tibble(
+      v = list(c(4, 9), c(9, 16)),
+      x = list(c(1, 2, 3, 4, 5), c(6, 7, 8, 9, 10)),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8))
+    )
+  )
+})
+
+test_that("'hof_filter' creating a new column", {
+  test_requires_version("2.4.0")
+  filtered <- test_tbl %>%
+    hof_filter(dest_col = mod_3_is_0_or_1, expr = x, func = x %->% (x %% 3 != 2)) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    filtered,
+    tibble::tibble(
+      v = c(11, 12),
+      x = list(c(1, 2, 3, 4, 5), c(6, 7, 8, 9, 10)),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8)),
+      mod_3_is_0_or_1 = list(c(1, 3, 4), c(6, 7, 9, 10))
+    )
+  )
+})
+
+test_that("'hof_filter' overwriting an existing column", {
+  test_requires_version("2.4.0")
+  filtered <- test_tbl %>%
+    hof_filter(dest_col = x, expr = x, func = x %->% (x %% 3 != 2)) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    filtered,
+    tibble::tibble(
+      v = c(11, 12),
+      x = list(c(1, 3, 4), c(6, 7, 9, 10)),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8))
+    )
+  )
+})
+
+test_that("'hof_filter' works with array(...) expression", {
+  test_requires_version("2.4.0")
+  filtered <- test_tbl %>%
+    hof_filter(dest_col = v, expr = array(8, v - 1, v + 1), func = x %->% (x %% 3 == 2)) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    filtered,
+    tibble::tibble(
+      v = list(c(8), c(8, 11)),
+      x = list(c(1, 2, 3, 4, 5), c(6, 7, 8, 9, 10)),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8))
+    )
+  )
+})
+
+test_that("'hof_aggregate' creating a new column", {
+  test_requires_version("2.4.0")
+  agg <- test_tbl %>%
+    hof_aggregate(
+      dest_col = sum,
+      expr = x,
+      start = v,
+      merge = .(sum_so_far, num) %->% (sum_so_far + num)
+    ) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    agg,
+    tibble::tibble(
+      v = c(11, 12),
+      x = list(c(1, 2, 3, 4, 5), c(6, 7, 8, 9, 10)),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8)),
+      sum = c(26, 52)
+    )
+  )
+})
+
+test_that("'hof_aggregate' overwriting an existing column", {
+  test_requires_version("2.4.0")
+  agg <- test_tbl %>%
+    hof_aggregate(
+      dest_col = x,
+      expr = x,
+      start = v,
+      merge = .(sum_so_far, num) %->% (sum_so_far + num)
+    ) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    agg,
+    tibble::tibble(
+      v = c(11, 12),
+      x = c(26, 52),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8))
+    )
+  )
+})
+
+test_that("'hof_aggregate' works with array(...) expression", {
+  test_requires_version("2.4.0")
+  agg <- test_tbl %>%
+    hof_aggregate(
+      dest_col = sum,
+      expr = array(1, 2, v, 3),
+      start = v,
+      merge = .(sum_so_far, num) %->% (sum_so_far + num)
+    ) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    agg,
+    tibble::tibble(
+      v = c(11, 12),
+      x = list(c(1, 2, 3, 4, 5), c(6, 7, 8, 9, 10)),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8)),
+      sum = c(28, 30)
+    )
+  )
+})
+
+test_that("'hof_aggregate' applies 'finish' transformation correctly", {
+  test_requires_version("2.4.0")
+  agg <- test_tbl %>%
+    hof_aggregate(
+      dest_col = x,
+      expr = x,
+      start = v,
+      merge = .(sum_so_far, num) %->% (sum_so_far + num),
+      finish = sum %->% (sum * sum)
+    ) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    agg,
+    tibble::tibble(
+      v = c(11, 12),
+      x = c(676, 2704),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8))
+    )
+  )
+})
+
+test_that("'hof_exists' creating a new column", {
+  test_requires_version("2.4.0")
+  res <- test_tbl %>%
+    hof_exists(
+      dest_col = found,
+      expr = x,
+      pred = num %->% (num == 5)
+    ) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    res,
+    tibble::tibble(
+      v = c(11, 12),
+      x = list(c(1, 2, 3, 4, 5), c(6, 7, 8, 9, 10)),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8)),
+      found = c(TRUE, FALSE)
+    )
+  )
+})
+
+test_that("'hof_exists' overwriting an existing column", {
+  test_requires_version("2.4.0")
+  res <- test_tbl %>%
+    hof_exists(
+      dest_col = x,
+      expr = x,
+      pred = num %->% (num == 5)
+    ) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    res,
+    tibble::tibble(
+      v = c(11, 12),
+      x = c(TRUE, FALSE),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8)),
+    )
+  )
+})
+
+test_that("'hof_exists' works with array(...) expression", {
+  test_requires_version("2.4.0")
+  res <- test_tbl %>%
+    hof_exists(
+      dest_col = x,
+      expr = array(10, v, 13, 14),
+      pred = num %->% (num == 12)
+    ) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    res,
+    tibble::tibble(
+      v = c(11, 12),
+      x = c(FALSE, TRUE),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8)),
+    )
+  )
+
+  res <- test_tbl %>%
+    hof_exists(
+      dest_col = x,
+      expr = array(10, v, 13, 14),
+      pred = num %->% (num == 10)
+    ) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    res,
+    tibble::tibble(
+      v = c(11, 12),
+      x = c(TRUE, TRUE),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8))
+    )
+  )
+
+  res <- test_tbl %>%
+    hof_exists(
+      dest_col = x,
+      expr = array(10, v, 13, 14),
+      pred = num %->% (num == 17)
+    ) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    res,
+    tibble::tibble(
+      v = c(11, 12),
+      x = c(FALSE, FALSE),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8))
+    )
+  )
+})
+
+test_that("'hof_zip_with' creating a new column", {
+  test_requires_version("2.4.0")
+  res <- test_tbl %>%
+    hof_zip_with(
+      dest_col = product,
+      left = x,
+      right = y,
+      func = .(x, y) %->% (x * y)
+    ) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    res,
+    tibble::tibble(
+      v = c(11, 12),
+      x = list(c(1, 2, 3, 4, 5), c(6, 7, 8, 9, 10)),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8)),
+      product = list(c(1, 8, 6, 32, 25), c(42, 7, 32, 18, 80))
+    )
+  )
+})
+
+test_that("'hof_zip_with' overwriting an existing column", {
+  test_requires_version("2.4.0")
+  res <- test_tbl %>%
+    hof_zip_with(
+      dest_col = x,
+      left = x,
+      right = y,
+      func = .(x, y) %->% (x * y)
+    ) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    res,
+    tibble::tibble(
+      v = c(11, 12),
+      x = list(c(1, 8, 6, 32, 25), c(42, 7, 32, 18, 80)),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8))
+    )
+  )
+})
+
+test_that("'hof_zip_with' works with array(...) expression", {
+  test_requires_version("2.4.0")
+  res <- test_tbl %>%
+    hof_zip_with(
+      dest_col = x,
+      left = array(3, 1, v, 4),
+      right = array(2, v, 5, 17),
+      func = .(x, y) %->% (x * y)
+    ) %>%
+    sdf_collect()
+
+  expect_data_equal(
+    res,
+    tibble::tibble(
+      v = c(11, 12),
+      x = list(c(6, 11, 55, 68), c(6, 12, 60, 68)),
+      y = list(c(1, 4, 2, 8, 5), c(7, 1, 4, 2, 8))
+    )
+  )
+})
