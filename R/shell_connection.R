5e5d7ce5cf0521b4ea33617722e0b0c39836f6bd
yl790 <yitao@rstudio.com>
2020-05-14 12:26:21 +0000

    revising
    Signed-off-by: yl790 <yitao@rstudio.com>

--- R/shell_connection.R
+++ R/shell_connection.R
@@ -101,7 +101,7 @@ spark_session_random <- function() {
   floor(openssl::rand_num(1) * 100000)
 }
 
-abort_shell <- function(message, spark_submit_path, shell_args, output_file, error_file, stack_trace = NULL) {
+abort_shell <- function(message, spark_submit_path, shell_args, output_file, error_file) {
   withr::with_options(list(
     warning.length = 8000
   ), {
@@ -138,10 +138,9 @@ abort_shell <- function(message, spark_submit_path, shell_args, output_file, err
       )
     )
   })
-  if (!identical(stack_trace, NULL)) {
-    print("\nLocal stack trace:\n")
-    print(stack_trace)
-  }
+
+  print("\nLocal stack trace:\n")
+  print(sys.calls())
 }
 
 # Start the Spark R Shell
@@ -391,8 +390,7 @@ start_shell <- function(master,
         spark_submit_path,
         shell_args,
         output_file,
-        error_file,
-        stack_trace = sys.calls()
+        error_file
       )
     })
   }
@@ -426,8 +424,7 @@ start_shell <- function(master,
       spark_submit_path,
       shell_args,
       output_file,
-      error_file,
-      stack_trace = sys.calls()
+      error_file
     )
   })
 
@@ -728,8 +725,7 @@ initialize_connection.spark_shell_connection <- function(sc) {
       spark_submit_path = NULL,
       shell_args = NULL,
       output_file = sc$output_file,
-      error_file = NULL,
-      stack_trace = sys.calls()
+      error_file = NULL
     )
   })
 }
