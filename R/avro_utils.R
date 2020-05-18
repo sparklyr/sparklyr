aa38cabc512d46d175b0e99c73295a742db31585
yl790 <yitao@rstudio.com>
2020-05-18 12:26:38 +0000

    revert temp workaround
    Signed-off-by: yl790 <yitao@rstudio.com>

--- R/avro_utils.R
+++ R/avro_utils.R
@@ -8,9 +8,6 @@ spark_avro_package_name <- function(spark_version) {
 
   if (spark_version < "2.4.0") stop("Avro requires Spark 2.4.0 or newer")
 
-  # temporary workaround for databricks connect use case
-  if (identical(spark_version, "2.4.6-SNAPSHOT")) spark_version <- "2.4.5"
-
   scala_version <- if (spark_version >= "3.0.0") "2.12" else "2.11"
   paste0(
     "org.apache.spark:spark-avro_",
