9d0e254c1ebe5f2880ef3ba7ddfe15a4ef748c05
yl790 <yitao@rstudio.com>
2020-05-19 12:39:14 +0000

    revise apply_config
    Signed-off-by: yl790 <yitao@rstudio.com>

--- R/spark_hive.R
+++ R/spark_hive.R
@@ -5,9 +5,11 @@ create_hive_context <- function(sc) {
 apply_config <- function(object, params, method, prefix) {
   if (!length(params)) return(object)
 
-  params %>%
+  params <- params %>%
     purrr::map_if(is.logical, ~ (if (.x) "true" else "false")) %>%
     purrr::map_chr(as.character) %>%
     purrr::imap(~ list(key = .y, value = .x)) %>%
-    purrr::reduce(~ invoke(.x, method, paste0(prefix, .y$key), .y$value), .init = object)
+    purrr::reduce(~ append(.x, list(list(method, paste0(prefix, .y$key), .y$value))), .init = list())
+
+  do.call(invoke, c(object, "%>%", params))
 }
