e62badac37647390b312a07a733297fece5620bc
Neal Richardson <neal.p.richardson@gmail.com>
2020-05-19 16:23:13 -0700

    Update embedded_sources
    Signed-off-by: Neal Richardson <neal.p.richardson@gmail.com>

--- java/embedded_sources.R
+++ java/embedded_sources.R
@@ -1,10 +1,30 @@
 # Changing this file requires running configure.R to rebuild sources and jars.
 
-arrow_write_record_batch <- function(df) {
-  record_batch <- get("record_batch", envir = as.environment(asNamespace("arrow")))
+# Someday this setting may depend on the Spark version
+arrow_env_vars <- list(ARROW_PRE_0_15_IPC_FORMAT = 1)
 
+get_arrow_function <- function(x) get(x, as.environment(asNamespace("arrow")))
+
+arrow_write_record_batch <- function(df) {
+  if ("write_to_raw" %in% ls(envir = asNamespace("arrow"))) {
+    # New in arrow 0.17: takes a data.frame and returns a raw buffer with Arrow data
+    write_to_raw <- get_arrow_function("write_to_raw")
+    # Also new in 0.17: arrow doesn't hardcode a GMT timezone anymore
+    # so set the local timezone to any POSIXt columns that don't have one set
+    # https://github.com/sparklyr/sparklyr/issues/2439
+    df[] <- lapply(df, function(x) {
+      if (inherits(x, "POSIXt") && is.null(attr(x, "tzone"))) {
+        attr(x, "tzone") <- Sys.timezone()
+      }
+      x
+    })
+
+    return(write_to_raw(df, format = "stream"))
+  }
+
+  record_batch <- get_arrow_function("record_batch")
   if (packageVersion("arrow") < "0.12") {
-    write_record_batch <- get("write_record_batch", envir = as.environment(asNamespace("arrow")))
+    write_record_batch <- get_arrow_function("write_record_batch")
 
     record <- record_batch(df)
     write_record_batch(record, raw())
@@ -12,77 +32,57 @@ arrow_write_record_batch <- function(df) {
   else if (packageVersion("arrow") <= "0.13") {
     record <- record_batch(df)
 
-    write_arrow <- get("write_arrow", envir = as.environment(asNamespace("arrow")))
+    write_arrow <- get_arrow_function("write_arrow")
     write_arrow(record, raw())
   }
   else {
-    environment <- list()
-    if (packageVersion("arrow") > "0.14") {
-      environment <- list(ARROW_PRE_0_15_IPC_FORMAT = 1)
-    }
-
-    withr::with_envvar(environment, {
+    withr::with_envvar(arrow_env_vars, {
       record <- record_batch(!!!df)
 
-      write_arrow <- get("write_arrow", envir = as.environment(asNamespace("arrow")))
+      write_arrow <- get_arrow_function("write_arrow")
       write_arrow(record, raw())
     })
   }
 }
 
 arrow_record_stream_reader <- function(stream) {
-  environment <- list()
-
   if (packageVersion("arrow") < "0.12") {
-    record_batch_stream_reader <- get("record_batch_stream_reader", envir = as.environment(asNamespace("arrow")))
+    record_batch_stream_reader <- get_arrow_function("record_batch_stream_reader")
   }
   else {
-    record_batch_stream_reader <- get("RecordBatchStreamReader", envir = as.environment(asNamespace("arrow")))
+    record_batch_stream_reader <- get_arrow_function("RecordBatchStreamReader")
   }
 
   if (packageVersion("arrow") > "0.14") {
     record_batch_stream_reader <- record_batch_stream_reader$create
-    environment <- list(ARROW_PRE_0_15_IPC_FORMAT = 1)
   }
 
-  withr::with_envvar(environment, {
+  withr::with_envvar(arrow_env_vars, {
     record_batch_stream_reader(stream)
   })
 }
 
 arrow_read_record_batch <- function(reader) {
-  environment <- list()
-
   if (packageVersion("arrow") < "0.12") {
-    read_record_batch <- get("read_record_batch", envir = as.environment(asNamespace("arrow")))
+    read_record_batch <- get_arrow_function("read_record_batch")
   }
   else {
     read_record_batch <- function(reader) reader$read_next_batch()
   }
 
-  if (packageVersion("arrow") > "0.14") {
-    environment <- list(ARROW_PRE_0_15_IPC_FORMAT = 1)
-  }
-
-  withr::with_envvar(environment, {
+  withr::with_envvar(arrow_env_vars, {
     read_record_batch(reader)
   })
 }
 
 arrow_as_tibble <- function(record) {
-  environment <- list()
-
-  if (packageVersion("arrow") <= "0.13")
+if (packageVersion("arrow") <= "0.13")
     as_tibble <- get("as_tibble", envir = as.environment(asNamespace("arrow")))
   else {
     as_tibble <- get("as.data.frame", envir = as.environment(asNamespace("arrow")))
   }
 
-  if (packageVersion("arrow") > "0.14") {
-    environment <- list(ARROW_PRE_0_15_IPC_FORMAT = 1)
-  }
-
-  withr::with_envvar(environment, {
+  withr::with_envvar(arrow_env_vars, {
     as_tibble(record)
   })
 }
