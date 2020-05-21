796cf0511228f0ad19b7b109755c4929935c38d7
yl790 <yitao@rstudio.com>
2020-05-21 11:50:41 +0000

    revise ci/install_r_dependencies.sh
    Signed-off-by: yl790 <yitao@rstudio.com>

--- ci/install_r_dependencies.sh
+++ ci/install_r_dependencies.sh
@@ -2,9 +2,33 @@
 
 export MAKEFLAGS="-j$(($(nproc) + 1))"
 
+TEST_DEPS="broom \
+           diffobj \
+           foreach \
+           ggplot2 \
+           iterators \
+           janeaustenr \
+           Lahman \
+           mlbench \
+           nnet \
+           nycflights13 \
+           R6 \
+           RCurl \
+           reshape2 \
+           shiny \
+           stringr \
+           testthat"
+SEP='"\\s+"'
+
 R_REMOTES_NO_ERRORS_FROM_WARNINGS=true Rscript <(
 cat << _RSCRIPT_EOF_
   install.packages("devtools")
-  devtools::install_deps()
+  devtools::install_deps(dependencies = c("Imports"))
+
+  test_deps <- strsplit("$TEST_DEPS", $SEP)[[1]]
+  for (pkg in test_deps)
+    if (!require(pkg, character.only = TRUE))
+      install.packages(pkg)
+
 _RSCRIPT_EOF_
 )
