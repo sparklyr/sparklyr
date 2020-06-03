5fa85c6937e682ebc38148b9267453a09d98f8d9
yl790 <yitao@rstudio.com>
2020-06-03 17:25:25 +0000

    update_embedded_sources.R should also sync java/embedded_sources.R
    Signed-off-by: yl790 <yitao@rstudio.com>

--- R/spark_update_embedded_sources.R
+++ R/spark_update_embedded_sources.R
@@ -11,6 +11,9 @@ spark_update_embedded_sources <- function(jars_to_skip = c()) {
   ensure_directory(srcs_dir)
   srcs <- file.path(srcs_dir, "embedded_sources.R")
   spark_gen_embedded_sources(output = srcs)
+  # apply the same update to all copies in sparklyr-*.jar and also to the copy
+  # inside java/ to avoid confusion
+  file.copy(from = srcs, to = file.path(pkg_root, "java"), overwrite = TRUE)
 
   sparklyr_jars <- list_sparklyr_jars()
 
