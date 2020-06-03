ba38dcc917dfdde6aaa7d10b891b4829c260c726
Andy Zhang <yue.zhang@databricks.com>
2020-06-02 18:47:02 -0700

    remove useless newline
    Signed-off-by: Andy Zhang <yue.zhang@databricks.com>

--- R/databricks_connection.R
+++ R/databricks_connection.R
@@ -62,7 +62,6 @@ new_databricks_connection <- function(scon, guid) {
     scon,
     class = "databricks_connection"
   )
-
   # In databricks, sparklyr should use the SqlContext associated with the RDriverLocal instance for
   # this guid.
   r_driver_local <- "com.databricks.backend.daemon.driver.RDriverLocal"
