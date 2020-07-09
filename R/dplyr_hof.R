dc2e9f5bc4a1fcda17c5b5c46bf75a1bea947b79
yl790 <yitao@rstudio.com>
2020-07-09 13:48:25 +0000

    transform_keys + example
    Signed-off-by: yl790 <yitao@rstudio.com>

--- R/dplyr_hof.R
+++ R/dplyr_hof.R
@@ -520,3 +520,49 @@ hof_forall <- function(
 
   do.mutate(x, dest_col, sql, ...)
 }
+
+#' Transforms keys of a map
+#'
+#' Applies the transformation function specified to all keys of a map
+#' (this is essentially a dplyr wrapper to the `transform_keys(expr, func)` higher-
+#' order function, which is supported since Spark 3.0)
+#'
+#' @param x The Spark data frame to be processed
+#' @param func The transformation function to apply (it should take (key, value) as
+#'  arguments and return a transformed key)
+#' @param expr The map being transformed, could be any SQL expression evaluating to a map
+#'  (default: the last column of the Spark data frame)
+#' @param dest_col Column to store the transformed result (default: expr)
+#' @param ... Additional params to dplyr::mutate
+#'
+#' @examples
+#' \dontrun{
+#'
+#' library(sparklyr)
+#' sc <- spark_connect(master = "local", version = "3.0.0")
+#' sdf <- sdf_len(sc, 1) %>% dplyr::mutate(m = map("a", 0L, "b", 2L, "c", -1L))
+#' transformed_sdf <- sdf %>% hof_transform_keys(~ CONCAT(.x, " == ", .y))
+#' }
+#'
+#' @export
+hof_transform_keys <- function(
+  x,
+  func,
+  expr = NULL,
+  dest_col = NULL,
+  ...
+) {
+  func <- process_lambda(func)
+  expr <- process_expr(x, rlang::enexpr(expr))
+  dest_col <- process_dest_col(expr, rlang::enexpr(dest_col))
+
+  sql <- paste(
+    "TRANSFORM_KEYS(",
+    as.character(dbplyr::translate_sql(!! expr)),
+    ",",
+    as.character(func),
+    ")"
+  )
+
+  do.mutate(x, dest_col, sql, ...)
+}
