3c003bdf5f36e002c5205e513955e29689038591
Yitao Li <yitao@rstudio.com>
2020-11-11 17:03:43 +0000

    implement Spark SQL equivalent of the `weighted.mean` functionality (#2798)
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- tests/testthat/test-dplyr.R
+++ tests/testthat/test-dplyr.R
@@ -74,6 +74,41 @@ test_that("grepl works as expected", {
   }
 })
 
+test_that("weighted.mean works as expected", {
+  df <- tibble::tibble(
+    x = c(NA_real_, 3.1, 2.2, NA_real_, 3.3, 4),
+    w = c(NA_real_, 1, 0.5, 1, 0.75, NA_real_)
+  )
+  sdf <- copy_to(sc, df, overwrite = TRUE)
+
+  expect_equal(
+    sdf %>% dplyr::summarize(wm = weighted.mean(x, w)) %>% dplyr::pull(wm),
+    df %>%
+      dplyr::summarize(
+        wm = sum(w * x, na.rm = TRUE) /
+             sum(w * as.numeric(!is.na(x)), na.rm = TRUE)
+      ) %>%
+      dplyr::pull(wm)
+  )
+
+  df <- tibble::tibble(
+    x = rep(c(NA_real_, 3.1, 2.2, NA_real_, 3.3, 4), 3L),
+    w = rep(c(NA_real_, 1, 0.5, 1, 0.75, NA_real_), 3L),
+    grp = c(rep(1L, 6L), rep(2L, 6L), rep(3L, 6L))
+  )
+  sdf <- copy_to(sc, df, overwrite = TRUE)
+
+  expect_equal(
+    sdf %>% dplyr::summarize(wm = weighted.mean(x, w)) %>% dplyr::pull(wm),
+    df %>%
+      dplyr::summarize(
+        wm = sum(w * x, na.rm = TRUE) /
+             sum(w * as.numeric(!is.na(x)), na.rm = TRUE)
+      ) %>%
+      dplyr::pull(wm)
+  )
+})
+
 test_that("'head' uses 'limit' clause", {
   test_requires("dplyr")
   test_requires("dbplyr")
