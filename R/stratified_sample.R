b96003afb220f937cb255bc97af6c6d4756c7d77
Yitao Li <yitao@rstudio.com>
2020-11-23 19:46:43 +0000

    support stratified sampling for dplyr::sample_frac
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- R/stratified_sample.R
+++ R/stratified_sample.R
@@ -1,7 +1,7 @@
 #' @include sdf_interface.R
 NULL
 
-sdf_stratified_sample <- function(x, grps, k, weight, replace, seed) {
+sdf_stratified_sample_n <- function(x, grps, k, weight, replace, seed) {
   sc <- spark_connection(x)
   if (spark_version(sc) < "3.0.0") {
     stop("Stratified sampling is only supported with Spark 3.0 or higher")
@@ -20,6 +20,31 @@ sdf_stratified_sample <- function(x, grps, k, weight, replace, seed) {
     as.list(grps),
     weight,
     as.integer(k),
+    as.integer(seed %||% Sys.time())
+  ) %>%
+    invoke(hive_context(sc), "createDataFrame", ., schema) %>%
+    sdf_register()
+}
+
+sdf_stratified_sample_frac <- function(x, grps, frac, weight, replace, seed) {
+  sc <- spark_connection(x)
+  if (spark_version(sc) < "3.0.0") {
+    stop("Stratified sampling is only supported with Spark 3.0 or higher")
+  }
+
+  method <- ifelse(
+    replace, "sampleFracWithReplacement", "sampleFracWithoutReplacement"
+  )
+  schema <- x %>% spark_dataframe() %>% invoke("schema")
+
+  invoke_static(
+    sc,
+    "sparklyr.StratifiedSamplingUtils",
+    method,
+    spark_dataframe(x),
+    as.list(grps),
+    weight,
+    frac,
     as.integer(seed %||% Sys.time())
   ) %>%
     invoke(hive_context(sc), "createDataFrame", ., schema) %>%
--- tests/testthat/test-stratified-sampling.R
+++ tests/testthat/test-stratified-sampling.R
@@ -29,10 +29,11 @@ sdf <- testthat_tbl(
 )
 
 sample_sz <- 20L
+sample_frac <- 0.12
 num_sampling_iters <- 100L
 alpha <- 0.05
 
-verify_results <- function(weighted, replacement) {
+verify_sample_n_results <- function(weighted, replacement) {
   expected_dist <- rep(0L, sample_space_sz + num_zeroes)
   actual_dist <- rep(0L, sample_space_sz + num_zeroes)
 
@@ -73,28 +74,72 @@ verify_results <- function(weighted, replacement) {
   expect_gte(res$p.value, alpha)
 }
 
+verify_sample_frac_results <- function(weighted, replacement) {
+  expected_dist <- rep(0L, sample_space_sz + num_zeroes)
+  actual_dist <- rep(0L, sample_space_sz + num_zeroes)
+
+  for (x in seq(num_sampling_iters)) {
+    set.seed(142857L + x)
+
+    args <- list(
+      size = sample_frac,
+      weight = if (weighted) as.symbol("weight") else NULL,
+      replace = replacement
+    )
+    sample <- sampling_test_data %>%
+      dplyr::group_by(group) %>>%
+      dplyr::sample_frac %@% args
+    for (id in sample$id) {
+      expected_dist[[id]] <- expected_dist[[id]] + 1L
+    }
+
+    sample <- sdf %>%
+      dplyr::group_by(group) %>>%
+      dplyr::sample_frac %@% args %>%
+      collect()
+    for (id in sample$id) {
+      actual_dist[[id]] <- actual_dist[[id]] + 1L
+    }
+
+    expect_equal(
+      sample %>%
+        dplyr::group_by(group) %>%
+        dplyr::summarize(n = n()) %>%
+        dplyr::pull(n),
+      rep(ceiling((sample_space_sz + num_zeroes) * sample_frac), num_groups)
+    )
+  }
+  res <- ks.test(x = actual_dist, y = expected_dist)
+
+  expect_gte(res$p.value, alpha)
+}
+
 test_that("stratified sampling without replacement works as expected", {
   test_requires_version("3.0.0")
 
-  verify_results(weighted = FALSE, replacement = FALSE)
+  verify_sample_n_results(weighted = FALSE, replacement = FALSE)
+  verify_sample_frac_results(weighted = FALSE, replacement = FALSE)
 })
 
 test_that("stratified sampling with replacement works as expected", {
   test_requires_version("3.0.0")
 
-  verify_results(weighted = FALSE, replacement = TRUE)
+  verify_sample_n_results(weighted = FALSE, replacement = TRUE)
+  verify_sample_frac_results(weighted = FALSE, replacement = TRUE)
 })
 
 test_that("stratified weighted sampling without replacement works as expected", {
   test_requires_version("3.0.0")
 
-  verify_results(weighted = TRUE, replacement = FALSE)
+  verify_sample_n_results(weighted = TRUE, replacement = FALSE)
+  verify_sample_frac_results(weighted = TRUE, replacement = FALSE)
 })
 
 test_that("stratified weighted sampling with replacement works as expected", {
   test_requires_version("3.0.0")
 
-  verify_results(weighted = TRUE, replacement = TRUE)
+  verify_sample_n_results(weighted = TRUE, replacement = TRUE)
+  verify_sample_frac_results(weighted = TRUE, replacement = TRUE)
 })
 
 test_that("stratified sampling returns repeatable results from a fixed PRNG seed", {
@@ -114,6 +159,27 @@ test_that("stratified sampling returns repeatable results from a fixed PRNG seed
           sdf %>%
             dplyr::group_by(group) %>>%
             dplyr::sample_n %@% args %>%
+            collect()
+        }
+      )
+
+      expect_equivalent(
+        samples[[1]] %>% dplyr::arrange(id),
+        samples[[2]] %>% dplyr::arrange(id)
+      )
+
+      args <- list(
+        size = sample_frac,
+        weight = if (weighted) as.symbol("weight") else NULL,
+        replace = replacement
+      )
+      samples <- lapply(
+        seq(2),
+        function(x) {
+          set.seed(142857L)
+          sdf %>%
+            dplyr::group_by(group) %>>%
+            dplyr::sample_frac %@% args %>%
             collect()
         }
       )
