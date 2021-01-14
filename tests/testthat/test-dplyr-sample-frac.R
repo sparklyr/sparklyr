ac9cdb52ad4d30ab2302edcd1a40f7de77edb339
Yitao Li <yitao@rstudio.com>
2021-01-14 22:36:34 +0000

    split dplyr sampling tests into separate files (#2897)
    Signed-off-by: Yitao Li <yitao@rstudio.com>

---
+++ tests/testthat/test-dplyr-sample-frac.R
@@ -0,0 +1,27 @@
+context("dplyr-sample-frac")
+
+sc <- testthat_spark_connection()
+
+iris_tbl <- testthat_tbl("iris")
+test_requires("dplyr")
+
+test_that("'sample_frac' works as expected", {
+  test_requires_version("2.0.0")
+  skip_livy()
+  test_requires("dplyr")
+
+  for (weight in list(NULL, rlang::sym("Petal_Length"))) {
+    for (replace in list(FALSE, TRUE)) {
+      sample_sdf <- iris_tbl %>%
+        sample_frac(0.2, weight = !!weight, replace = replace)
+      expect_equal(colnames(sample_sdf), colnames(iris_tbl))
+      expect_equal(sample_sdf %>% collect() %>% nrow(), round(0.2 * nrow(iris)))
+
+      sample_sdf <- iris_tbl %>%
+        select(Petal_Length) %>%
+        sample_frac(0.2, weight = !!weight, replace = replace)
+      expect_equal(colnames(sample_sdf), "Petal_Length")
+      expect_equal(sample_sdf %>% collect() %>% nrow(), round(0.2 * nrow(iris)))
+    }
+  }
+})
