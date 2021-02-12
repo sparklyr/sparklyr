c4a3c8acf1f20a00be1c75af4e78086127bcc2c2
Yitao Li <yitao@rstudio.com>
2021-02-12 19:36:31 +0000

    support schema specification for struct columns in `spark_read_*()` methods
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- R/spark_data_build_types.R
+++ R/spark_data_build_types.R
@@ -1,8 +1,29 @@
 spark_data_build_types <- function(sc, columns) {
   names <- names(columns)
-  fields <- lapply(names, function(name) {
-    invoke_static(sc, "sparklyr.SQLUtils", "createStructField", name, columns[[name]][[1]], TRUE)
-  })
+  fields <- lapply(
+    names,
+    function(name) {
+      if (is.list(columns[[name]])) {
+        struct <- spark_data_build_types(sc, columns[[name]])
+        invoke_static(
+          sc,
+          "sparklyr.SQLUtils",
+          "createStructField",
+          name,
+          struct
+        )
+      } else {
+        invoke_static(
+          sc,
+          "sparklyr.SQLUtils",
+          "createStructField",
+          name,
+          columns[[name]][[1]],
+          TRUE
+        )
+      }
+    }
+  )
 
   invoke_static(sc, "sparklyr.SQLUtils", "createStructType", fields)
 }
