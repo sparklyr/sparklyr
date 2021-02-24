90b34840e8b64a439dd671a59a13c6bee3b59af3
Yitao Li <yitao@rstudio.com>
2021-02-24 21:58:18 +0000

    convenience functions for Spark runtime configurations
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- R/spark_context_config.R
+++ R/spark_context_config.R
@@ -40,6 +40,7 @@ hive_context_config <- function(sc) {
 #'   configuration entries.
 #'
 #' @rdname spark_configuration
+#' @family Spark runtime configuration
 #'
 #' @export
 spark_session_config <- function(sc, config = TRUE, value = NULL) {
@@ -61,4 +62,169 @@ spark_session_config <- function(sc, config = TRUE, value = NULL) {
       invoke("%>%", list("conf"), list("set", config, value)) %>%
       invisible()
   }
+}
+
+#' Retrieves or sets status of Spark AQE
+#'
+#' Retrieves or sets whether Spark adaptive query execution is enabled
+#'
+#' @param sc A \code{spark_connection}.
+#' @param enable Whether to enable Spark adaptive query execution. Defaults to
+#'   \code{NULL} to retrieve configuration entries.
+#'
+#' @family Spark runtime configuration
+#'
+#' @export
+spark_adaptive_query_execution <- function(sc, enable = NULL) {
+  if (is.null(enable)) {
+    spark_session_config(sc, "spark.sql.adaptive.enabled")
+  } else {
+    spark_session_config(sc, "spark.sql.adaptive.enabled", enable)
+  }
+}
+
+#' Retrieves or sets whether coalescing contiguous shuffle partitions is enabled
+#'
+#' Retrieves or sets whether coalescing contiguous shuffle partitions is enabled
+#'
+#' @param sc A \code{spark_connection}.
+#' @param enable Whether to enable coalescing of contiguous shuffle partitions.
+#'   Defaults to \code{NULL} to retrieve configuration entries.
+#'
+#' @family Spark runtime configuration
+#'
+#' @export
+spark_coalesce_shuffle_partitions <- function(sc, enable = NULL) {
+  if (spark_version(sc) < "3.0") {
+    warning(
+      "Coalescing coalesce shuffle partitions is only supported in Spark 3.0 ",
+      "or above."
+    )
+  }
+  if (is.null(enable)) {
+    spark_session_config(sc, "spark.sql.adaptive.coalescePartitions.enabled")
+  } else {
+    if (enable) {
+      spark_adaptive_query_execution(sc, TRUE)
+    }
+    spark_session_config(sc, "spark.sql.adaptive.coalescePartitions.enabled", enable)
+  }
+}
+
+#' Retrieves or sets advisory size of the shuffle partition
+#'
+#' Retrieves or sets advisory size in bytes of the shuffle partition during adaptive optimization
+#'
+#' @param sc A \code{spark_connection}.
+#' @param size Advisory size in bytes of the shuffle partition.
+#'   Defaults to \code{NULL} to retrieve configuration entries.
+#'
+#' @family Spark runtime configuration
+#'
+#' @export
+spark_advisory_shuffle_partition_size <- function(sc, size = NULL) {
+  if (spark_version(sc) < "3.0") {
+    warning(
+      "Setting advisory size of shuffle partition during adaptive optimization",
+      " is only supported in Spark 3.0 or above."
+    )
+  }
+  # but proceed anyways even if we don't detect Spark version 3 or above
+  if (is.null(size)) {
+    spark_session_config(
+      sc, "spark.sql.adaptive.advisoryPartitionSizeInBytes"
+    )
+  } else {
+    spark_coalesce_shuffle_partitions(sc, TRUE)
+    spark_session_config(
+      sc, "spark.sql.adaptive.advisoryPartitionSizeInBytes", size
+    )
+  }
+}
+
+#' Retrieves or sets initial number of shuffle partitions before coalescing
+#'
+#' Retrieves or sets initial number of shuffle partitions before coalescing
+#'
+#' @param sc A \code{spark_connection}.
+#' @param num_partitions Initial number of shuffle partitions before coalescing.
+#'   Defaults to \code{NULL} to retrieve configuration entries.
+#'
+#' @family Spark runtime configuration
+#'
+#' @export
+spark_coalesce_initial_num_partitions <- function(sc, num_partitions = NULL) {
+  if (spark_version(sc) < "3.0") {
+    warning(
+      "Setting initial number of shuffle partitions before coalescing is only ",
+      "supported in Spark 3.0 or above."
+    )
+  }
+  # but proceed anyways even if we don't detect Spark version 3 or above
+  if (is.null(num_partitions)) {
+    spark_session_config(
+      sc, "spark.sql.adaptive.coalescePartitions.initialPartitionNum"
+    )
+  } else {
+    spark_coalesce_shuffle_partitions(sc, TRUE)
+    spark_session_config(
+      sc,
+      "spark.sql.adaptive.coalescePartitions.initialPartitionNum",
+      num_partitions
+    )
+  }
+}
+
+#' Retrieves or sets the minimum number of shuffle partitions after coalescing
+#'
+#' Retrieves or sets the minimum number of shuffle partitions after coalescing
+#'
+#' @param sc A \code{spark_connection}.
+#' @param num_partitions Minimum number of shuffle partitions after coalescing.
+#'   Defaults to \code{NULL} to retrieve configuration entries.
+#'
+#' @family Spark runtime configuration
+#'
+#' @export
+spark_coalesce_min_num_partitions <- function(sc, num_partitions = NULL) {
+  if (spark_version(sc) < "3.0") {
+    warning(
+      "Setting minimum number of shuffle partitions after coalescing is only ",
+      "supported in Spark 3.0 or above."
+    )
+  }
+  # but proceed anyways even if we don't detect Spark version 3 or above
+  if (is.null(num_partitions)) {
+    spark_session_config(
+      sc, "spark.sql.adaptive.coalescePartitions.minPartitionNum"
+    )
+  } else {
+    spark_coalesce_shuffle_partitions(sc, TRUE)
+    spark_session_config(
+      sc,
+      "spark.sql.adaptive.coalescePartitions.minPartitionNum",
+      num_partitions
+    )
+  }
+}
+
+#' Retrieves or sets the auto broadcast join threshold
+#'
+#' Configures the maximum size in bytes for a table that will be broadcast to all worker nodes
+#' when performing a join. By setting this value to -1 broadcasting can be disabled. Note that
+#' currently statistics are only supported for Hive Metastore tables where the command
+#' `ANALYZE TABLE <tableName> COMPUTE STATISTICS noscan` has been run, and file-based data source
+#' tables where the statistics are computed directly on the files of data.
+#'
+#' @param sc A \code{spark_connection}.
+#' @param threshold Maximum size in bytes for a table that will be broadcast to all worker nodes
+#'   when performing a join. Defaults to \code{NULL} to retrieve configuration entries.
+#'
+#' @family Spark runtime configuration
+#'
+#' @export
+spark_auto_broadcast_join_threshold <- function(sc, threshold = NULL) {
+  spark_session_config(
+    sc, "spark.sql.autoBroadcastJoinThreshold", threshold
+  )
 }
--- tests/testthat/test-spark-dynamic-config.R
+++ tests/testthat/test-spark-dynamic-config.R
@@ -18,3 +18,124 @@ test_that("configuration getter works", {
     )
   }
 })
+
+test_that("spark_adaptive_query_execution() works", {
+  spark_session_config(sc, "spark.sql.adaptive.enabled", FALSE)
+  spark_adaptive_query_execution(sc, TRUE)
+
+  expect_equal(
+    spark_session_config(sc, "spark.sql.adaptive.enabled") %>%
+      unname() %>%
+      unlist(),
+    "true"
+  )
+  expect_equal(
+    spark_adaptive_query_execution(sc) %>% unname() %>% unlist(), "true"
+  )
+})
+
+test_that("spark_coalesce_shuffle_partitions() works", {
+  spark_session_config(
+    sc, "spark.sql.adaptive.coalescePartitions.enabled", FALSE
+  )
+  spark_coalesce_shuffle_partitions(sc, TRUE)
+
+  expect_equal(
+    spark_session_config(sc, "spark.sql.adaptive.enabled") %>%
+      unname() %>%
+      unlist(),
+    "true"
+  )
+  expect_equal(
+    spark_session_config(
+      sc, "spark.sql.adaptive.coalescePartitions.enabled"
+    ) %>%
+      unname() %>%
+      unlist(),
+    "true"
+  )
+  expect_equal(
+    spark_coalesce_shuffle_partitions(sc) %>% unname() %>% unlist(), "true"
+  )
+})
+
+test_that("spark_advisory_shuffle_partition_size() works", {
+  test_requires_version("3.0.0")
+
+  spark_session_config(sc, "spark.sql.adaptive.enabled", FALSE)
+  spark_session_config(sc, "spark.sql.adaptive.coalescePartitions.enabled", FALSE)
+
+  advisory_shuffle_partition_size <- 64 * 1024 * 1024
+  spark_advisory_shuffle_partition_size(sc, advisory_shuffle_partition_size)
+
+  expect_equal(
+    spark_session_config(sc, "spark.sql.adaptive.enabled") %>%
+      unname() %>%
+      unlist(),
+    "true"
+  )
+  expect_equal(
+    spark_session_config(sc, "spark.sql.adaptive.coalescePartitions.enabled") %>%
+      unname() %>%
+      unlist(),
+    "true"
+  )
+  expect_equal(
+    spark_session_config(sc, "spark.sql.adaptive.advisoryPartitionSizeInBytes") %>%
+      unname() %>%
+      unlist(),
+    as.character(advisory_shuffle_partition_size)
+  )
+  expect_equal(
+    spark_advisory_shuffle_partition_size(sc) %>% unname() %>% unlist(),
+    as.character(advisory_shuffle_partition_size)
+  )
+})
+
+test_that("spark_coalesce_initial_num_partitions() works", {
+  test_requires_version("3.0.0")
+
+  spark_session_config(sc, "spark.sql.adaptive.enabled", FALSE)
+  spark_session_config(sc, "spark.sql.adaptive.coalescePartitions.enabled", FALSE)
+
+  num_partitions <- 64
+  spark_coalesce_initial_num_partitions(sc, num_partitions)
+
+  expect_equal(
+    spark_session_config(sc, "spark.sql.adaptive.enabled") %>%
+      unname() %>%
+      unlist(),
+    "true"
+  )
+  expect_equal(
+    spark_session_config(sc, "spark.sql.adaptive.coalescePartitions.enabled") %>%
+      unname() %>%
+      unlist(),
+    "true"
+  )
+  expect_equal(
+    spark_session_config(
+      sc, "spark.sql.adaptive.coalescePartitions.initialPartitionNum"
+    ) %>%
+      unname() %>%
+      unlist(),
+    as.character(num_partitions)
+  )
+  expect_equal(
+    spark_coalesce_initial_num_partitions(sc) %>% unname() %>% unlist(),
+    as.character(num_partitions)
+  )
+})
+
+test_that("spark_auto_broadcast_join_threshold() works", {
+  spark_session_config(sc, "spark.sql.autoBroadcastJoinThreshold", -1)
+  threshold <- 10 * 1024 * 1024
+  spark_auto_broadcast_join_threshold(sc, threshold)
+
+  expect_equal(
+    spark_session_config(sc, "spark.sql.autoBroadcastJoinThreshold") %>%
+      unname() %>%
+      unlist(),
+    as.character(threshold)
+  )
+})
