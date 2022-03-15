7b5e98b3ccce98ad175578cb11ab960186f506f3
Edgar Ruiz <edgararuiz@gmail.com>
2022-03-15 12:30:25 -0500

    Runs devtools test() instead of source(), finaly!

--- tests/testthat/test-dbi.R
+++ tests/testthat/test-dbi.R
@@ -4,7 +4,7 @@ sc <- testthat_spark_connection()
 iris_tbl <- testthat_tbl("iris")
 dbi_df <- data.frame(a = 1:3, b = letters[1:3])
 
-psersited_table_name <- random_table_name("persisted_")
+persisted_table_name <- random_table_name("persisted_")
 temp_table_name <- random_table_name("temporary_")
 
 test_that("dbWriteTable can write a table", {
@@ -12,13 +12,13 @@ test_that("dbWriteTable can write a table", {
 
   test_requires("DBI")
 
-  dbWriteTable(sc, psersited_table_name, dbi_df)
+  dbWriteTable(sc, persisted_table_name, dbi_df)
   dbWriteTable(sc, temp_table_name, dbi_df, temporary = TRUE)
 
   tables <- dbGetQuery(sc, "SHOW TABLES")
 
   expect_equal(
-    tables[tables$tableName == psersited_table_name, ]$isTemporary,
+    tables[tables$tableName == persisted_table_name, ]$isTemporary,
     FALSE
   )
 
