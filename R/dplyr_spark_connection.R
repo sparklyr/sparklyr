8fceadeaaaf1658916453a214c0bfb47fb6aa10e
Maximilian Girlich <maximilian.girlich@metoda.com>
2022-04-06 09:09:45 +0000

    Add compatibility with new dbplyr
    Signed-off-by: Maximilian Girlich <maximilian.girlich@metoda.com>

--- R/dplyr_spark_connection.R
+++ R/dplyr_spark_connection.R
@@ -44,17 +44,30 @@ filter.tbl_spark <- function(.data, ..., .preserve = FALSE) {
 
   dots <- rlang::quos(...)
   dots <- partial_eval_dots(dots, sim_data = simulate_vars(.data))
-  dbplyr::add_op_single("filter", .data, dots = dots)
+
+  if (dbplyr_uses_ops()) {
+    dbplyr::add_op_single("filter", .data, dots = dots)
+  } else {
+    .data$lazy_query <- dbplyr:::add_filter(.data, dots)
+    .data
+  }
 }
 
 #' @export
 #' @importFrom dplyr select
 select.tbl_spark <- function(.data, ...) {
   sim_data <- simulate_vars(.data)
+
+  if (dbplyr_uses_ops()) {
+    grps <- dbplyr::op_grps(.data$ops)
+  } else {
+    grps <- dbplyr::op_grps(.data$lazy_query)
+  }
+
   loc <- tidyselect::eval_select(
     rlang::expr(c(...)),
     sim_data,
-    include = dbplyr::op_grps(.data$ops)
+    include = grps
   )
   new_vars <- rlang::set_names(rlang::syms(names(sim_data)[loc]), names(loc))
   .class <- class(.data)
@@ -70,7 +83,6 @@ select.tbl_spark <- function(.data, ...) {
 
 #' @export
 #' @importFrom dplyr summarise
-#' @importFrom dbplyr add_op_single
 #' @importFrom dbplyr op_vars
 summarise.tbl_spark <- function(.data, ..., .groups = NULL) {
   # NOTE: this is mostly copy-pasted from
@@ -123,12 +135,17 @@ summarise.tbl_spark <- function(.data, ..., .groups = NULL) {
   check_summarise_vars(dots)
   check_groups(.groups)
 
-  add_op_single(
-    "summarise",
-    .data,
-    dots = dots,
-    args = list(.groups = .groups, env_caller = rlang::caller_env())
-  )
+  if (dbplyr_uses_ops()) {
+    dbplyr::add_op_single(
+      "summarise",
+      .data,
+      dots = dots,
+      args = list(.groups = .groups, env_caller = rlang::caller_env())
+    )
+  } else {
+    .data$lazy_query <- dbplyr:::add_summarise(.data, dots, .groups = .groups, env_caller = rlang::caller_env())
+    .data
+  }
 }
 
 #' @export
