f28228f84fc5bc17ec8dc7f6480f470d5ab73502
Maximilian Girlich <maximilian.girlich@metoda.com>
2022-04-21 08:39:16 +0000

    Adapt `ft_extract_sql()` to new interface

--- R/ml_feature_sql_transformer_utils.R
+++ R/ml_feature_sql_transformer_utils.R
@@ -1,12 +1,25 @@
 ft_extract_sql <- function(x) {
-  get_base_name <- function(o) {
-    if (!inherits(o$x, "ident")) {
-      get_base_name(o$x)
-    } else {
-      o$x
+  if (dbplyr_uses_ops()) {
+    get_base_name <- function(o) {
+      if (!inherits(o$x, "ident")) {
+        get_base_name(o$x)
+      } else {
+        o$x
+      }
     }
+
+    pattern <- paste0("\\b", get_base_name(x$ops), "\\b")
+  } else {
+    get_base_name <- function(o) {
+      if (!inherits(o$from, "lazy_query_base")) {
+        get_base_name(o$from)
+      } else {
+        o$from$x
+      }
+    }
+
+    pattern <- paste0("\\b", get_base_name(x$lazy_query), "\\b")
   }
-  pattern <- paste0("\\b", get_base_name(x$ops), "\\b")
 
   gsub(pattern, "__THIS__", dbplyr::sql_render(x))
 }
