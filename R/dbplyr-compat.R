5fdcf38d1a82277f42ea35dbf52ea3ddbbd3dd4a
Maximilian Girlich <maximilian.girlich@metoda.com>
2022-04-22 07:19:35 +0000

    Fix typo

--- R/dbplyr-compat.R
+++ R/dbplyr-compat.R
@@ -1,6 +1,6 @@
 add_op_single <- function(...) {
   if (dbplyr_uses_ops()) {
-    f <- utils::getFromNamespace("distinct.tbl_lazy", "dbplyr")
+    f <- utils::getFromNamespace("add_op_single", "dbplyr")
     f(...)
   }
 }
