33ce42590cf576a278830d49880c4fdda38c7b35
Maximilian Girlich <maximilian.girlich@metoda.com>
2022-04-22 06:49:31 +0000

    Remove unnecessary functions

--- R/dbplyr-compat.R
+++ R/dbplyr-compat.R
@@ -4,30 +4,3 @@ add_op_single <- function(...) {
     f(...)
   }
 }
-
-add_filter <- function(...) {
-  if (dbplyr_uses_ops()) {
-    rlang::abort("Internal error")
-  } else {
-    f <- utils::getFromNamespace("add_filter", "dbplyr")
-    f(...)
-  }
-}
-
-add_select <- function(...) {
-  if (dbplyr_uses_ops()) {
-    rlang::abort("Internal error")
-  } else {
-    f <- utils::getFromNamespace("add_select", "dbplyr")
-    f(...)
-  }
-}
-
-add_summarise <- function(...) {
-  if (dbplyr_uses_ops()) {
-    rlang::abort("Internal error")
-  } else {
-    f <- utils::getFromNamespace("add_summarise", "dbplyr")
-    f(...)
-  }
-}
