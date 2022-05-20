f47f99561481a74a08bf0415a329d12fe3b28170
Edgar Ruiz <edgararuiz@gmail.com>
2022-05-20 12:42:34 -0500

    Updates Isotonic regression with new methodology

--- R/ml_regression_isotonic_regression.R
+++ R/ml_regression_isotonic_regression.R
@@ -37,102 +37,43 @@ ml_isotonic_regression <- function(x, formula = NULL, feature_index = 0, isotoni
   UseMethod("ml_isotonic_regression")
 }
 
-#' @export
-ml_isotonic_regression.spark_connection <- function(x, formula = NULL, feature_index = 0, isotonic = TRUE,
-                                                    weight_col = NULL, features_col = "features",
-                                                    label_col = "label", prediction_col = "prediction",
-                                                    uid = random_string("isotonic_regression_"), ...) {
-  .args <- list(
-    feature_index = feature_index,
-    isotonic = isotonic,
-    weight_col = weight_col,
-    features_col = features_col,
-    label_col = label_col,
-    prediction_col = prediction_col
-  ) %>%
-    c(rlang::dots_list(...)) %>%
-    validator_ml_isotonic_regression()
-
-  jobj <- spark_pipeline_stage(
-    x, "org.apache.spark.ml.regression.IsotonicRegression", uid,
-    features_col = .args[["features_col"]], label_col = .args[["label_col"]],
-    prediction_col = .args[["prediction_col"]]
-  ) %>%
-    invoke("setFeatureIndex", .args[["feature_index"]]) %>%
-    invoke("setIsotonic", .args[["isotonic"]]) %>%
-    jobj_set_param("setWeightCol", .args[["weight_col"]])
-
-  new_ml_isotonic_regression(jobj)
-}
-
-#' @export
-ml_isotonic_regression.ml_pipeline <- function(x, formula = NULL, feature_index = 0, isotonic = TRUE,
-                                               weight_col = NULL, features_col = "features",
-                                               label_col = "label", prediction_col = "prediction",
-                                               uid = random_string("isotonic_regression_"), ...) {
-  stage <- ml_isotonic_regression.spark_connection(
-    x = spark_connection(x),
+ml_isotonic_regression_default <- function(x, formula = NULL, feature_index = 0, isotonic = TRUE,
+                                           weight_col = NULL, features_col = "features",
+                                           label_col = "label", prediction_col = "prediction",
+                                           uid = random_string("isotonic_regression_"),
+                                           response = NULL, features = NULL, ...) {
+  ml_process_model(
+    x = x,
+    spark_class = "org.apache.spark.ml.regression.IsotonicRegression",
+    r_class = "ml_isotonic_regression",
+    ml_function = new_ml_model_isotonic_regression,
+    features = features,
+    response = response,
+    uid = uid,
     formula = formula,
-    feature_index = feature_index,
-    isotonic = isotonic,
-    weight_col = weight_col,
-    features_col = features_col,
-    label_col = label_col,
-    prediction_col = prediction_col,
-    uid = uid,
-    ...
+    invoke_steps = list(
+      setFeaturesCol = features_col,
+      setLabelCol = label_col,
+      setPredictionCol = prediction_col,
+      setFeatureIndex = cast_scalar_integer(feature_index),
+      setIsotonic = cast_scalar_logical(isotonic),
+      setWeightCol = cast_nullable_string(weight_col)
+    )
   )
-  ml_add_stage(x, stage)
+
 }
+
+# ------------------------------- Methods --------------------------------------
+#' @export
+ml_isotonic_regression.spark_connection <- ml_isotonic_regression_default
 
 #' @export
-ml_isotonic_regression.tbl_spark <- function(x, formula = NULL, feature_index = 0, isotonic = TRUE,
-                                             weight_col = NULL, features_col = "features",
-                                             label_col = "label", prediction_col = "prediction",
-                                             uid = random_string("isotonic_regression_"),
-                                             response = NULL, features = NULL, ...) {
-  formula <- ml_standardize_formula(formula, response, features)
+ml_isotonic_regression.ml_pipeline <- ml_isotonic_regression_default
 
-  stage <- ml_isotonic_regression.spark_connection(
-    x = spark_connection(x),
-    formula = NULL,
-    feature_index = feature_index,
-    isotonic = isotonic,
-    weight_col = weight_col,
-    features_col = features_col,
-    label_col = label_col,
-    prediction_col = prediction_col,
-    uid = uid,
-    ...
-  )
+#' @export
+ml_isotonic_regression.tbl_spark <- ml_isotonic_regression_default
 
-  if (is.null(formula)) {
-    stage %>%
-      ml_fit(x)
-  } else {
-    ml_construct_model_supervised(
-      new_ml_model_isotonic_regression,
-      predictor = stage,
-      formula = formula,
-      dataset = x,
-      features_col = features_col,
-      label_col = label_col
-    )
-  }
-}
-
-# Validator
-validator_ml_isotonic_regression <- function(.args) {
-  .args[["feature_index"]] <- cast_scalar_integer(.args[["feature_index"]])
-  .args[["isotonic"]] <- cast_scalar_logical(.args[["isotonic"]])
-  .args[["weight_col"]] <- cast_nullable_string(.args[["weight_col"]])
-  .args
-}
-
-new_ml_isotonic_regression <- function(jobj) {
-  new_ml_estimator(jobj, class = "ml_isotonic_regression")
-}
-
+# ---------------------------- Constructors ------------------------------------
 new_ml_isotonic_regression_model <- function(jobj) {
   new_ml_transformer(
     jobj,
