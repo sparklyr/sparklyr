c33083caebc93141528cbce0107c6afa1ba351c8
Edgar Ruiz <edgararuiz@gmail.com>
2022-07-26 13:43:41 -0400

    Adds ml_metrics script, first pass at regression metrics

---
+++ R/ml_metrics.R
@@ -0,0 +1,61 @@
+
+# "rmse" (default): root mean squared error -
+# "mse": mean squared error -
+# "r2": R^2^ metric -
+# "mae": mean absolute error -
+# "var": explained variance
+
+#' @importFrom rlang as_name
+#' @importFrom purrr map_dfr
+#' @importFrom tibble tibble
+#' @export
+ml_metrics_regression <- function(data, truth, estimate = prediction,
+                                  metrics = c("rmse", "rsq", "mae"),
+                                  ...) {
+  estimate <- enquo(estimate)
+  truth <- enquo(truth)
+
+  conn <- spark_connection(data)
+  df_data <- spark_dataframe(data)
+
+  evaluator <- "org.apache.spark.ml.evaluation.RegressionEvaluator"
+  init_steps <- list(
+    "setLabelCol" = as_name(estimate),
+    "setPredictionCol" = as_name(truth)
+  )
+  init <- ml_metrics_init(evaluator, init_steps)
+
+  map_dfr(
+    metrics,
+    ~ {
+      metric <- ifelse(.x == "rsq", "r2", .x)
+      steps <- list(
+        "setMetricName" = metric,
+        "evaluate" = df_data
+      )
+      val <- ml_metrics_steps(init, steps)
+      tibble(.metric = .x, .estimator = "standard", .estimate = val)
+    }
+  )
+}
+
+ml_metrics_init <- function(evaluator, invoke_steps = list()) {
+  new_jobj <- invoke_new(sc, list(evaluator, random_string("metric_")))
+  ml_metrics_steps(new_jobj, invoke_steps)
+}
+
+#' @importFrom purrr imap
+ml_metrics_steps <- function(jobj, invoke_steps = list()) {
+  l_steps <- imap(invoke_steps, ~ list(.y, .x))
+  for(i in seq_along(l_steps)) {
+    jobj <- do.call(invoke, c(jobj, l_steps[[i]]))
+  }
+  jobj
+}
+
+
+
+
+
+
+
