311c28ed71711906a993de5c7d2e701fe0df4d45
Edgar Ruiz <edgararuiz@gmail.com>
2023-07-06 17:36:54 -0500

    Cleans up sample tests, adds skip-if lines to handle pysparklyr

--- tests/testthat/test-dplyr-sample-n.R
+++ tests/testthat/test-dplyr-sample-n.R
@@ -1,39 +1,40 @@
 skip_connection("dplyr-sample-n")
+test_requires_version("2.0.0")
+test_requires("dplyr")
 skip_on_livy()
 
 sc <- testthat_spark_connection()
 
 iris_tbl <- testthat_tbl("iris")
-test_requires("dplyr")
 
 test_that("'sample_n' works as expected", {
-  test_requires_version("2.0.0")
-  skip_on_livy()
-  test_requires("dplyr")
 
+  s_replace <- spark_integ_test_skip(sc,  "sample-n-replace")
+  s_weights <- spark_integ_test_skip(sc, "sample-n-weights")
   for (weight in list(NULL, rlang::sym("Petal_Length"))) {
     for (replace in list(FALSE, TRUE)) {
-      sample_sdf <- iris_tbl %>%
-        sample_n(10, weight = !!weight, replace = replace)
-      expect_equal(colnames(sample_sdf), colnames(iris_tbl))
-      expect_equal(sample_sdf %>% collect() %>% nrow(), 10)
+      skip_test <- FALSE
+      if(replace && s_replace) skip_test <- TRUE
+      if(!is.null(weight) && s_weights) skip_test <- TRUE
+      if(!skip_test) {
+        sample_sdf <- iris_tbl %>%
+          sample_n(10, weight = !!weight, replace = replace)
+        expect_equal(colnames(sample_sdf), colnames(iris_tbl))
+        expect_equal(sample_sdf %>% collect() %>% nrow(), 10)
 
-      sample_sdf <- iris_tbl %>%
-        select(Petal_Length) %>%
-        sample_n(10, weight = !!weight, replace = replace)
-      expect_equal(colnames(sample_sdf), "Petal_Length")
-      expect_equal(sample_sdf %>% collect() %>% nrow(), 10)
+        sample_sdf <- iris_tbl %>%
+          select(Petal_Length) %>%
+          sample_n(10, weight = !!weight, replace = replace)
+        expect_equal(colnames(sample_sdf), "Petal_Length")
+        expect_equal(sample_sdf %>% collect() %>% nrow(), 10)
+      }
     }
   }
 })
 
 test_that("weighted sampling works as expected with integer weight columns", {
-  test_requires_version("2.0.0")
-  skip_on_livy()
-  test_requires("dplyr")
-
+  skip_connection("sample-n-weights")
   sdf <- copy_to(sc, tibble::tibble(id = seq(100), weight = seq(100)))
-
   for (replace in list(FALSE, TRUE)) {
     sample_sdf <- sdf %>%
       sample_n(20, weight = weight, replace = replace)
