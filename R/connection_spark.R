d77a58bb4df0dd1d640add7019a9b654e588fe08
Edgar Ruiz <edgararuiz@gmail.com>
2023-10-26 19:34:58 -0500

    Fixes arguments in connection method

--- R/connection_spark.R
+++ R/connection_spark.R
@@ -240,7 +240,6 @@ spark_connect <- function(master,
     config = config,
     app_name = app_name,
     version = version,
-    packages = packages,
     extensions = extensions,
     scala_version = scala_version,
     ...
@@ -327,7 +326,6 @@ spark_connect_method <- function(
     config,
     app_name,
     version,
-    packages,
     extensions,
     scala_version,
     ...) {
@@ -343,7 +341,6 @@ spark_connect_method.default <- function(
     config,
     app_name,
     version,
-    packages,
     extensions,
     scala_version,
     ...) {
