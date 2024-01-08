5a8e021f22d94282e77b98dfa41325a60285bcd7
Edgar Ruiz <edgararuiz@gmail.com>
2024-01-08 13:37:15 -0600

    Removes dcf and apps

--- inst/rstudio/connections/Livy.R
+++
@@ -1,6 +0,0 @@
-library(sparklyr)
-sc <- spark_connect(master = "${1:Livy URL=http$colon$//server/livy/}",
-                    version = "${2:Version=2.4.0}",
-                    method = "livy", config = livy_config(
-                      username = "${3:User}",
-                      password = rstudioapi::askForPassword("Livy password:")))
