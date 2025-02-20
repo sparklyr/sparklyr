a3c043f9c348d450b485b54927a5d760a9b384f7
Edgar Ruiz <edgararuiz@gmail.com>
2025-02-19 20:48:55 -0600

    Adds Scala 2.13 option to Avro

--- R/avro_utils.R
+++ R/avro_utils.R
@@ -12,7 +12,13 @@ spark_avro_package_name <- function(spark_version, scala_version = NULL) {
   if (spark_version < "2.4.0") stop("Avro requires Spark 2.4.0 or newer")
 
   scala_version <- scala_version %||% (
-    if (spark_version >= "3.0.0") "2.12" else "2.11"
+    if (spark_version >= "4.0.0") {
+      "2.13"
+    } else if (spark_version >= "3.0.0") {
+      "2.12"
+    } else {
+      "2.11"
+    }
   )
   paste0(
     "org.apache.spark:spark-avro_",
