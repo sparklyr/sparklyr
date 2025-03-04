da04b8b74656d0e3197b17313e6a248e10696afa
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-03-04 14:49:56 -0600

    Addresses failure in 3.3

--- tests/testthat/test-broom-isotonic_regression.R
+++ tests/testthat/test-broom-isotonic_regression.R
@@ -19,7 +19,7 @@ test_that("isotonic_regression.tidy() works", {
   td1 <- tidy(ir_model)
 
   check_tidy(td1,
-    exp.row = 20,
+    exp.row = length(ir_model$model$boundaries()),
     exp.names = c("boundaries", "predictions")
   )
 
